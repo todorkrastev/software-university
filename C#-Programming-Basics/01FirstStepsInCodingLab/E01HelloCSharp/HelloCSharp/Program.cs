36c40eab2b19169fd923959ce3d43b0446ca31fc
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-06 18:32:11 +0300

    Added Exercise

---
+++ C#-Programming-Basics/01FirstStepsInCodingLab/E01HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Nums 1...10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++);
+        Console.WriteLine(i);
+        }
+    }
+}
