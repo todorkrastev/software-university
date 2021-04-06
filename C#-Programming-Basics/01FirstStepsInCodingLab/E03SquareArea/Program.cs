53e3d1dd47a7f8fbf7e601a6fb6e223b0bad6ff5
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-06 18:17:06 +0300

    Added Exercises

---
+++ C#-Programming-Basics/01FirstStepsInCodingLab/E03SquareArea/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var side = int.Parse(Console.ReadLine());
+            var area = side * side;
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ C#-Programming-Basics/01FirstStepsInCodingLab/E08PetShop/PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofDogs = int.Parse(Console.ReadLine());
+            int numberofAnimals = int.Parse(Console.ReadLine());
+            double Dogfoodprice = 2.5;
+            double Animalfoodprice = 4;
+            double Totalprice = (numberofDogs * Dogfoodprice) + (numberofAnimals * Animalfoodprice);
+            Console.WriteLine($"{Totalprice} lv.");
+
+        }
+    }
+}
