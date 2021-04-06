f87a7dbe227684ddef70e684ad726a780e680784
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-06 18:12:29 +0300

    Added Exercise

---
+++ C#-Programming-Basics/01FirstStepsInCodingLab/E02Nums1To10ForLoop/Nums1To10ForLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nums1to10_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+           for(int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}N
