122c7be651b33f72624f5426097423f8dbaf795d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-06 18:33:40 +0300

    Added Exercise

---
+++ C#-Programming-Basics/01FirstStepsInCodingLab/E02Nums1To10/Nums1to10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nums1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+             for(int i = 0; i< 10; i++)
+        {
+            Console.WriteLine("Value of i: {0}", i);
+        }
+    }
+}
