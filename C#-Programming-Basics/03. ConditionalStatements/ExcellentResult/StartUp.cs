08a455e5f87d7735de1721902938f7dcd6e452bb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-08 19:02:34 +0300

    Adding Conditional Statements - Lab

---
+++ C#-Programming-Basics/03. ConditionalStatements/ExcellentResult/StartUp.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ExcellentResult
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            double insertGrade = double.Parse(Console.ReadLine());
+            if (insertGrade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
---
+++ C#-Programming-Basics/03. ConditionalStatements/GreaterNumber/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace GreaterNumber
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            if (numOne > numTwo)
+            {
+                Console.WriteLine(numOne);
+            }
+            else
+            {
+                Console.WriteLine(numTwo);
+            }
+        }
+    }
+}
