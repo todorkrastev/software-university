0b56b76781a1355b6b2c53071c1037ec863d13a2
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-12 19:32:07 +0300

    Initial Commit

---
+++ 01. C#-Programming-Basics/3.2. ConditionalStatementsAdvancedExercise/E01Cinema/StartUp.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Cinema
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string typeOfFilmShow = Console.ReadLine();
+            int numberOfRows = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            double priceOfTicket = 0;
+
+            switch(typeOfFilmShow)
+            {
+                case "Premiere":
+                    priceOfTicket = 12;
+                    break;
+
+                case "Normal":
+                    priceOfTicket = 7.5;
+                    break;
+
+                case "Discount":
+                    priceOfTicket = 5;
+                    break;
+            }
+            double totalIncome = (numberOfLines * numberOfRows) * priceOfTicket;
+            Console.WriteLine($"{totalIncome:f2}");
+        }
+    }
+}
