d3e29528e963532d51333ad3dda89be06301f0db
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-13 17:48:36 +0300

    Initial Commit

---
+++ 01. C#-Programming-Basics/3.3. ConditionalStatementsMoreExercises/05.Firm/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.Firm
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int hoursNeeded = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            int numEmployees = int.Parse(Console.ReadLine());
+
+            double trainingEmployees = (day * 0.9) * 8;
+            int overtime = numEmployees * (2 * day);
+            double workingOnProject = Math.Floor(trainingEmployees + overtime);
+            double diff = Math.Abs(workingOnProject - hoursNeeded);
+
+            if (hoursNeeded <= workingOnProject)
+            {
+                Console.WriteLine($"Yes!{diff} hours left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{diff} hours needed.");
+            }
+        }
+    }
+}
---
+++ 01. C#-Programming-Basics/3.3. ConditionalStatementsMoreExercises/07.FlowerShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07.FlowerShop
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int magnolia = int.Parse(Console.ReadLine());
+            int hyiacinth = int.Parse(Console.ReadLine());
+            int rose = int.Parse(Console.ReadLine());
+            int cactus = int.Parse(Console.ReadLine());
+            int giftPrice = int.Parse(Console.ReadLine());
+
+            double magnoliaPrice = 3.25;
+            double hyiacinthPrice = 4.00;
+            double rosePrice = 3.50;
+            double cactusPrice = 8;
+
+            double income = (magnolia * magnoliaPrice + hyiacinth * hyiacinthPrice + rose * rosePrice + cactus * cactusPrice) * 0.95;
+
+            double diff = Math.Abs(income - giftPrice);
+
+            if (giftPrice <= income)
+            {
+                Console.WriteLine($"She is left with {Math.Floor(diff)} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(diff)} leva.");
+            }
+        }
+    }
+}
