0a049a2e46985d3224331f1db55a014605b84a27
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-16 23:18:45 +0200

    Adding E01NumbersFrom1To100

---
+++ 01. C#-Programming-Basics/4.1. ForLoopLab/E01NumbersFrom1To100/StartUp.cs
@@ -0,0 +1,15 @@
+﻿
+namespace NumbersFrom1To100
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
