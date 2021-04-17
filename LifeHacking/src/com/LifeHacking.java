d3142bfe2a2ae8a93600854b4129c164eb517bcc
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-18 01:27:56 +0200

    Update LifeHacking.java

--- LifeHacking/src/com/LifeHacking.java
+++ LifeHacking/src/com/LifeHacking.java
@@ -1,14 +1,7 @@
 package com;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-
 public class LifeHacking {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    public static void main(String[] args) {
 
         boolean stillAlive = true;
 
