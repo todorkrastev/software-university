b2a249d9f71e75211d62da50292ac75213366f4f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-20 22:51:26 +0200

    Create E04EvenPowersOf2

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E04EvenPowersOf2/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenPowersOf2
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    double result = Math.Pow(2, i);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
