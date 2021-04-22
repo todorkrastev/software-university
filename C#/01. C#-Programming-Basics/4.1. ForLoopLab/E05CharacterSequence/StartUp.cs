73d6b74dcae8f9ef123e4c17f3bf3a45fdb95966
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-22 23:29:58 +0200

    Create E05CharacterSequence

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E05CharacterSequence/StartUp.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CharacterSequence
+{
+using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
