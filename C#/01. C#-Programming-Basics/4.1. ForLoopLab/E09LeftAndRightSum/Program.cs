3aa7520f38782e998bcacd115d32e100b3282e3a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-24 11:37:54 +0200

    Create E09 LeftAndRightSum

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E09LeftAndRightSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int resultLeft = 0;
+            int resultRight = 0;
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                int numLeft = int.Parse(Console.ReadLine());
+                resultLeft += numLeft;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                int numRight = int.Parse(Console.ReadLine());
+                resultRight += numRight;
+            }
+
+            if (resultLeft == resultRight)
+            {
+                Console.WriteLine($"Yes, sum = {resultLeft}");
+            }
+            else
+            {
+                int totalResult = resultLeft - resultRight;
+                Console.WriteLine($"No, diff = {Math.Abs(totalResult)}");
+            }
+        }
+    }
+}
