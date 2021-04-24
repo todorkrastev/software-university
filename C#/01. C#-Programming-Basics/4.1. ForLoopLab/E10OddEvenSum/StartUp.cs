1d909255600aaa17416f962f798e9cf26d458113
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-24 11:39:43 +0200

    Create E10OddEvenSum

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E10OddEvenSum/StartUp.cs
@@ -0,0 +1,41 @@
+﻿
+namespace OddEvenSum
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int odd = 0;
+            int even = 0;
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += num;
+                }
+                else
+                {
+                    oddSum += num;
+                }
+            }
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(evenSum - oddSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
