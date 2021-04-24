bfd1f81e1c770bd250870516e60f22e84d8ab96a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-24 11:35:50 +0200

    Create E08NumberSequence

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E08NumberSequence/StartUp.cs
@@ -0,0 +1,35 @@
+﻿
+namespace NumberSequence
+{
+    using System;
+    using System.Xml.Schema;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int smallestNum = int.MaxValue;
+            int biggestNum = int.MinValue;
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > biggestNum)
+                {
+                    biggestNum = num;
+                }
+
+                if (num < smallestNum)
+                {
+                    smallestNum = num;
+                }
+
+            }
+            Console.WriteLine($"Max number: {biggestNum}");
+            Console.WriteLine($"Min number: {smallestNum}");
+        }
+    }
+}
