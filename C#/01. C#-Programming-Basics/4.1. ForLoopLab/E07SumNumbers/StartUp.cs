cd54e642cdfbe11043ad9224d7552202bbd21153
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-24 11:34:30 +0200

    Create E07SumNumbers

---
+++ C#/01. C#-Programming-Basics/4.1. ForLoopLab/E07SumNumbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace SumNumbers
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int result = 0;
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                result += num;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
