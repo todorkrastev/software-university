154c5bc0dd0dc47885e706161e51c35fb3ec5255
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 22:56:47 +0200

    Create E03SumNumbers

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E03SumNumbers/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.SumNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+           
+            int sum = 0;
+
+            while (sum < num)
+            {
+                int countNum = int.Parse(Console.ReadLine());
+                sum += countNum;    
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
