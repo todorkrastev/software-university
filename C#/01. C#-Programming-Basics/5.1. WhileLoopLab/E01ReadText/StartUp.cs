3afaba25a3ed2fe4b055a41a138070bab41230cf
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 22:54:24 +0200

    Create E01ReadText

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E01ReadText/StartUp.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _01.ReadText
+{
+using System;
+    using System.Reflection.Metadata.Ecma335;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string txt = Console.ReadLine();
+                if (txt == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(txt);
+            }
+        }
+    }
+}
