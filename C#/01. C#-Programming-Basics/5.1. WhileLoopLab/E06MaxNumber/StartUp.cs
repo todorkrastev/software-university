e3732ac2a3ba5693ddfe8ccf5898368846875e18
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 23:00:51 +0200

    Create E06MaxNumber

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E06MaxNumber/StartUp.cs
@@ -0,0 +1,24 @@
+﻿
+namespace _06.MaxNumber
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int biggestNum = int.MinValue;
+
+            while (input != "Stop")
+            {
+                int inputNum = int.Parse(input);
+                if (biggestNum < inputNum)
+                {
+                    biggestNum = inputNum;
+                }
+                    input = Console.ReadLine();
+            }
+            Console.WriteLine(biggestNum);
+        }
+    }
+}
