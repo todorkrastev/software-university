c18fc73faad836d2b50c70445b408b7f196c526f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 22:58:15 +0200

    Create E04Sequence2k+1

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E04Sequence2k+1/StartUp.cs
@@ -0,0 +1,19 @@
+﻿
+namespace _04.Sequence2k_1
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int startCounting = 1;
+
+            while (startCounting <= num)
+            {
+                Console.WriteLine(startCounting);
+                startCounting = (startCounting * 2) + 1;
+            }
+        }
+    }
+}
