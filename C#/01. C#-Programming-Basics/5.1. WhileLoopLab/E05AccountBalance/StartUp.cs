33cd48149b65ab6a85e36f6d5a99b5756c5992f3
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 22:59:38 +0200

    Create E05AccountBalance

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E05AccountBalance/StartUp.cs
@@ -0,0 +1,30 @@
+﻿    using System;
+
+    namespace _05.AccountBalance
+    {
+        class StartUp
+        {
+            static void Main(string[] args)
+            {
+            string input = Console.ReadLine();
+            double balance = 0.0;
+            while (input != "NoMoreMoney")
+            {
+                double amount = double.Parse(input);
+                if (amount >= 0)
+                {
+                    balance += amount;
+                    Console.WriteLine($"Increase: {amount:F2}");
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+            }
+            Console.WriteLine($"Total: {balance:F2}");
+        }
+    }
+}
