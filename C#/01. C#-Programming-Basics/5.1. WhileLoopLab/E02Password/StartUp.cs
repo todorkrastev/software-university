170431db208c586d2e6646c64b848063c4e72857
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-25 22:55:34 +0200

    Create E02Password

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E02Password/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace _02.Password
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+            string checkPassword = Console.ReadLine();
+
+
+            while (checkPassword != password)
+            {
+                checkPassword = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
