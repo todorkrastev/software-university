28d711e626f7a3d6dfab905a1f6b4bb81b3abb63
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-26 19:23:47 +0200

    Create E07MinNumber

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E07MinNumber/StartUp.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _07.MinNumber
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            int smallestNum = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int inputNum = int.Parse(input);
+                if (inputNum < smallestNum)
+                {
+                    smallestNum = inputNum;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(smallestNum);
+        }
+    }
+}
+
