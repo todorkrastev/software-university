ed08285cac7ce9031bb0497d75e17d740fd58b44
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:30:10 +0200

    Create  E08E08GraduationPt.2

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E08GraduationPt.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.GraduationPt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 0;
+            double averageGrade = 0.0;
+            int isExcluded = 0;
+
+            while (true)
+            {
+                double gradePerYear = double.Parse(Console.ReadLine());
+                averageGrade += gradePerYear;
+                grade++;
+                if (gradePerYear < 4)
+                {
+                    isExcluded++;
+                    if (1 < isExcluded)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {grade - 1} grade");
+                        break;
+                    }
+                }
+                if (12 <= grade)
+                {
+                    double finalAverageGrade = averageGrade / grade;
+                    Console.WriteLine($"{name} graduated. Average grade: {finalAverageGrade:f2}");
+                    break;
+                }
+            }
+        }
+    }
+}
