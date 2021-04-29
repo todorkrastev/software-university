d2a182347daef423ea5850d6af2e663b3edb7cc9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:45:57 +0200

    Create E06Cake

---
+++ C#/01. C#-Programming-Basics/5.2. WhileLoopExercises/E06Cake/StartUp.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _06.Cake
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int widthCake = int.Parse(Console.ReadLine());
+            int lengthCake = int.Parse(Console.ReadLine());
+            int sumOfCake = widthCake * lengthCake;
+
+            string command = "";
+
+            while (0 <= sumOfCake)
+            {
+                command = Console.ReadLine();
+
+                if (command == "STOP" && 0 <= sumOfCake)
+                {
+                    Console.WriteLine($"{sumOfCake} pieces are left.");
+                    return;
+                }
+
+                int pieceOfCake = int.Parse(command);
+                sumOfCake -= pieceOfCake;
+
+            }
+            Console.WriteLine($"No more cake left! You need {Math.Abs(sumOfCake)} pieces more.");
+        }
+    }
+}
