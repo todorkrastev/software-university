f12301513d369983b1009e2ec081dd7ff2af74e7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:31:09 +0200

    Create E09Moving

---
+++ C#/01. C#-Programming-Basics/5.1. WhileLoopLab/E09Moving/StartUp.cs
@@ -0,0 +1,39 @@
+﻿
+namespace _09.Moving
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int m3Apartment = width * lenght * height;
+            int sumBoxes = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                int boxes = int.Parse(command);
+                sumBoxes += boxes;
+
+                if (sumBoxes > m3Apartment)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(m3Apartment - sumBoxes)} Cubic meters more.");
+                    break;
+                }
+
+                command = Console.ReadLine();
+
+                if (command == "Done" && sumBoxes < m3Apartment)
+                {
+                    Console.WriteLine($"{m3Apartment - sumBoxes} Cubic meters left.");
+                    break;
+                }
+            }
+        }
+    }
+}
