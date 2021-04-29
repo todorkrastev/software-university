f202dab25fd9500a721b394432416a17fe8824c9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:43:36 +0200

    Create E03Vacation

---
+++ C#/01. C#-Programming-Basics/5.2. WhileLoopExercises/E03Vacation/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double moneyNeeded = double.Parse(Console.ReadLine());
+            double currentMoney = double.Parse(Console.ReadLine());
+
+            int daysCounter = 0;
+            int spendCounter = 0;
+
+            while (currentMoney < moneyNeeded)
+            {
+                string input = Console.ReadLine();
+                double moneySavedSpend = double.Parse(Console.ReadLine());
+
+                if (input == "spend")
+                {
+                    currentMoney = Math.Max((currentMoney - moneySavedSpend), 0.00);
+                    spendCounter++;
+                    daysCounter++;
+                }
+
+                else
+                {
+                    currentMoney += moneySavedSpend;
+                    spendCounter = 0;
+                    daysCounter++;
+                }
+                if (spendCounter == 5)
+                {
+                    Console.WriteLine("You can't save the money.");
+                    Console.WriteLine(daysCounter);
+                    return;
+                }
+            }
+            Console.WriteLine($"You saved the money for {daysCounter} days.");
+        }
+    }
+}
