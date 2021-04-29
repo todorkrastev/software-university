b9b9164ccba3cfb50689a279a7b9c4eb26cb429f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:39:06 +0200

    Create E01OldBooks

---
+++ C#/01. C#-Programming-Basics/5.2. WhileLoopExercises/Resources/E01OldBooks/StartUp.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _01.OldBooks
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string requiredBook = Console.ReadLine();
+            int count = 0;
+
+            while (requiredBook != "No More Books")
+            {
+                string inputBook = Console.ReadLine();
+                if (inputBook == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {count} books.");
+                    break;
+                }
+                else if (inputBook == requiredBook)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    break;
+                }
+                count++;
+            }
+        }
+    }
+}
