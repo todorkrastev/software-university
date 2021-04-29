50148ea86a5e8f4f12fd0c8e2e2ab14533867aa8
Todor Krastev <krastevtodordinev@yahoo.com>
2021-04-29 23:45:18 +0200

    Create E05Coins

---
+++ C#/01. C#-Programming-Basics/5.2. WhileLoopExercises/E05Coins/StartUp.cs
@@ -0,0 +1,65 @@
+﻿
+namespace _05.Coins
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double sum = double.Parse(Console.ReadLine());
+            int coins = 0;
+
+            while (sum > 0)
+            {
+                sum = Math.Round(sum, 2);
+
+                if (sum >= 3)
+                {
+                    sum -= 2;
+                    coins++;
+                }
+                else if (sum >= 2)
+                {
+                    sum -= 2;
+                    coins++;
+                }
+                else if (sum >= 1)
+                {
+                    sum -= 1;
+                    coins++;
+                }
+                else if (sum >= 0.50)
+                {
+                    sum -= 0.50;
+                    coins++;
+                }
+                else if (sum >= 0.20)
+                {
+                    sum -= 0.20;
+                    coins++;
+                }
+                else if (sum >= 0.10)
+                {
+                    sum -= 0.10;
+                    coins++;
+                }
+                else if (sum >= 0.05)
+                {
+                    sum -= 0.05;
+                    coins++;
+                }
+                else if (sum >= 0.02)
+                {
+                    sum -= 0.02;
+                    coins++;
+                }
+                else if (sum >= 0.01)
+                {
+                    sum -= 0.01;
+                    coins++;
+                }
+            }
+            Console.WriteLine(coins);
+        }
+    }
+}
