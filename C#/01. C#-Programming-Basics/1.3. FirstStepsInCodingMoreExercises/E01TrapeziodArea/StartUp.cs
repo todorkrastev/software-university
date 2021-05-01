c5d27de9c0f3f2db1db7edbaf237e1fa0c55766e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:18:47 +0200

    Create E01TrapeziodArea

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E01TrapeziodArea/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.TrapeziodArea
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double areaOfTrapeziod = (a + b) * h / 2;
+
+            Console.WriteLine($"{areaOfTrapeziod:f2}");
+        }
+    }
+}
