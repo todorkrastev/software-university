86fdb6ff8d3c65b310e73a731c463a1fbb5debe6
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:07:17 +0200

    Create E04TrainTheTrainers

---
+++ C#/01. C#-Programming-Basics/6.2. NestedLoopsExercises/E04TrainTheTrainers/StartUp.cs
@@ -0,0 +1,36 @@
+﻿
+namespace _04.TrainTheTrainers
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfJury = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            double sumOfGrades = 0.0;
+            int countOfGrades = 0;
+            double finalAssesment = 0.0;
+            int sumOfCountOfGrades = 0;
+
+            while (command != "Finish")
+            {
+
+                for (int i = 1; i <= numOfJury; i++)
+                {
+                    double numOfGrades = double.Parse(Console.ReadLine());
+                    sumOfGrades += numOfGrades;
+                    countOfGrades++;
+
+                }
+                Console.WriteLine($"{command} - {(sumOfGrades / countOfGrades):f2}.");
+                sumOfCountOfGrades += countOfGrades;
+                finalAssesment += sumOfGrades;
+                command = Console.ReadLine();
+                countOfGrades = 0;
+                sumOfGrades = 0.0;
+            }
+            Console.WriteLine($"Student's final assessment is {(finalAssesment / sumOfCountOfGrades):f2}.");
+        }
+    }
+}
