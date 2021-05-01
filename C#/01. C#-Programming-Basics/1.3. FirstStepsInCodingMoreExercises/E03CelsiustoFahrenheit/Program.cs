4260f7be96a575736c7f9d2df6320f7fe42c3c90
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:20:57 +0200

    Create E03CelsiustoFahrenheit

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E03CelsiustoFahrenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.CelsiustoFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputCelsius = double.Parse(Console.ReadLine());
+
+            double CelsiusToFahrenheit = inputCelsius * 9 / 5 + 32;
+
+            Console.WriteLine($"{CelsiusToFahrenheit:f2}");
+            
+        }
+    }
+}
