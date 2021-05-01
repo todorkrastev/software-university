54416795754f87791f4b658451d9b6ec92534a13
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 22:13:00 +0200

    Create E04GiftsFromSantaClaus

---
+++ C#/01. C#-Programming-Basics/Exam/E04GiftsFromSantaClaus/StartUp.cs
@@ -0,0 +1,30 @@
+﻿
+namespace _04.GiftsFromSantaClaus
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int S = int.Parse(Console.ReadLine());
+
+            for (int i = M; N >= 1; i--)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    if (i == S)
+                    {
+                        break;
+                    }
+                    Console.Write($"{i} ");
+                }
+                else if (i <= 1)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
