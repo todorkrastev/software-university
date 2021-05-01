53f6a4237d94bc725a1eebbabd9b9faa94db243e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:23:08 +0200

    Create E06Fishland

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E06Fishland/StartUp.cs
@@ -0,0 +1,27 @@
+﻿
+namespace _06.Fishland
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double mackerelPrice = double.Parse(Console.ReadLine());
+            double spratPrice = double.Parse(Console.ReadLine());
+            double bonitoKG = double.Parse(Console.ReadLine());
+            double scadKG = double.Parse(Console.ReadLine());
+            double musselKG = double.Parse(Console.ReadLine());
+
+            double bonitoPrice = mackerelPrice * 1.60;
+            double scadPrice = spratPrice * 1.80;
+            double musselPrice = 7.5;
+
+            double result = bonitoKG * bonitoPrice + scadKG * scadPrice + musselKG * musselPrice;
+
+            Console.WriteLine($"{result:f2}");
+
+
+
+        }
+    }
+}
