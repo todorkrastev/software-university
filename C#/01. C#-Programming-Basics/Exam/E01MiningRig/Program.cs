6f5bdd8841ce63033e6f92a172208c0fd3211652
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 22:01:08 +0200

    Create E01MiningRig

---
+++ C#/01. C#-Programming-Basics/Exam/E01MiningRig/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.MiningRig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int videoCardPrice = int.Parse(Console.ReadLine());
+            int jumperPrice = int.Parse(Console.ReadLine());
+            double electricity = double.Parse(Console.ReadLine());
+            double cardBenefit = double.Parse(Console.ReadLine());
+
+            int videoCard = 13;
+            int jumper = 13;
+            int systemParts = 1000;
+
+            int totalPrice = (videoCardPrice * videoCard) + (jumperPrice * jumper) + systemParts;
+            double totalBenefitCards = (cardBenefit - electricity) * videoCard;
+            double result = totalPrice / totalBenefitCards;
+
+            Console.WriteLine(totalPrice);
+            Console.WriteLine($"{Math.Ceiling(result)}");
+        }
+    }
+}
