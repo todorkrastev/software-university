73262475f14bc378dc9a954f75d58f628a097894
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:19:48 +0200

    Create E02TriangleArea

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E02TriangleArea/StartUp.cs
@@ -0,0 +1,17 @@
+﻿
+namespace _02.TriangleArea
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double areaOfTriangle = a * h / 2;
+
+            Console.WriteLine($"{areaOfTriangle:f2}");
+        }
+    }
+}
