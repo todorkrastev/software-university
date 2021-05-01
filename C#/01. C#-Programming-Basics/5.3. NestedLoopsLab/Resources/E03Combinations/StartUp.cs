286efa36f8187fd30eac3b0d934d9371774518b9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 20:41:15 +0200

    Create E03Combinations

---
+++ C#/01. C#-Programming-Basics/5.3. NestedLoopsLab/Resources/E03Combinations/StartUp.cs
@@ -0,0 +1,29 @@
+﻿
+namespace _03.Combinations
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int a = 0; a <= inputNum; a++)
+            {
+                for (int b = 0; b <= inputNum; b++)
+                {
+                    for (int c = 0; c <= inputNum; c++)
+                    {
+                        int sum = a + b + c;
+                        if (sum == inputNum)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
