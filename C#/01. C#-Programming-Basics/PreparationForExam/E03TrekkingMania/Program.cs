78c085a319570d66b3030ad9fce455fd4135bb75
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:58:32 +0200

    Create PreparationForExam

---
+++ C#/01. C#-Programming-Basics/PreparationForExam/E03TrekkingMania/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _04.TrekkingMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupsNum = int.Parse(Console.ReadLine());
+
+            int musalaCount = 0;
+            int montblanCount = 0;
+            int kiliCount = 0;
+            int k2Count = 0;
+            int everestCount = 0;
+
+
+            for (int i = 1; i <= groupsNum; i++)
+            {
+                int peopleNum = int.Parse(Console.ReadLine());
+
+                if (peopleNum <= 5 && peopleNum >= 1)
+                {
+                    musalaCount += peopleNum;
+                }
+                else if (peopleNum <= 12 && peopleNum >= 6)
+                {
+                    montblanCount += peopleNum;
+                }
+                else if (peopleNum <= 25 && peopleNum >= 13)
+                {
+                    kiliCount += peopleNum;
+                }
+                else if (peopleNum <= 40 && peopleNum >= 26)
+                {
+                    k2Count += peopleNum;
+                }
+                else if (peopleNum <= 1000 && peopleNum >= 41)
+                {
+                    everestCount += peopleNum;
+                }
+            }
+            int totalPeople = musalaCount + montblanCount + everestCount + k2Count + kiliCount;
+            double musalaPercentage = musalaCount * 1.0 / totalPeople * 100;
+            double montblanPercentage = montblanCount * 1.0 / totalPeople * 100;
+            double kiliPercentage = kiliCount * 1.0 / totalPeople * 100;
+            double k2Percentage = k2Count * 1.0 / totalPeople * 100;
+            double everestPercentage = everestCount * 1.0 / totalPeople * 100;
+
+            Console.WriteLine($"{musalaPercentage:f2}%");
+            Console.WriteLine($"{montblanPercentage:f2}%");
+            Console.WriteLine($"{kiliPercentage:f2}%");
+            Console.WriteLine($"{k2Percentage:f2}%");
+            Console.WriteLine($"{everestPercentage:f2}%");
+        }
+    }
+}
---
+++ C#/01. C#-Programming-Basics/PreparationForExam/E06TournamentofChristmas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _06.TournamentofChristmas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string command = "";
+            int winCount = 0;
+            int lostCount = 0;
+            double sum = 0.00;
+            double totalSum = 0.00;
+            int totalWin = 0;
+            int totalLose = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                command = Console.ReadLine();
+                while (command != "Finish")
+                {
+                    string result = Console.ReadLine();
+                    if (result == "win")
+                    {
+                        sum += 20;
+                        winCount++;
+                    }
+                    else if (result == "lose")
+                    {
+                        lostCount++;
+                    }
+                    command = Console.ReadLine();
+                }
+
+                if (lostCount < winCount)
+                {
+                    sum *= 1.10;
+                }
+                totalWin += winCount;
+                totalLose += lostCount;
+                totalSum += sum;
+                winCount = 0;
+                lostCount = 0;
+                sum = 0;
+            }
+            //At the end of the championship, if the wins are more than losts -> increase the money with 20 %;
+            if (totalLose < totalWin)
+            {
+                totalSum *= 1.20;
+                Console.WriteLine($"You won the tournament! Total raised money: {totalSum:f2}");
+            }
+            else if (totalLose > totalWin)
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {totalSum:f2}");
+            }
+        }
+    }
+}
