2085f860f36db136842cc9ae40dc421bcc710c8f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:08:06 +0200

    Create E05PasswordGenerator

---
+++ C#/01. C#-Programming-Basics/6.2. NestedLoopsExercises/E05PasswordGenerator/StartUp.cs
@@ -0,0 +1,33 @@
+﻿
+namespace _05.PasswordGenerator
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (int k = 97; k < 97 + l; k++)
+                    {
+                        for (int m = 97; m < 97 + l; m++)
+                        {
+                            for (int o = 1; o <= n; o++)
+                            {
+                                if (o > i && o > j)
+                                {
+                                    Console.Write($"{i}{j}{Convert.ToChar(k)}{Convert.ToChar(m)}{o} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
