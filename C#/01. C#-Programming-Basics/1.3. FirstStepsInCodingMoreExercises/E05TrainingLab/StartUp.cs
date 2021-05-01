73a49c467de64d45ca9b8a6afd2de4296586a6ee
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:22:19 +0200

    Create E05TrainingLab

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E05TrainingLab/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.TrainingLab
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            width = 100 * width;
+            height = 100 * height - 100;
+
+
+            int countWidth = 0;
+            int countHeight = 0;
+
+            while (120 <= width)
+            {
+                width -= 120;
+                countWidth++;
+            }
+            while (70 <= height)
+            {
+                height -= 70;
+                countHeight++;
+            }
+
+            int result = countHeight * countWidth - 3;
+            Console.WriteLine(result);
+        }
+    }
+}
