173c4091459aad9fcaaa7a71459d4eb9513cabd8
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:48:24 +0200

    Create E01ChangeBureau

---
+++ C#/01. C#-Programming-Basics/PreparationForExam/E01ChangeBureau/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ChangeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitcoinNum = int.Parse(Console.ReadLine());
+            double chineseNum = double.Parse(Console.ReadLine());
+            double commission = double.Parse(Console.ReadLine());
+
+            double bitcoin = 1168;
+            double chinese = 0.15;
+            double dollar = 1.76;
+            double euro = 1.95;
+
+            double converter = (((chineseNum * chinese * dollar) + (bitcoin * bitcoinNum)) / euro);
+            double result = converter - (converter * commission / 100);
+
+
+            Console.WriteLine($"{result:f2}");
+
+        }
+    }
+}
