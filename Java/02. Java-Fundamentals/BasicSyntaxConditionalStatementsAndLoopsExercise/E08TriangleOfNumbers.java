7d8b35be9940543fbab78d1363d298dde7c242ff
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 23:03:53 +0200

    Create E08TriangleOfNumbers.java

---
+++ Java/02. Java-Fundamentals/BasicSyntaxConditionalStatementsAndLoopsExercise/E08TriangleOfNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class E08TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= num; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.printf("%d ", i);
+            }
+            System.out.println();
+        }
+    }
+}
