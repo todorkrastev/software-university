fa66928a042e0b8ac8170f4a6905248a0afcb898
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:24:47 +0200

    Create E08CircleAreaandPerimeter

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E08CircleAreaandPerimeter/StartUp.cs
@@ -0,0 +1,20 @@
+﻿
+namespace _08.CircleAreaandPerimeter
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            double areaOfCircuit = Math.PI * input * input;
+            double perimeterOfCircuit = 2 * Math.PI * input;
+
+            Console.WriteLine($"{areaOfCircuit:f2}");
+            Console.WriteLine($"{perimeterOfCircuit:f2}");
+
+
+        }
+    }
+}
