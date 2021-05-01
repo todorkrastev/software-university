dde2bb5200058dc06dee68da09e6de8c901fd2f2
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:26:32 +0200

    Create E10WeatherForecastPart2

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E10WeatherForecastPart2/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10.WeatherForecastPart2
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            if (26 <= input && input <= 35)
+            {
+                Console.WriteLine("Hot");
+            }
+            else if (20.1 <= input && input <= 25.9)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if (15 <= input && input <= 20)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if (12 <= input && input <= 14.9)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if (5 <= input && input == 11.9)
+            {
+                Console.WriteLine("Cold");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
