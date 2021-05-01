6a7414203a2ad325d61f0073634fd81c0ffdf05a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 20:42:33 +0200

    Create E05Travelling

---
+++ C#/01. C#-Programming-Basics/5.3. NestedLoopsLab/Resources/E05Travelling/StartUp.cs
@@ -0,0 +1,38 @@
+﻿
+namespace _05.Travelling
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            double savedMoney = 0.0;
+            double sum = 0.0;
+
+            while (command != "End")
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                {
+                    break;
+                }
+                double budget = double.Parse(Console.ReadLine());
+             
+
+                while (sum < budget)
+                {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    sum += savedMoney;
+                }
+
+                if (budget <= sum)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                    sum = 0;
+                    continue;
+                }
+            }
+        }
+    }
+}
