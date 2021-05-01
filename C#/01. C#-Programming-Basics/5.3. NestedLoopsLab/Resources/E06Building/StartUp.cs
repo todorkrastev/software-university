54a15f3c06bd619880c0a4ef03a0b552fe98d9fb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 20:43:28 +0200

    Create E06Building

---
+++ C#/01. C#-Programming-Basics/5.3. NestedLoopsLab/Resources/E06Building/StartUp.cs
@@ -0,0 +1,36 @@
+﻿
+namespace _06.Building
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numOfFloors = int.Parse(Console.ReadLine());
+            int numOfRooms = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfRooms; i++)
+            {
+                Console.Write($"L{numOfFloors}{i} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = numOfFloors - 1; i > 0; i--)
+            {
+                for (int j = 0; j < numOfRooms; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else if (i % 2 == 1)
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
