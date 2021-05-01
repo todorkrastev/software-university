83a71b2c7053be5ec4c46ee416b8bcb948061189
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:04:14 +0200

    Create E02EqualSumsEvenOddPosition

---
+++ C#/01. C#-Programming-Basics/6.2. NestedLoopsExercises/E02EqualSumsEvenOddPosition/StartUp.cs
@@ -0,0 +1,41 @@
+﻿
+namespace _02.EqualSumsEvenOddPosition
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numFirst = int.Parse(Console.ReadLine());
+            int numSecond = int.Parse(Console.ReadLine());
+
+            for (int i = numFirst; i <= numSecond; i++)
+            {
+                int currNum = i;
+                int evenSum = 0;
+                int oddSum = 0;
+                int count = 0;
+
+                while (currNum != 0)
+                {
+                    int digit = currNum % 10;
+                    if (count % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+
+                    currNum = currNum / 10;
+                    count++;
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
