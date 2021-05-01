262c24bb5c581d202812869dd994b8b5572349ed
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 20:39:02 +0200

    Create E01Clock

---
+++ C#/01. C#-Programming-Basics/5.3. NestedLoopsLab/Resources/E01Clock/StartUp.cs
@@ -0,0 +1,18 @@
+﻿
+namespace _01.Clock
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h <= 23; h++)
+            {
+                for (int m = 0; m <= 59; m++)
+                {
+                    Console.WriteLine($"{h}:{m}");
+                }
+            }
+        }
+    }
+}
