037e629ef7f52a3bebac9664036c9456008dcd5e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 20:41:56 +0200

    E04SumofTwoNumbers

---
+++ C#/01. C#-Programming-Basics/5.3. NestedLoopsLab/Resources/E04SumofTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.SumofTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int combinationNum = 0;
+            
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    int sum = i + j;
+                    combinationNum++;
+                    if (sum == magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{combinationNum} ({i} + {j} = {magicNum})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{combinationNum} combinations - neither equals {magicNum}");
+        }
+    }
+}
