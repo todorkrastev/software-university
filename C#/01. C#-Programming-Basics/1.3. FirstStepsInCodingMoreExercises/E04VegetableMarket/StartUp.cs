a6810bfa5c611a6736e9894d733c5844d61b069d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:21:39 +0200

    Create E04VegetableMarket

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E04VegetableMarket/StartUp.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _04.VegetableMarket
+{
+using System;
+    using System.Transactions;
+
+    class StartUP
+    {
+        static void Main(string[] args)
+        {
+            double priceVeg = double.Parse(Console.ReadLine());
+            double priceFruit = double.Parse(Console.ReadLine());
+
+            int vegTotalKg = int.Parse(Console.ReadLine());
+            int fruitTotalKg = int.Parse(Console.ReadLine());
+
+            double totalSum = priceVeg * vegTotalKg + priceFruit * fruitTotalKg;
+            double BGNtoEUR = totalSum / 1.94;
+
+            Console.WriteLine($"{BGNtoEUR:f2}");
+        }
+    }
+}
