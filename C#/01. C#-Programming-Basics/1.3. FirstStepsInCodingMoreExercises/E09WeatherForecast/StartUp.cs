cc4d4fd1882d964ed2f09d4bbb0813b2549604a6
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:25:57 +0200

    Create E09WeatherForecast

---
+++ C#/01. C#-Programming-Basics/1.3. FirstStepsInCodingMoreExercises/E09WeatherForecast/StartUp.cs
@@ -0,0 +1,21 @@
+﻿
+namespace _09.WeatherForecast
+{
+using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "sunny")
+            {
+                Console.WriteLine($"It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine($"It's cold outside!");
+            }
+        }
+    }
+}
