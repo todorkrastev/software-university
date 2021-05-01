76f170319285054d46b1e8bb35582c403f4e9aa8
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 22:45:15 +0200

    Create E13RefactorSumOfOddNumbers.java

---
+++ Java/02. Java-Fundamentals/BasicSyntaxConditionalStatementsAndLoopsLab/E13RefactorSumOfOddNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class E13RefactorSumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 1 && count < input) {
+                System.out.println(i);
+                sum += i;
+                count++;
+            }
+        }
+        System.out.printf("Sum: %d", sum);
+    }
+}
