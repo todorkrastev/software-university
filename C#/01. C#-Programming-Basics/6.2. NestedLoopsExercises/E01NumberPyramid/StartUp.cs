75aec71105f75e1b83054d7ed8ad9c1b9b676ab9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-01 21:03:25 +0200

    Create E01NumberPyramid

---
+++ C#/01. C#-Programming-Basics/6.2. NestedLoopsExercises/E01NumberPyramid/StartUp.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _01.NumberPyramid
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numberForPrint = 1;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int numbersPerRow = 1; numbersPerRow <= row; numbersPerRow++)
+                {
+                    Console.Write($"{numberForPrint} ");
+                    numberForPrint++;
+                    if (numberForPrint > n)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                if (numberForPrint > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
