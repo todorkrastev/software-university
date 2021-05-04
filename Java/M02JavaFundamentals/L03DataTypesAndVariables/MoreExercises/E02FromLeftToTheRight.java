b2a85ab71f29662affde2e090e61a02e8ceb170f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-04 18:51:00 +0200

    Create E02FromLeftToTheRight.java

---
+++ Java/M02JavaFundamentals/L03DataTypesAndVariables/MoreExercises/E02FromLeftToTheRight.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class E02FromLeftToTheRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
