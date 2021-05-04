7b186871d6edb93c59c0be2a3dbc59a4f8aa1f94
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-04 19:59:23 +0200

    Create Song.java

---
+++ Java/M02JavaFundamentals/L06ObjectsAndClasses/Lab/Song.java
@@ -0,0 +1,33 @@
+
+public class Song {
+    private String typeList;
+    private String name;
+    private String time;
+
+
+    public String getTypeList() {
+        return typeList;
+    }
+
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
+
+
