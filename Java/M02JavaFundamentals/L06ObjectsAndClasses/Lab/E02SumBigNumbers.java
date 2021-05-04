213e6aa20209a0b86e4683d0579a3e409527ea03
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-04 19:58:16 +0200

    Create E02SumBigNumbers.java

---
+++ Java/M02JavaFundamentals/L06ObjectsAndClasses/Lab/E02SumBigNumbers.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class E02SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger firstNum = new BigInteger(scanner.nextLine());
+        BigInteger secondNum = new BigInteger(scanner.nextLine());
+
+        BigInteger sum = firstNum.add(secondNum);
+
+        System.out.println(sum);
+    }
+}
