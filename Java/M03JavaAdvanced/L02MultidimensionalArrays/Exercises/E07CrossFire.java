1ae31ab35e6a81c15ca24f2b8a774638a5565029
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-09 17:28:04 +0200

    Create E07CrossFire.java

---
+++ Java/M03JavaAdvanced/L02MultidimensionalArrays/Exercises/E07CrossFire.java
@@ -0,0 +1,68 @@
+package bg.softuni.java_advanced.multidimensional_arrays.exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class E07CrossFire {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+        int[] rowsAndCols = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int rows = rowsAndCols[0];
+        int cols = rowsAndCols[1];
+
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        int count = 1;
+
+        for (int row = 0; row < rows; row++) {
+            matrix.add(new ArrayList<>());
+            for (int col = 0; col < cols; col++) {
+                matrix.get(row).add(count++);
+            }
+        }
+
+        String line = reader.readLine();
+
+        while (!line.equals("Nuke it from orbit")) {
+            String[] data = line.trim().split("\\s+");
+
+            int row = Integer.parseInt(data[0]);
+            int col = Integer.parseInt(data[1]);
+            int radius = Integer.parseInt(data[2]);
+
+            for (int i = row - radius; i <= row + radius; i++) {
+                if (isInRange(i, col, matrix) && i != row) {
+                    matrix.get(i).remove(col);
+                }
+            }
+
+            for (int i = col + radius; i >= col - radius; i--) {
+                if (isInRange(row, i, matrix)) {
+                    matrix.get(row).remove(i);
+                }
+            }
+
+            matrix.removeIf(List::isEmpty);
+            line = reader.readLine();
+        }
+
+        for (List<Integer> integers : matrix) {
+            for (Integer integer : integers) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isInRange(int row, int col, List<List<Integer>> matrix) {
+        return 0 <= row && row < matrix.size() && 0 <= col && col < matrix.get(row).size();
+    }
+}
