cac80e3d8ec701c7031269a8c55462f4b192faba
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-12 00:44:02 +0200

    Create E02Nums1to10.java

---
+++ Java/M01JavaProgrammingBasics/L01FirstStepsInCoding/Lab/E02Nums1to10.java
@@ -0,0 +1,9 @@
+package bg.softuni.programming_basics.first_steps_in_coding.lab;
+
+public class E02Nums1to10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
