cb9e0e19ad37affef63d0b744f5746e006bb8e81
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-12 00:43:56 +0200

    Create E01HelloSoftuni.java

---
+++ Java/M01JavaProgrammingBasics/L01FirstStepsInCoding/Lab/E01HelloSoftuni.java
@@ -0,0 +1,9 @@
+package bg.softuni.programming_basics.first_steps_in_coding.lab;
+
+import java.util.Scanner;
+
+public class E01HelloSoftuni {
+    public static void main(String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+}
