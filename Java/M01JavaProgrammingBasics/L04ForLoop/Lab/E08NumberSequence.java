e9fb50502b822c94494f32a10536fdd7652bd696
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-13 22:35:09 +0200

    Create E08NumberSequence.java

---
+++ Java/M01JavaProgrammingBasics/L04ForLoop/Lab/E08NumberSequence.java
@@ -0,0 +1,26 @@
+package bg.softuni.programming_basics.for_loop.lab;
+
+import java.util.Scanner;
+
+public class E08NumberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+        }
+        System.out.printf("Max number: %d%n", max);
+        System.out.printf("Min number: %d", min);
+    }
+}
