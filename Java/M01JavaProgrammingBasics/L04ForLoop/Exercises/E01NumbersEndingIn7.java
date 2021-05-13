0933ab07dfd14b09ab6912b447455300c6e33063
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-13 22:34:23 +0200

    Create E01NumbersEndingIn7.java

---
+++ Java/M01JavaProgrammingBasics/L04ForLoop/Exercises/E01NumbersEndingIn7.java
@@ -0,0 +1,10 @@
+package bg.softuni.programming_basics.for_loop.exercises;
+
+public class E01NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
