306ce3c5d9e90954b2e7c675ea6587f65ff1b324
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-13 22:34:44 +0200

    Create E01NumbersFrom1To100.java

---
+++ Java/M01JavaProgrammingBasics/L04ForLoop/Lab/E01NumbersFrom1To100.java
@@ -0,0 +1,10 @@
+package bg.softuni.programming_basics.for_loop.lab;
+
+public class E01NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int i =1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
