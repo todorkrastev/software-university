d7f3a1c727969614e65262404447aa6b814477e4
Todor Krastev <krastevtodordinev@yahoo.com>
2021-05-13 22:39:12 +0200

    Create E07CinemaTickets.java

---
+++ Java/M01JavaProgrammingBasics/L06NestedLoops/Lab/E07CinemaTickets.java
@@ -0,0 +1,39 @@
+package bg.softuni.programming_basics.nested_loops.lab;
+
+import java.util.Scanner;
+
+public class E07CinemaTickets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int student = 0;
+        int standard = 0;
+        int kid = 0;
+        int tickets = 0;
+        String movie = scanner.nextLine();
+
+        while (!movie.equals("Finish")) {
+            int places = Integer.parseInt(scanner.nextLine());
+            for (int i = places; i >= 1; i--) {
+                String text = scanner.nextLine();
+                if ("student".equals(text)) {
+                    student++;
+                } else if ("standard".equals(text)) {
+                    standard++;
+                } else if ("kid".equals(text)) {
+                    kid++;
+                } else {
+                    break;
+                }
+                tickets++;
+            }
+            System.out.printf("%s - %.2f%% full.%n", movie, (1.0 * tickets / places * 100));
+            tickets = 0;
+            movie = scanner.nextLine();
+        }
+        System.out.printf("Total tickets: %d%n", student + standard + kid);
+        System.out.printf("%.2f%% student tickets.%n", (1.0 * student / (student + standard + kid) * 100));
+        System.out.printf("%.2f%% standard tickets.%n", (1.0 * standard / (student + standard + kid) * 100));
+        System.out.printf("%.2f%% kids tickets.", (1.0 * kid / (student + standard + kid) * 100));
+    }
+}
