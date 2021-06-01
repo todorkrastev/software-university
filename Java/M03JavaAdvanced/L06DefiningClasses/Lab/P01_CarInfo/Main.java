4be8f47dd46d515f6852d3492f14ffec4f9751ca
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-01 16:29:23 +0200

    Update Main.java

--- Java/M03JavaAdvanced/L06DefiningClasses/Lab/P01_CarInfo/Main.java
+++ Java/M03JavaAdvanced/L06DefiningClasses/Lab/P01_CarInfo/Main.java
@@ -1,27 +1,33 @@
 package bg.softuni.java_advanced.defining_classes.lab.P01_CarInfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
-        final int numOfCars = Integer.parseInt(scanner.nextLine());
+        final int numOfCars = Integer.parseInt(reader.readLine());
 
         List<Car> carInfo = new ArrayList<>();
 
         for (int i = 0; i < numOfCars; i++) {
-            String[] input = scanner.nextLine().trim().split("\\s+");
+            String[] input = reader.readLine().trim().split("\\s+");
             String brand = input[0];
             String model = input[1];
             int horsePower = Integer.parseInt(input[2]);
 
-            Car data = new Car(brand, model, horsePower);
-            carInfo.add(data);
+            Car car = new Car(brand, model, horsePower);
+            carInfo.add(car);
         }
+        Consumer<Car> printer = System.out::println;
+
         carInfo
-                .forEach(System.out::println);
+                .forEach(printer);
     }
 }
