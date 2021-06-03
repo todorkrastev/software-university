7d39a865ce2b49feec70acdc954cbf9222aeeee7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-03 15:11:32 +0200

    Rename Folder

--- Python/M01PythonProgrammingBasics/L01FirstStepsInCoding/Lab/E02Nums1...10.py
+++
@@ -1,2 +0,0 @@
-for x in range(1, 11):
-    print(x)
--- Python/M01PythonProgrammingBasics/L01FirstStepsInCoding/Lab/E07ProjectsCreation.py
+++
@@ -1,8 +0,0 @@
-name = input()
-num_of_projects = int(input())
-
-
-time_needed = num_of_projects * 3
-
-print(f'The architect {name} will need {time_needed} hours to complete {num_of_projects} project/s.')
-
