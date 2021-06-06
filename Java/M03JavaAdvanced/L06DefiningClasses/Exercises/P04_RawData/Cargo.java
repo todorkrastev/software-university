d9ec92a2e1180f94a199897881d5adc24a960729
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-06 21:11:25 +0200

    Create Cargo.java

---
+++ Java/M03JavaAdvanced/L06DefiningClasses/Exercises/P04_RawData/Cargo.java
@@ -0,0 +1,15 @@
+package bg.softuni.java_advanced.defining_classes.exercises.P04_RawData;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+
+    public String getCargoType() {
+        return this.cargoType;
+    }
+}
