19443019afc93b31c33352aeb56cb77aa8e0e4b8
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-06 15:40:34 +0200

    Create Employee.java

---
+++ Java/M03JavaAdvanced/L06DefiningClasses/Exercises/P02_CompanyRosterSecondOption/Employee.java
@@ -0,0 +1,28 @@
+package bg.softuni.java_advanced.defining_classes.exercises.P02_CompanyRosterSecondOption;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private String position;
+    private String department;
+    private String email;
+    private int age;
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %s %d", this.name, this.salary, this.email, this.age);
+    }
+}
