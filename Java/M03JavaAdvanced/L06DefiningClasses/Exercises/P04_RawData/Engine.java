5b4659627986cf0dd079ff81a896a0ce5d7914a4
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-06 21:11:28 +0200

    Create Engine.java

---
+++ Java/M03JavaAdvanced/L06DefiningClasses/Exercises/P04_RawData/Engine.java
@@ -0,0 +1,15 @@
+package bg.softuni.java_advanced.defining_classes.exercises.P04_RawData;
+
+public class Engine {
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int engineSpeed, int enginePower) {
+        this.engineSpeed = engineSpeed;
+        this.enginePower = enginePower;
+    }
+
+    public int getEnginePower() {
+        return this.enginePower;
+    }
+}
