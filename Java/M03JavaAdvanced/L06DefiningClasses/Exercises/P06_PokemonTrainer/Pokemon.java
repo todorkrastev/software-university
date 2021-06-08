7ef2d99dcbdaa17406027a737f9c12cce531b54d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-08 09:41:27 +0200

    Create Pokemon.java

---
+++ Java/M03JavaAdvanced/L06DefiningClasses/Exercises/P06_PokemonTrainer/Pokemon.java
@@ -0,0 +1,29 @@
+package bg.softuni.java_advanced.defining_classes.exercises.P06_PokemonTrainer;
+
+public class Pokemon {
+    private final String name;
+    private final String element;
+    private int health;
+
+    public Pokemon(String name, String element, int health) {
+        this.name = name;
+        this.element = element;
+        this.health = health;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
