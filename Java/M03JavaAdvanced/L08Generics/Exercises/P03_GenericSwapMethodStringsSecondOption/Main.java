bfbce9ce3fe2b5e10206bfdf905a69659a5c155f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-12 19:12:19 +0200

    Create Main.java

---
+++ Java/M03JavaAdvanced/L08Generics/Exercises/P03_GenericSwapMethodStringsSecondOption/Main.java
@@ -0,0 +1,30 @@
+package bg.softuni.java_advanced.generics.exercises.P03_GenericSwapMethodStringsSecondOption;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+        int numOfLines = Integer.parseInt(reader.readLine());
+
+        Box<String> stringBox = new Box<>();
+
+        for (int i = 0; i < numOfLines; i++) {
+            String input = reader.readLine();
+
+            stringBox.add(input);
+        }
+        String[] tokens = reader.readLine().trim().split("\\s+");
+        int firstIndex = Integer.parseInt(tokens[0]);
+        int secondIndex = Integer.parseInt(tokens[1]);
+
+        stringBox.swap(firstIndex, secondIndex);
+
+        System.out.println(stringBox);
+    }
+}
