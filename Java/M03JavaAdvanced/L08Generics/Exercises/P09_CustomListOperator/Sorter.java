9ae2d9c808f8756328b63cc4cd9d5e24669941a7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-13 02:49:51 +0200

    Create Sorter.java

---
+++ Java/M03JavaAdvanced/L08Generics/Exercises/P09_CustomListOperator/Sorter.java
@@ -0,0 +1,19 @@
+package bg.softuni.java_advanced.generics.exercises.P09_CustomListOperator;
+
+public class Sorter {
+
+    public Sorter() {
+    }
+
+    public static <T extends Comparable<T>> void sort(Box<T> box) {
+        for (int i = 0; i < box.size(); i++) {
+            T element = box.get(i);
+            for (int j = i + 1; j < box.size(); j++) {
+                T nextElement = box.get(j);
+                if (element.compareTo(nextElement) > 0) {
+                    box.swap(i, j);
+                }
+            }
+        }
+    }
+}
