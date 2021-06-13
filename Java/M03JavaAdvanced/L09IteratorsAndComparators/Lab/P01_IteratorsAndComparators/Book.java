f294d0cfc3f69365f329d30134fc63e942c475ed
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-13 12:39:07 +0200

    Create Book.java

---
+++ Java/M03JavaAdvanced/L09IteratorsAndComparators/Lab/P01_IteratorsAndComparators/Book.java
@@ -0,0 +1,40 @@
+package bg.softuni.java_advanced.iterators_and_comparators.lab.P01_IteratorsAndComparators;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... args) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(args);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+}
