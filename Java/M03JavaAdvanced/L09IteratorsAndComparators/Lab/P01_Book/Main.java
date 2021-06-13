7b17ab7eb5ab8f0f10cd2e80345525dd0e00d5b8
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-13 14:26:39 +0200

    Create Main.java

---
+++ Java/M03JavaAdvanced/L09IteratorsAndComparators/Lab/P01_Book/Main.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_advanced.iterators_and_comparators.lab.P01_Book;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("War and Peace", 1865, "Leo Tolstoy");
+
+        System.out.printf("%s %d %s", book.getTitle(), book.getYear(), book.getAuthors().get(0));
+    }
+}
