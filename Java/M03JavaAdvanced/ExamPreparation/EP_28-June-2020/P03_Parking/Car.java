e0926dd417b5a7a4820d2626fb891f142a70ace9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-15 16:00:04 +0200

    Create Car.java

---
+++ Java/M03JavaAdvanced/ExamPreparation/EP_28-June-2020/P03_Parking/Car.java
@@ -0,0 +1,42 @@
+package bg.softuni.java_advanced.preparation_exam_28_June_2020.P03_Parking;
+
+public class Car {
+    private String manufacturer;
+    private String model;
+    private int year;
+
+    public Car(String manufacturer, String model, int year) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s (%d)",getManufacturer(),getModel(),getYear());
+    }
+}
