a36ace3ba0769261d84fafd35066971280c01829
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-19 01:39:13 +0200

    Create Rabbit.java

---
+++ Java/M03JavaAdvanced/ExamPreparation/EP_26-October-2019/P03_Rabbits/Rabbit.java
@@ -0,0 +1,34 @@
+package bg.softuni.java_advanced.exam_preparation_26_October_2019.P03_Rabbits;
+
+public class Rabbit {
+    private String name;
+    private String species;
+    private boolean available;
+
+    public Rabbit(String name, String species) {
+        this.name = name;
+        this.species = species;
+        this.available = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Rabbit (%s): %s", getSpecies(), getName());
+    }
+}
