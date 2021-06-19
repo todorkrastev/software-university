ee38dc1df0f985b65e5c999d8baad94689d8a3a5
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-19 20:09:23 +0200

    Create Player.java

---
+++ Java/M03JavaAdvanced/ExamPreparation/EP_22-Feb-2020/P03_Guild/Player.java
@@ -0,0 +1,52 @@
+package bg.softuni.java_advanced.exam_preparation_22_February_2020.P03_Guild;
+
+public class Player {
+    private String name;
+    private String clazz;
+    private String rank;
+    private String description;
+
+    public Player(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+        this.rank = "Trial";
+        this.description = "n/a";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Player %s: %s%nRank: %s%nDescription: %s", getName(), getClazz(), getRank(), getDescription());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
