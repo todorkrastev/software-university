14296a1e0b5ea9aa7a5aa8740e696b0866dd5e4e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-20 11:57:16 +0200

    Create Clinic.java

---
+++ Java/M03JavaAdvanced/ExamPreparation/EP_19-August-2020/P03_VetClinic/Clinic.java
@@ -0,0 +1,59 @@
+package bg.softuni.java_advanced.exam_preparation_19_August_2020.P03_VetClinic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clinic {
+    private List<Pet> data;
+    private int capacity;
+
+    public Clinic(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet) {
+        if (data.size() < capacity) {
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        return this.data.removeIf(pet -> pet.getName().equals(name));
+    }
+
+    public Pet getPet(String name, String owner) {
+        return data.stream().filter(e -> e.getName().equals(name) && e.getOwner().equals(owner)).findFirst().orElse(null);
+    }
+
+    public Pet getOldestPet() {
+        return this.data.stream().min((p1, p2) -> Integer.compare(p2.getAge(), p1.getAge())).orElse(null);
+    }
+
+    public int getCount() {
+        return this.data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder output = new StringBuilder("The clinic has the following patients:");
+        output.append(System.lineSeparator());
+        data.forEach(e -> output.append(e.getName()).append(" ").append(e.getOwner()).append(System.lineSeparator()));
+        return output.toString();
+    }
+
+    public void setData(List<Pet> data) {
+        this.data = data;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Pet> getData() {
+        return data;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
