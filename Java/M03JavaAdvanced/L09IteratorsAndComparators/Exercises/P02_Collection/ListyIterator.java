53b9e993200392f98068698ffbeb5fde23b3ca85
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-23 23:56:58 +0200

    Create ListyIterator.java

---
+++ Java/M03JavaAdvanced/L09IteratorsAndComparators/Exercises/P02_Collection/ListyIterator.java
@@ -0,0 +1,56 @@
+package P02_Collection;
+
+import java.util.List;
+
+public class ListyIterator {
+    private int index;
+    private final List<String> data;
+
+    public ListyIterator(List<String> data) {
+        this.data = data;
+    }
+
+    public void printAll() {
+        validatePrint();
+        this.data.forEach(e -> System.out.printf("%s ", e));
+        System.out.println();
+    }
+
+
+    public void print() {
+        validatePrint();
+        System.out.println(this.data.get(index));
+    }
+
+    private void validatePrint() {
+        if (this.data.isEmpty()) {
+            throw new IllegalStateException("Invalid Operation!");
+        }
+    }
+
+    public boolean move() {
+        if (iterator().hasNext()) {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return index < data.size() - 1;
+            }
+
+            @Override
+            public String next() {
+                String element = data.get(index);
+                index++;
+                return element;
+            }
+        };
+    }
+}
+
