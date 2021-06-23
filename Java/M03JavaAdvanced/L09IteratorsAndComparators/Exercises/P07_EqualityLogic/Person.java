cccb4592bfe7ac0473143ec95f38796c4a700f5e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-24 01:06:55 +0200

    Create Person.java

---
+++ Java/M03JavaAdvanced/L09IteratorsAndComparators/Exercises/P07_EqualityLogic/Person.java
@@ -0,0 +1,27 @@
+package bg.softuni.java_advanced.iterators_and_comparators.exercises.P07_EqualityLogic;
+
+import java.util.Objects;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+}
