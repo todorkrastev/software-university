c5195efc7f180de037cde6d8c5ffc183e7958d61
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-24 01:12:11 +0200

    Create LinkedList.java

---
+++ Java/M03JavaAdvanced/L09IteratorsAndComparators/Exercises/P09_LinkedListTraversal/LinkedList.java
@@ -0,0 +1,42 @@
+package bg.softuni.java_advanced.iterators_and_comparators.exercises.P09_LinkedListTraversal;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class LinkedList implements Iterable<Integer> {
+    List<Integer> list;
+
+    public LinkedList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(Integer data) {
+        list.add(data);
+    }
+
+    public boolean remove(Integer data) {
+        return this.list.remove(data);
+    }
+
+    public int getSize() {
+        return this.list.size();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < list.size();
+            }
+
+            @Override
+            public Integer next() {
+                return list.get(index++);
+            }
+        };
+    }
+}
