0ccb7bc8e29d89b068d1eca3ed98e2b131001b1a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-06-26 23:36:55 +0300

    Create Pet.java

---
+++ Java/M03JavaAdvanced/Exam/P03_GroomingSalon/Pet.java
@@ -0,0 +1,46 @@
+package P03_GroomingSalon;
+
+public class Pet {
+    //•	name: String
+    private String name;
+    //•	age: int
+    private int age;
+    //•	owner: String
+    private String owner;
+
+    public Pet(String name, int age, String owner) {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        //"{name} {age} - ({owner})"
+        return String.format("%s %d - (%s)", this.getName(), this.getAge(), this.getOwner());
+    }
+}
