f4a8232cacb155f0041fbdc8703f969b832df717
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-01 20:20:03 +0200

    Create CardRanks.java

---
+++ Java/M04JavaOOP/L01WorkingWithAbstraction/Exercises/P02_CardRank/CardRanks.java
@@ -0,0 +1,17 @@
+package bg.softuni.java_oop.working_with_abstraction.exercises.P02_CardRank;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
