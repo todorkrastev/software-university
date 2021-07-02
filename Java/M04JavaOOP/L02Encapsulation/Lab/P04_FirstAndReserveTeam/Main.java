56a3be596dd3ae1a87cd98e38f90d46489c3df3f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-02 23:18:46 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L02Encapsulation/Lab/P04_FirstAndReserveTeam/Main.java
@@ -0,0 +1,20 @@
+package bg.softuni.java_oop.encapsulation.lab.P04_FirstAndReserveTeam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+        Team team = new Team("Black Eagles");
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            team.addPlayer(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
+        }
+        System.out.printf("First team have %d players%n", team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players%n", team.getReserveTeam().size());
+    }
+}
