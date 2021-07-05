77969c01f4d8463d02c71ca67f4f045470e15037
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:04:17 +0200

    Create Person.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P01_Person/Person.java
@@ -0,0 +1,19 @@
+package bg.softuni.java_oop.inheritance.exercises.P01_Person;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
