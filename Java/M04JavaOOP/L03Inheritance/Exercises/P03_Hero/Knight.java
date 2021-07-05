589cb33247b4ea87f02a9c9966c2cc1f55ab4c1d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:55 +0200

    Create Knight.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/Knight.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class Knight extends Hero {
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
