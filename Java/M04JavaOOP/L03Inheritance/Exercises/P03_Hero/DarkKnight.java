ddfb78d6d7371b07c0042a192ff23a2b27aac72c
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:34 +0200

    Create DarkKnight.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/DarkKnight.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class DarkKnight extends Knight {
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
