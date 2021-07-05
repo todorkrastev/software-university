fee6239a1c19c1ce0cb15be6b57278fe16d19dff
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-05 20:12:30 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L03Inheritance/Lab/P02_MultipleInheritance/Main.java
@@ -0,0 +1,10 @@
+package bg.softuni.java_oop.inheritance.lab.P02_MultipleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}
