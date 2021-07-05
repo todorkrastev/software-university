579d181ef35031073db0537b9b598fb77add193a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-05 20:12:39 +0200

    Create Puppy.java

---
+++ Java/M04JavaOOP/L03Inheritance/Lab/P02_MultipleInheritance/Puppy.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.lab.P02_MultipleInheritance;
+
+public class Puppy extends Dog {
+    public void weep() {
+        System.out.println("weeping...");
+    }
+}
