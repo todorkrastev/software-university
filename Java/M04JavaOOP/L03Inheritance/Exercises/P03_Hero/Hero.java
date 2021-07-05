00c363e9a8f7fa7381548ff8ba2a2c83e88d445e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:45 +0200

    Create Hero.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/Hero.java
@@ -0,0 +1,27 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class Hero {
+    private String username;
+    private int level;
+
+    public Hero(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s Username: %s Level: %s",
+                this.getClass().getName(),
+                this.getUsername(),
+                this.getLevel());
+    }
+}
