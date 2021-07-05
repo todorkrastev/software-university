a0893bc3da25957759ebcc23b50e5cc5ac17e4b7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:42 +0200

    Create Elf.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/Elf.java
@@ -0,0 +1,8 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class Elf extends Hero {
+
+    public Elf(String username, int level) {
+        super(username, level);
+    }
+}
