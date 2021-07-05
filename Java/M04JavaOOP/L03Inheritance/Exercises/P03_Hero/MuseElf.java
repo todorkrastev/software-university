670bde31840ae60ca5855759398a0b24e4d16243
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:41:02 +0200

    Create MuseElf.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/MuseElf.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
