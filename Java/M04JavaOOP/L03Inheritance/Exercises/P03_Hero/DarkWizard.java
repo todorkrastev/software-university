8dd57fb1f5f4ce11d695934ec61739fff313de24
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:37 +0200

    Create DarkWizard.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/DarkWizard.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
