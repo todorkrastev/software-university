92cbf60fab6a3ff07e1f2ed1ec10174b61a95c06
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:40:29 +0200

    Create BladeKnight.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/BladeKnight.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class BladeKnight extends DarkKnight {
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
