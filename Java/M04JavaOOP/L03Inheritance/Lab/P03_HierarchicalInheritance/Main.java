5fa1987d47d4747bc5544978ddcd367d236705cc
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-05 20:22:26 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L03Inheritance/Lab/P03_HierarchicalInheritance/Main.java
@@ -0,0 +1,13 @@
+package bg.softuni.java_oop.inheritance.lab.P03_HierarchicalInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
