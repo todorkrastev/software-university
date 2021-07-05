8a662d25ba6ed2ca2ca68fb7910a18af5b76fb36
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-05 20:06:19 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L03Inheritance/Lab/P01_SingleInheritance/Main.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_oop.inheritance.lab.P01_SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
