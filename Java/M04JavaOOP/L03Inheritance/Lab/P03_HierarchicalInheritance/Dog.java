b81cb5080d356132a7881d0bedd0f653b76ec783
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-05 20:22:23 +0200

    Create Dog.java

---
+++ Java/M04JavaOOP/L03Inheritance/Lab/P03_HierarchicalInheritance/Dog.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.lab.P03_HierarchicalInheritance;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
