7a6190a1202dead88bdfa66b271edbd673cb8eff
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:41:11 +0200

    Create Wizard.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/Wizard.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class Wizard extends Hero {
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
