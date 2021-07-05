c5f58c25292d2f468c0f95675687526336e20650
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:41:07 +0200

    Create SoulMaster.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P03_Hero/SoulMaster.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P03_Hero;
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
