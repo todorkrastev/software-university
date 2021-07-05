0a149898f8f2f2c7a495fc96dccbea00521d2c44
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-06 00:04:24 +0200

    Create Animal.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P02_Zoo/Animal.java
@@ -0,0 +1,13 @@
+package bg.softuni.java_oop.inheritance.exercises.P02_Zoo;
+
+public class Animal {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
