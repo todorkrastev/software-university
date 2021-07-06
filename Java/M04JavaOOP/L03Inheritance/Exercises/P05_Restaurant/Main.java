d252ee7165df8a2bf6697baa72b0b7e878317674
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:51:37 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/Main.java
@@ -0,0 +1,8 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant;
+
+public class Main {
+    public static void main(String[] args) {
+        Cake cake = new Cake("Cheese cake");
+        System.out.println(cake.getCalories());
+    }
+}
