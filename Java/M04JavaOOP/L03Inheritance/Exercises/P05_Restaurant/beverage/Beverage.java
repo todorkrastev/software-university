d744ed1161e8b4b0d7f5608305ef013a2b85a50c
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 01:26:16 +0200

    Create Beverage.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/beverage/Beverage.java
@@ -0,0 +1,23 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.beverage;
+
+import bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.Product;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product {
+
+    protected double milliliters;
+
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return this.milliliters;
+    }
+
+    public void setMilliliters(double milliliters) {
+        this.milliliters = milliliters;
+    }
+}
