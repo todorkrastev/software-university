78150733ffccb7218536032024c4eb29bfdbed6b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:51:24 +0200

    Create ColdBeverage.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
