41ee6b891df6e01580957d506e9628c777b8fe77
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 01:26:41 +0200

    Create Food.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/food/Food.java
@@ -0,0 +1,18 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.food;
+
+import bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.Product;
+
+import java.math.BigDecimal;
+
+public class Food extends Product {
+    protected double grams;
+
+    public Food(String name, BigDecimal price, double grams) {
+        super(name, price);
+        this.grams = grams;
+    }
+
+    public double getGrams() {
+        return this.grams;
+    }
+}
