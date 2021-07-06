e641965f85fdcf44b352c4a5005c5ab4f6b3f19f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 01:26:45 +0200

    Create MainDish.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/food/MainDish.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.food;
+
+import java.math.BigDecimal;
+
+public class MainDish extends Food {
+    public MainDish(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
