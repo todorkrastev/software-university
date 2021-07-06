20c4ca65696a24256c0a21f59ce7d16a23152e58
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 01:26:51 +0200

    Create Soup.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/food/Soup.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant.food;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
