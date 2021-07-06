3a5c06452b7693d1c1d87f6fa4355b922857a4f5
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:18:21 +0200

    Create Motorcycle.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P04_NeedForSpeed/Motorcycle.java
@@ -0,0 +1,8 @@
+package bg.softuni.java_oop.inheritance.exercises.P04_NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
