4a6d948623c94284de73ad81c688a29f5392c5cb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:18:15 +0200

    Create FamilyCar.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P04_NeedForSpeed/FamilyCar.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P04_NeedForSpeed;
+
+public class FamilyCar extends Car {
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
