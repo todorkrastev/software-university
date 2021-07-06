631276f0e9bb8dfa484323d0830b03c76ec87b12
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:18:12 +0200

    Create CrossMotorcycle.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P04_NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.inheritance.exercises.P04_NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
