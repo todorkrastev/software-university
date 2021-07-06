c775ce5c4f46a4d5a012b28b87b24fccaf5de27f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:52:12 +0200

    Create Tea.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P05_Restaurant/Tea.java
@@ -0,0 +1,9 @@
+package bg.softuni.java_oop.inheritance.exercises.P05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
