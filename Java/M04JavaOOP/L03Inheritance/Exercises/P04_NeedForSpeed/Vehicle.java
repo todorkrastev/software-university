10f90a73db6c51f1ca3230fd2e6ad3109b6704cb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 00:18:34 +0200

    Create Vehicle.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P04_NeedForSpeed/Vehicle.java
@@ -0,0 +1,45 @@
+package bg.softuni.java_oop.inheritance.exercises.P04_NeedForSpeed;
+
+public class Vehicle {
+    protected final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+    }
+
+    public double getFuelConsumption() {
+        return this.fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return this.fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers) {
+        double fuelNeeded = kilometers * this.getFuelConsumption();
+        if (fuelNeeded <= this.getFuel()) {
+            this.setFuel(this.getFuel() - fuelNeeded);
+        }
+    }
+}
