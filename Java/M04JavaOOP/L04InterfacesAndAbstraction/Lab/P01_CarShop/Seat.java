dea5854646b36da00015d23010f2b2748b8bd988
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-08 00:10:11 +0200

    Create Seat.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Lab/P01_CarShop/Seat.java
@@ -0,0 +1,46 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.lab.P01_CarShop;
+
+import java.io.Serializable;
+
+public class Seat implements Car, Serializable {
+    private final String model;
+    private final String color;
+    private final Integer horsePower;
+    private final String countryProduced;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    public String getCountryProduced() {
+        return this.countryProduced;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return this.countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %s tires", this.getModel(), this.getCountryProduced(), Car.TYRES);
+    }
+}
