28844adad9ef0cf5b0836de0364e0e0e92f61f27
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 02:44:39 +0200

    Create Dog.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P06_Animals/Dog.java
@@ -0,0 +1,12 @@
+package bg.softuni.java_oop.inheritance.exercises.P06_Animals;
+
+public class Dog extends Animal {
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Woof!";
+    }
+}
