3b2821f544c677a2170f6266a6aa4706ed48ef27
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-07 02:44:42 +0200

    Create Frog.java

---
+++ Java/M04JavaOOP/L03Inheritance/Exercises/P06_Animals/Frog.java
@@ -0,0 +1,12 @@
+package bg.softuni.java_oop.inheritance.exercises.P06_Animals;
+
+public class Frog extends Animal {
+    public Frog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Ribbit";
+    }
+}
