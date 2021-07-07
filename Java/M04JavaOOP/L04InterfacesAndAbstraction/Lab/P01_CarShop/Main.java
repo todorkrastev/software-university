e31659177e70d64724a305fe618d54f5407cb6b4
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-08 00:10:08 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Lab/P01_CarShop/Main.java
@@ -0,0 +1,14 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.lab.P01_CarShop;
+
+public class Main {
+    public static void main(String[] args) {
+        Car seat = new Seat("Leon", "gray", 110, "Spain");
+
+        System.out.printf(
+                "%s is %s color and have %s horse power%n",
+                seat.getModel(),
+                seat.getColor(),
+                seat.getHorsePower());
+        System.out.println(seat);
+    }
+}
