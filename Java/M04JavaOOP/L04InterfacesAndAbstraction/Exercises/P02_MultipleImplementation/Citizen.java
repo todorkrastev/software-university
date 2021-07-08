90d6d7f7c63bd928357170871e4c52de53ff8727
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-09 00:13:27 +0200

    Create Citizen.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P02_MultipleImplementation/Citizen.java
@@ -0,0 +1,41 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P02_MultipleImplementation;
+
+public class Citizen implements  Person, Identifiable, Birthable{
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
