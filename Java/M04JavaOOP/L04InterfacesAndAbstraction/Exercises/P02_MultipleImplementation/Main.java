5688f793a1aeec0ab73e51cd7a6179d5c998b99f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-09 00:13:37 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P02_MultipleImplementation/Main.java
@@ -0,0 +1,27 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P02_MultipleImplementation;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Class[] citizenInterfaces = Citizen.class.getInterfaces();
+        if (Arrays.asList(citizenInterfaces).contains(Birthable.class)
+                && Arrays.asList(citizenInterfaces).contains(Identifiable.class)) {
+            Method[] methods = Birthable.class.getDeclaredMethods();
+            methods = Identifiable.class.getDeclaredMethods();
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            int age = Integer.parseInt(scanner.nextLine());
+            String id = scanner.nextLine();
+            String birthDate = scanner.nextLine();
+            Identifiable identifiable = new Citizen(name, age, id, birthDate);
+            Birthable birthable = new Citizen(name, age, id, birthDate);
+            System.out.println(methods.length);
+            System.out.println(methods[0].getReturnType().getSimpleName());
+            System.out.println(methods.length);
+            System.out.println(methods[0].getReturnType().getSimpleName());
+        }
+    }
+}
