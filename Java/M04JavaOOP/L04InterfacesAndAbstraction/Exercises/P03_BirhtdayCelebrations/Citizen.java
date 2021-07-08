fa9c4f4f2c4eeb2d93389ac6ad0c225c04a06a8d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-09 00:14:01 +0200

    Create Citizen.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P03_BirhtdayCelebrations/Citizen.java
@@ -0,0 +1,40 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P03_BirhtdayCelebrations;
+
+public class Citizen implements Identifiable, Birthable, Person {
+    private final String name;
+    private final int age;
+    private final String id;
+    private final String birthDate;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
