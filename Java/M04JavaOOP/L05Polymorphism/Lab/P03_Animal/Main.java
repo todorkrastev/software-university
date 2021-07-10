78bcb09665c3ae81ceb24d5cfdd6ff0a0fa0e846
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-10 15:35:07 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L05Polymorphism/Lab/P03_Animal/Main.java
@@ -0,0 +1,10 @@
+package bg.softuni.java_oop.polymorphism.lab.P03_Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal cat = new Cat("Oscar", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+    }
+}
