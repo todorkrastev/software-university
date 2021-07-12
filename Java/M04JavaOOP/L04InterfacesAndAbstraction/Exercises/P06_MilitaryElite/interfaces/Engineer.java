59ba9dae34dee8e47d2d595edf9b35cca4eed025
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:37:00 +0200

    Create Engineer.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/interfaces/Engineer.java
@@ -0,0 +1,11 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces;
+
+import java.util.List;
+
+public interface Engineer {
+
+    void addRepair(Repair repair);
+
+    List<Repair> getRepairs();
+
+}
