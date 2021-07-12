5396166ec9fbcf6a2f78ab44341ca8abdbe3dd80
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:37:22 +0200

    Create Soldier.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/interfaces/Soldier.java
@@ -0,0 +1,11 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces;
+
+public interface Soldier {
+
+    int getId();
+
+    String getFirstName();
+
+    String getLastName();
+
+}
