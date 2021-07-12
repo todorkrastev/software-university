ed236878a0ef120c79c04bd4c864a523fd245421
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:37:10 +0200

    Create Mission.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/interfaces/Mission.java
@@ -0,0 +1,10 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces;
+
+public interface Mission {
+
+    void completeMission();
+
+    String getCodeName();
+
+    String getState();
+}
