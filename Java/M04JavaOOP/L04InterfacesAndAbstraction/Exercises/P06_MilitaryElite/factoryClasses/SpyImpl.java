919324dc6035eeb3088a5e2c79bd1abcd57ce202
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:36:45 +0200

    Create SpyImpl.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/factoryClasses/SpyImpl.java
@@ -0,0 +1,23 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.factoryClasses;
+
+import bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces.Spy;
+
+public class SpyImpl extends SoldierImpl implements Spy {
+
+    private final String codeNumber;
+
+    public SpyImpl(int id, String firstName, String lastName, String codeNumber) {
+        super(id, firstName, lastName);
+        this.codeNumber = codeNumber;
+    }
+
+    @Override
+    public String getCodeNumber() {
+        return this.codeNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%nCode Number: %s", super.toString(), this.getCodeNumber());
+    }
+}
