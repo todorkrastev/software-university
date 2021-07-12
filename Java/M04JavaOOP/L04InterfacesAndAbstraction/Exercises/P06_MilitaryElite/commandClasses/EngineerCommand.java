df1b61858bbd1863ee73db4909690160ff176e45
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:35:30 +0200

    Create EngineerCommand.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/commandClasses/EngineerCommand.java
@@ -0,0 +1,22 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.commandClasses;
+
+import bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.enumeration.Corps;
+import bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.factoryClasses.SoldierFactory;
+import bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces.Soldier;
+
+import java.util.List;
+
+public class EngineerCommand extends BaseCommand {
+
+
+    public EngineerCommand(List<Soldier> soldiers) {
+        super(soldiers);
+    }
+
+    @Override
+    public void execute(List<String> args) {
+        if (Corps.isValidCorps(args.get(4))) {
+            super.add(SoldierFactory.produceEngineer(args));
+        }
+    }
+}
