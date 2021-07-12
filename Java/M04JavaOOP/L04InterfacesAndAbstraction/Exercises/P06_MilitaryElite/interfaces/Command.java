dc63a21ab635063b7cafb3cc9dc9c67cfa40bd8a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-12 23:36:52 +0200

    Create Command.java

---
+++ Java/M04JavaOOP/L04InterfacesAndAbstraction/Exercises/P06_MilitaryElite/interfaces/Command.java
@@ -0,0 +1,8 @@
+package bg.softuni.java_oop.interfaces_and_abstraction.exercises.P06_MilitaryElite.interfaces;
+
+import java.util.List;
+
+public interface Command {
+
+    void execute(List<String> args);
+}
