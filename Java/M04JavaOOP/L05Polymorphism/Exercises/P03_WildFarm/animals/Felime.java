fa3e1334f80d5d6fe114629936791a6bc7a1ff0b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-14 00:41:23 +0200

    Create Felime.java

---
+++ Java/M04JavaOOP/L05Polymorphism/Exercises/P03_WildFarm/animals/Felime.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.polymorphism.exercises.P03_WildFarm.animals;
+
+public abstract class Felime extends Mammal {
+    protected Felime(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
