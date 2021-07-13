201d687d50f697c84fd4084ce330c179c612757b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-14 00:41:48 +0200

    Create Vegetable.java

---
+++ Java/M04JavaOOP/L05Polymorphism/Exercises/P03_WildFarm/foods/Vegetable.java
@@ -0,0 +1,7 @@
+package bg.softuni.java_oop.polymorphism.exercises.P03_WildFarm.foods;
+
+public class Vegetable extends Food{
+    public Vegetable(int quantity) {
+        super(quantity);
+    }
+}
