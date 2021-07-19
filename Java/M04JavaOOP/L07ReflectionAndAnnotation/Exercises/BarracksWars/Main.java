785b56409f26ad14e6d16f0d562173f69c2ff577
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-19 22:58:52 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L07ReflectionAndAnnotation/Exercises/BarracksWars/Main.java
@@ -0,0 +1,19 @@
+package barracksWars;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.Runnable;
+import barracksWars.interfaces.UnitFactory;
+import barracksWars.core.Engine;
+import barracksWars.core.factories.UnitFactoryImpl;
+import barracksWars.data.UnitRepository;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Repository repository = new UnitRepository();
+        UnitFactory unitFactory = new UnitFactoryImpl();
+
+        Runnable engine = new Engine(repository, unitFactory);
+        engine.run();
+    }
+}
