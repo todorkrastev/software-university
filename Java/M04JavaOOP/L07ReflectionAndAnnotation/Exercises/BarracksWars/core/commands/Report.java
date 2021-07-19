28347c179e901c4258ee9dfe960f87271ef9e3d9
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-19 22:57:48 +0200

    Create Report.java

---
+++ Java/M04JavaOOP/L07ReflectionAndAnnotation/Exercises/BarracksWars/core/commands/Report.java
@@ -0,0 +1,18 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+
+public class Report extends Command {
+
+
+    public Report(String[] data, Repository repository, UnitFactory unitFactory) {
+        super(data, repository, unitFactory);
+    }
+
+    @Override
+    public String execute() {
+
+        return getRepository().getStatistics();
+    }
+}
