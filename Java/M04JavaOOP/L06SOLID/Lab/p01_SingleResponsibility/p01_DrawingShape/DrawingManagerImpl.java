8eb831adf9242eb2595244fca9397094501cea67
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-20 20:47:48 +0200

    Create DrawingManagerImpl.java

---
+++ Java/M04JavaOOP/L06SOLID/Lab/p01_SingleResponsibility/p01_DrawingShape/DrawingManagerImpl.java
@@ -0,0 +1,24 @@
+package solidLab.p01_SingleResponsibility.p01_DrawingShape;
+
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingManager;
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.DrawingRepository;
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Rengerer;
+import solidLab.p01_SingleResponsibility.p01_DrawingShape.interfaces.Shape;
+
+
+public class DrawingManagerImpl implements DrawingManager {
+
+    private final DrawingRepository drawingRepository;
+    private final Rengerer renderer;
+
+    public DrawingManagerImpl(DrawingRepository drawingRepository, Rengerer renderer) {
+        this.drawingRepository = drawingRepository;
+        this.renderer = renderer;
+    }
+
+
+    @Override
+    public void draw(Shape shape) {
+        shape.draw(this.renderer, this.drawingRepository);
+    }
+}
