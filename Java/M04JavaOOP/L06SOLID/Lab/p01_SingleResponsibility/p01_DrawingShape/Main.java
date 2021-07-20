6a455d990c2774c0c3649001c7b70f3b619fbded
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-20 20:48:07 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/L06SOLID/Lab/p01_SingleResponsibility/p01_DrawingShape/Main.java
@@ -0,0 +1,7 @@
+package solidLab.p01_SingleResponsibility.p01_DrawingShape;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
