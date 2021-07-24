33206d03f01be9c34c6fecf55a5711d6c7c6d92d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-25 01:00:44 +0200

    Create Dummy.java

---
+++ Java/M04JavaOOP/L10UnitTesting/Lab/Problems_01-06/src/main/java/rpg_lab/Dummy.java
@@ -0,0 +1,36 @@
+package rpg_lab;
+
+public class Dummy {
+
+    private int health;
+    private int experience;
+
+    public Dummy(int health, int experience) {
+        this.health = health;
+        this.experience = experience;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void takeAttack(int attackPoints) {
+        if (this.isDead()) {
+            throw new IllegalStateException("Dummy is dead.");
+        }
+
+        this.health -= attackPoints;
+    }
+
+    public int giveExperience() {
+        if (!this.isDead()) {
+            throw new IllegalStateException("Target is not dead.");
+        }
+
+        return this.experience;
+    }
+
+    public boolean isDead() {
+        return this.health <= 0;
+    }
+}
