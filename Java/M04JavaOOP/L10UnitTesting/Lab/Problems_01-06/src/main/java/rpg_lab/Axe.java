b8569abd3c04e241d74e0d47303ac5c6e5e314c4
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-25 01:00:41 +0200

    Create Axe.java

---
+++ Java/M04JavaOOP/L10UnitTesting/Lab/Problems_01-06/src/main/java/rpg_lab/Axe.java
@@ -0,0 +1,29 @@
+package rpg_lab;
+
+public class Axe {
+
+    private int attackPoints;
+    private int durabilityPoints;
+
+    public Axe(int attack, int durability) {
+        this.attackPoints = attack;
+        this.durabilityPoints = durability;
+    }
+
+    public int getAttackPoints() {
+        return this.attackPoints;
+    }
+
+    public int getDurabilityPoints() {
+        return this.durabilityPoints;
+    }
+
+    public void attack(Dummy target) {
+        if (this.durabilityPoints <= 0) {
+            throw new IllegalStateException("Axe is broken.");
+        }
+
+        target.takeAttack(this.attackPoints);
+        this.durabilityPoints -= 1;
+    }
+}
