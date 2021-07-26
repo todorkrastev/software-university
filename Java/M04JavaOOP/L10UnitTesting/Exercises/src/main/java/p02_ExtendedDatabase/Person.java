1b5a400d20357ab17672be06946dba7c66bede19
Todor Krastev <krastevtodordinev@yahoo.com>
2021-07-26 19:53:51 +0200

    Create Person.java

---
+++ Java/M04JavaOOP/L10UnitTesting/Exercises/src/main/java/p02_ExtendedDatabase/Person.java
@@ -0,0 +1,35 @@
+package p02_ExtendedDatabase;
+
+import java.util.Objects;
+
+public class Person {
+
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
