2e768a1397d9e570119348776c7e889d9f1d6afe
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-01 01:06:07 +0200

    Create P06QuickSort.java

---
+++ Java/M03JavaAdvanced/L10BasicAlgorithms/P06QuickSort.java
@@ -0,0 +1,48 @@
+package bg.softuni.java_advanced.basic_algorithms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class P06QuickSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+        int[] array = Arrays.stream(reader.readLine().trim().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        sort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array).replaceAll("[\\[\\],]", ""));
+    }
+
+    static void sort(int[] subArray, int startIndex, int endIndex) {
+
+        if (startIndex < endIndex) {
+            int pivot = partition(subArray, startIndex, endIndex);
+            sort(subArray, startIndex, pivot - 1);
+            sort(subArray, pivot + 1, endIndex);
+        }
+    }
+
+    static int partition(int[] subArray, int startIndex, int endIndex) {
+
+        int pivot = subArray[endIndex];
+        int i = (startIndex - 1);
+
+        for (int j = startIndex; j < endIndex; j++) {
+            if (subArray[j] <= pivot) {
+                i++;
+                int temp = subArray[i];
+                subArray[i] = subArray[j];
+                subArray[j] = temp;
+            }
+        }
+        int temp = subArray[i + 1];
+        subArray[i + 1] = subArray[endIndex];
+        subArray[endIndex] = temp;
+        return i + 1;
+    }
+}
