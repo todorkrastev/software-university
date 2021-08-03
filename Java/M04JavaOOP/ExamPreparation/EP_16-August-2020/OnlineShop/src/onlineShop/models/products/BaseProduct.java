2338f28218391c395daddb72cbff793997e295b0
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-04 00:52:36 +0200

    Create BaseProduct.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_16-August-2020/OnlineShop/src/onlineShop/models/products/BaseProduct.java
@@ -0,0 +1,87 @@
+package onlineShop.models.products;
+
+import static onlineShop.common.constants.ExceptionMessages.*;
+import static onlineShop.common.constants.OutputMessages.PRODUCT_TO_STRING;
+
+public abstract class BaseProduct implements Product {
+    private int id;
+    private String manufacturer;
+    private String model;
+    private double price;
+    private double overallPerformance;
+
+    protected BaseProduct(int id, String manufacturer, String model, double price, double overallPerformance) {
+        this.setId(id);
+        this.setManufacturer(manufacturer);
+        this.setModel(model);
+        this.setPrice(price);
+        this.setOverallPerformance(overallPerformance);
+    }
+
+    private void setId(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException(INVALID_PRODUCT_ID);
+        }
+        this.id = id;
+    }
+
+    private void setManufacturer(String manufacturer) {
+        if (manufacturer == null || manufacturer.trim().isEmpty()) {
+            throw new IllegalArgumentException(INVALID_MANUFACTURER);
+        }
+        this.manufacturer = manufacturer;
+    }
+
+    private void setModel(String model) {
+        if (model == null || model.trim().isEmpty()) {
+            throw new IllegalArgumentException(INVALID_MODEL);
+        }
+        this.model = model;
+    }
+
+    private void setPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException(INVALID_PRICE);
+        }
+        this.price = price;
+    }
+
+    private void setOverallPerformance(double overallPerformance) {
+        if (overallPerformance <= 0) {
+            throw new IllegalArgumentException(INVALID_OVERALL_PERFORMANCE);
+        }
+        this.overallPerformance = overallPerformance;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public double getOverallPerformance() {
+        return this.overallPerformance;
+    }
+
+    @Override
+    public String toString() {
+        //"Overall Performance: {overall performance}. Price: {price} - {product type}: {manufacturer} {model} (Id: {id})"
+        return String.format(PRODUCT_TO_STRING, getOverallPerformance(), getPrice(), this.getClass().getSimpleName(),
+                getManufacturer(), getModel(), getId());
+    }
+}
