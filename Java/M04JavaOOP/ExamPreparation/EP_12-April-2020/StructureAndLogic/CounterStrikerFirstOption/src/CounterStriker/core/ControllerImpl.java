42e7b180f66ae33e1ae81ef4d213c5206639ce13
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-06 13:11:16 +0200

    Create ControllerImpl.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_12-April-2020/StructureAndLogic/CounterStrikerFirstOption/src/CounterStriker/core/ControllerImpl.java
@@ -0,0 +1,116 @@
+package CounterStriker.core;
+
+import CounterStriker.models.field.Field;
+import CounterStriker.models.field.FieldImpl;
+import CounterStriker.models.guns.Gun;
+import CounterStriker.models.guns.Pistol;
+import CounterStriker.models.guns.Rifle;
+import CounterStriker.models.players.CounterTerrorist;
+import CounterStriker.models.players.Player;
+import CounterStriker.models.players.Terrorist;
+import CounterStriker.repositories.GunRepository;
+import CounterStriker.repositories.PlayerRepository;
+import CounterStriker.repositories.Repository;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+import static CounterStriker.common.ExceptionMessages.*;
+import static CounterStriker.common.OutputMessages.*;
+
+public class ControllerImpl implements Controller {
+    private Repository<Gun> guns;
+    private Repository<Player> players;
+    private Field field;
+
+    public ControllerImpl() {
+        this.guns = new GunRepository<>();
+        this.players = new PlayerRepository<>();
+        this.field = new FieldImpl();
+    }
+
+    @Override
+    public String addGun(String type, String name, int bulletsCount) {
+        Gun gun;
+        switch (type) {
+            case "Pistol":
+                gun = new Pistol(name, bulletsCount);
+                break;
+            case "Rifle":
+                gun = new Rifle(name, bulletsCount);
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_GUN_TYPE);
+        }
+        this.guns.add(gun);
+
+        return String.format(SUCCESSFULLY_ADDED_GUN, name);
+    }
+
+    @Override
+    public String addPlayer(String type, String username, int health, int armor, String gunName) {
+        Gun gun = this.guns.findByName(gunName);
+        if (gun == null) {
+            throw new NullPointerException(GUN_CANNOT_BE_FOUND);
+        }
+        Player player;
+        switch (type) {
+            case "Terrorist":
+                player = new Terrorist(username, health, armor, gun);
+                break;
+            case "CounterTerrorist":
+                player = new CounterTerrorist(username, health, armor, gun);
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_PLAYER_TYPE);
+        }
+        this.players.add(player);
+
+        return String.format(SUCCESSFULLY_ADDED_PLAYER, username);
+    }
+
+    @Override
+    public String startGame() {
+        Collection<Player> players = this.players
+                .getModels()
+                .stream()
+                .filter(Player::isAlive)
+                .collect(Collectors.toList());
+        return this.field.start(players);
+    }
+
+    @Override
+    public String report() {
+        StringBuilder output = new StringBuilder();
+        this.players
+                .getModels()
+                .stream()
+                .sorted((p1, p2) -> {
+                    int result = p1.getClass().getSimpleName().compareTo(p2.getClass().getSimpleName());
+                    if (result == 0) {
+                        result = Integer.compare(p2.getHealth(), p1.getHealth());
+                    }
+                    if (result == 0) {
+                        result = p1.getUsername().compareTo(p2.getUsername());
+                    }
+                    return result;
+                })
+                .forEach(player -> output
+                        .append(player)
+                        .append(System.lineSeparator()));
+
+        // second option
+      /*  this.players
+                .getModels()
+                .stream()
+                .sorted(Comparator.comparing(player -> player.getClass().getSimpleName()))
+                .sorted(Comparator.comparing(Player::getHealth)
+                        .thenComparing(Player::getUsername))
+                .forEach(player -> output
+                        .append(player)
+                        .append(System.lineSeparator())); */
+
+        return output.toString().trim();
+    }
+}
