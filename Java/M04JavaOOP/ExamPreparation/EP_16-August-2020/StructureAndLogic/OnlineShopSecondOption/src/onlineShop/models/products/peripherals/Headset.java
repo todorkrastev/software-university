2ac78bdeb25c5564ad897c2322b40f7fb0fbaadb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-08 02:48:25 +0200

    Create Headset.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_16-August-2020/StructureAndLogic/OnlineShopSecondOption/src/onlineShop/models/products/peripherals/Headset.java
@@ -0,0 +1,7 @@
+package onlineShop.models.products.peripherals;
+
+public class Headset extends BasePeripheral{
+    public Headset(int id, String manufacturer, String model, double price, double overallPerformance, String connectionType) {
+        super(id, manufacturer, model, price, overallPerformance, connectionType);
+    }
+}
