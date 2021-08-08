07e116b72b02598c18046a8578ed5c186e0b3d68
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-08 02:46:54 +0200

    Create BaseComponent.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_16-August-2020/StructureAndLogic/OnlineShopSecondOption/src/onlineShop/models/products/components/BaseComponent.java
@@ -0,0 +1,28 @@
+package onlineShop.models.products.components;
+
+import onlineShop.models.products.BaseProduct;
+
+import static onlineShop.common.constants.OutputMessages.*;
+
+public abstract class BaseComponent extends BaseProduct implements Component {
+    private int generation;
+
+    protected BaseComponent(int id, String manufacturer, String model, double price, double overallPerformance, int generation) {
+        super(id, manufacturer, model, price, overallPerformance);
+        this.setGeneration(generation);
+    }
+
+    private void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    @Override
+    public int getGeneration() {
+        return this.generation;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(COMPONENT_TO_STRING, this.getGeneration());
+    }
+}
