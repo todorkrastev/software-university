87edc6b793d082873edcae86c650bf64a600ed43
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-10 00:16:21 +0200

    Create BaseGun.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_19-December-2020/StructureAndLogic/ViceCity/src/viceCity/models/guns/BaseGun.java
@@ -0,0 +1,62 @@
+package viceCity.models.guns;
+
+import static viceCity.common.ExceptionMessages.*;
+
+public abstract class BaseGun implements Gun{
+    private String name;
+    private int bulletsPerBarrel;
+    private int totalBullets;
+    private boolean canFire;
+
+    protected BaseGun(String name, int bulletsPerBarrel, int totalBullets) {
+        this.setName(name);
+        this.setBulletsPerBarrel(bulletsPerBarrel);
+        this.setTotalBullets(totalBullets);
+        this.setCanFire();
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(NAME_NULL);
+        }
+        this.name = name;
+    }
+
+    private void setBulletsPerBarrel(int bulletsPerBarrel) {
+        if (bulletsPerBarrel < 0) {
+            throw new IllegalArgumentException(BULLETS_LESS_THAN_ZERO);
+        }
+        this.bulletsPerBarrel = bulletsPerBarrel;
+    }
+
+    protected void setTotalBullets(int totalBullets) {
+        if (totalBullets < 0) {
+            throw new IllegalArgumentException(TOTAL_BULLETS_LESS_THAN_ZERO);
+        }
+        this.totalBullets = totalBullets;
+    }
+
+    private void setCanFire() {
+        this.canFire = canFire();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBulletsPerBarrel() {
+        return this.bulletsPerBarrel;
+    }
+
+    @Override
+    public boolean canFire() {
+        return 0 < this.totalBullets;
+    }
+
+    @Override
+    public int getTotalBullets() {
+        return this.totalBullets;
+    }
+}
