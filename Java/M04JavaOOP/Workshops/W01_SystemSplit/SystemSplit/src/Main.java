293655e24e8cab83ee2461195d7c54d5670c89a6
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-12 23:27:02 +0200

    Create Main.java

---
+++ Java/M04JavaOOP/Workshops/W01_SystemSplit/SystemSplit/src/Main.java
@@ -0,0 +1,12 @@
+
+
+import manager.MyManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyManager manager = new MyManager();
+        manager.run();
+    }
+}
