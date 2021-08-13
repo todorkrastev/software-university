3a0e7a57e8207b7f70ff30d27a478e01b5c8c1b0
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-13 22:09:34 +0200

    Create InsufficientContestantsException.java

---
+++ Java/M04JavaOOP/Workshops/W02_BoatRacingSimulator/src/exceptions/InsufficientContestantsException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InsufficientContestantsException extends Exception {
+    public InsufficientContestantsException(String message){
+        super(message);
+    }
+}
