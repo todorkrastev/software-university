b1b9fd8ceae2b8a0185d87d02621a7657b4db9e4
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-14 21:58:45 +0200

    Create MissionImpl.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_18-April-2021/StructureAndLogic/SpaceStation/src/spaceStation/models/mission/MissionImpl.java
@@ -0,0 +1,28 @@
+package spaceStation.models.mission;
+
+import spaceStation.models.astronauts.Astronaut;
+import spaceStation.models.planets.Planet;
+
+import java.util.List;
+
+public class MissionImpl implements Mission{
+    @Override
+    public void explore(Planet planet, List<Astronaut> astronauts) {
+        //TODO: DOUBLE CHECK!
+        for (int astronaut = 0; astronaut < astronauts.size(); astronaut++) {
+            Astronaut currAstronaut = astronauts.get(astronaut);
+            for (int item = 0; item < planet.getItems().size(); item++) {
+                String currItem = planet.getItems().get(item);
+                currAstronaut.getBag().getItems().add(currItem);
+                currAstronaut.breath();
+                planet.getItems().remove(currItem);
+                item--;
+                if (!currAstronaut.canBreath()) {
+                    astronauts.remove(currAstronaut);
+                    astronaut--;
+                    break;
+                }
+            }
+        }
+    }
+}
