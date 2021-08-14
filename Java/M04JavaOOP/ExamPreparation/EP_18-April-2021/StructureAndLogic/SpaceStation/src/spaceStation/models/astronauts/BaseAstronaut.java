3afdbdb630ba474a45159441a634529613f5b83d
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-14 21:58:20 +0200

    Create BaseAstronaut.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_18-April-2021/StructureAndLogic/SpaceStation/src/spaceStation/models/astronauts/BaseAstronaut.java
@@ -0,0 +1,62 @@
+package spaceStation.models.astronauts;
+
+import spaceStation.common.ExceptionMessages;
+import spaceStation.models.bags.Backpack;
+import spaceStation.models.bags.Bag;
+
+public abstract class BaseAstronaut implements Astronaut {
+    private String name;
+    private double oxygen;
+    private Bag bag;
+    private static final double DECREASE_OXYGEN_WITH_10_UNITS = 10;
+
+    protected BaseAstronaut(String name, double oxygen) {
+        this.setName(name);
+        this.setOxygen(oxygen);
+        this.setBag();
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.ASTRONAUT_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    protected void setOxygen(double oxygen) {
+        if (oxygen < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.ASTRONAUT_OXYGEN_LESS_THAN_ZERO);
+        }
+        this.oxygen = oxygen;
+    }
+
+    private void setBag() {
+        this.bag = new Backpack();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getOxygen() {
+        return this.oxygen;
+    }
+
+    @Override
+    public boolean canBreath() {
+        //TODO: check 0 <= || < this.getOxygen();
+        return 0 < this.getOxygen();
+    }
+
+    @Override
+    public Bag getBag() {
+        return this.bag;
+    }
+
+    @Override
+    public void breath() {
+        this.setOxygen(this.getOxygen() - DECREASE_OXYGEN_WITH_10_UNITS);
+    }
+}
