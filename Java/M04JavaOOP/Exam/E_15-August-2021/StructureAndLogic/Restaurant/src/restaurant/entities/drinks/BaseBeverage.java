804705ad945d2bcc13d45f97bd14fe3294561120
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-15 18:15:13 +0200

    Create BaseBeverage.java

---
+++ Java/M04JavaOOP/Exam/E_15-August-2021/StructureAndLogic/Restaurant/src/restaurant/entities/drinks/BaseBeverage.java
@@ -0,0 +1,66 @@
+package restaurant.entities.drinks;
+
+import restaurant.common.ExceptionMessages;
+import restaurant.entities.drinks.interfaces.Beverages;
+
+public abstract class BaseBeverage implements Beverages {
+    private String name;
+    private int counter;
+    private double price;
+    private String brand;
+
+    protected BaseBeverage(String name, int counter, double price, String brand) {
+        this.setName(name);
+        this.setCounter(counter);
+        this.setPrice(price);
+        this.setBrand(brand);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_NAME);
+        }
+        this.name = name;
+    }
+
+    private void setCounter(int counter) {
+        if (counter <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_COUNTER);
+        }
+        this.counter = counter;
+    }
+
+    private void setPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
+        }
+        this.price = price;
+    }
+
+    private void setBrand(String brand) {
+        if (brand == null || brand.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_BRAND);
+        }
+        this.brand = brand;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getCounter() {
+        return this.counter;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String getBrand() {
+        return this.brand;
+    }
+}
