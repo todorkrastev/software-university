54be736fbb99bb5fe78fd5ca0bd673dd00d9d20a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-19 13:56:55 +0200

    Create GunImpl.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_12-April-2020/StructureAndLogic/CounterStrikerThirdOption/src/CounterStriker/models/guns/GunImpl.java
@@ -0,0 +1,42 @@
+package CounterStriker.models.guns;
+
+import CounterStriker.common.ExceptionMessages;
+
+public abstract class GunImpl implements Gun{
+    private String name;
+    private int bulletsCount;
+
+    protected GunImpl(String name, int bulletsCount) {
+        this.setName(name);
+        this.setBulletsCount(bulletsCount);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.INVALID_GUN_NAME);
+        }
+        this.name = name;
+    }
+
+    protected void setBulletsCount(int bulletsCount) {
+        if (bulletsCount < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_GUN_BULLETS_COUNT);
+        }
+        this.bulletsCount = bulletsCount;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBulletsCount() {
+        return this.bulletsCount;
+    }
+
+    @Override
+    public int fire() {
+        return 0;
+    }
+}
