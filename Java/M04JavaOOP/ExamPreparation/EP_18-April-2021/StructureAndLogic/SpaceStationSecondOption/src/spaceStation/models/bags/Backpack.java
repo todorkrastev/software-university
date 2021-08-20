d86aaa50ef6e606c4fe62e27f13114ccbb630730
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-20 21:01:45 +0200

    Create Backpack.java

---
+++ Java/M04JavaOOP/ExamPreparation/EP_18-April-2021/StructureAndLogic/SpaceStationSecondOption/src/spaceStation/models/bags/Backpack.java
@@ -0,0 +1,21 @@
+package spaceStation.models.bags;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Backpack implements Bag{
+    private Collection<String> items;
+
+    public Backpack() {
+        this.setItems();
+    }
+
+    public void setItems() {
+        this.items = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<String> getItems() {
+        return this.items;
+    }
+}
