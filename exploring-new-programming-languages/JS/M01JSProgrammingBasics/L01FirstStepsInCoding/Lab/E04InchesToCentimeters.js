959b51bc5737b9520cc86abcc5378e89f8099443
Todor Krastev <krastevtodordinev@yahoo.com>
2021-08-25 23:17:44 +0200

    Delete E04InchesToCentimeters.js

--- exploring-new-programming-languages/JS/M01JSProgrammingBasics/L01FirstStepsInCoding/Lab/E04InchesToCentimeters.js
+++
@@ -1,7 +0,0 @@
-function inchesToCentimeters(input) {
-    let inches = Number(input[0]);
-    let inchesToCentimeters = inches * 2.54;
-    console.log(inchesToCentimeters);
-}
-
-inchesToCentimeters(["5"]);
