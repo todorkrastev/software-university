834862eb6a2121c85104e466c91f1e10508c499b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-08 03:05:24 +0200

    Create P02_StringLength.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P02_StringLength.js
@@ -0,0 +1,6 @@
+function strLen(firstString, secondString, thirdString) {
+    let sumOfStrLen = firstString.length + secondString.length + thirdString.length;
+    let avgLen = Math.floor(sumOfStrLen / 3);
+    console.log(sumOfStrLen);
+    console.log(avgLen);
+}
