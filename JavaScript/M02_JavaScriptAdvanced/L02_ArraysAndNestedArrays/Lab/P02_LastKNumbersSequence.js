194307f7835ec6295c2f74711db30c499f3272fb
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-12 23:38:13 +0200

    Create P02_LastKNumbersSequence.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P02_LastKNumbersSequence.js
@@ -0,0 +1,11 @@
+function lastKNumbersSequence(n, k) {
+    let result = [1];
+
+    for (let i = 1; i < n; i++) {
+        let startIndex = Math.max(0, i - k);
+        let currentElement = result.slice(startIndex, startIndex + k).reduce((a, b) => a + b, 0);
+        result.push(currentElement);
+    }
+
+    console.log(result.join(" "));
+}
