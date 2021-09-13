e2ed227068013742643ff1f09288e21b24f9446c
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-13 16:17:15 +0200

    Delete P01_EvenPositionElement.js

--- JavaScript/M02_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P01_EvenPositionElement.js
+++
@@ -1,7 +0,0 @@
-function printEvenLement(inputArr) {
-    let result = [];
-    for (let index = 0; index < inputArr.length; index += 2) {
-        result[result.length] = inputArr[index];
-    }
-    return result.join(' ');
-}
