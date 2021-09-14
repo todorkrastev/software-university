06fa53a9bb328b6e13bf10342e414390b7168d2b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-14 17:21:08 +0200

    Create P09_BiggestElement.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P09_BiggestElement.js
@@ -0,0 +1,22 @@
+function printBiggestElement(array2d) {
+    let biggestElement = Number.NEGATIVE_INFINITY;
+
+    for (let row = 0; row < array2d.length; row++) {
+        for (let col = 0; col < array2d[row].length; col++) {
+            let currElement = array2d[row][col];
+            let isItString = Boolean(isNaN(currElement));
+            if (isItString) {
+                let index = currElement.charCodeAt(0);
+                if (biggestElement < index) {
+                    biggestElement = index;
+                }
+            } else {
+                if (biggestElement < currElement) {
+                    biggestElement = currElement;
+                }
+            }
+        }
+    }
+    let typeOfElement = array2d.every(row => row.every(element => typeof element === 'number'));
+    return typeOfElement === true ? biggestElement : String.fromCharCode(biggestElement);
+}
