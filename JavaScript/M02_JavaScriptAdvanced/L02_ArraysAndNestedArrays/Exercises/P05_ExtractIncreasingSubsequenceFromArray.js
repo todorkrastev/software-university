19c84836f5d2f8a043e359c6291f55178c9ac055
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-16 01:36:26 +0200

    Create P05_ExtractIncreasingSubsequenceFromArray.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Exercises/P05_ExtractIncreasingSubsequenceFromArray.js
@@ -0,0 +1,8 @@
+function printSequenceNums(arr) {
+    return arr.reduce((a, v) => {
+        if (v >= (a[a.length - 1] || arr[0])) {
+            a.push(v)
+        }
+        return a;
+    }, [])
+}
