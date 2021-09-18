1c570f26cf4296b0179350662aecc73b31a14b0c
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-18 20:10:14 +0200

    Create P11_DiagonalAttack.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Exercises/P11_DiagonalAttack.js
@@ -0,0 +1,17 @@
+function printArray(arr) {
+    arr = arr.map(x => x.split(" ").map(Number));
+    const getDiagSum = arr => arr.reduce((a, v, i) => a + v[i], 0);
+    const mapToSum = (arr, sum) =>
+        arr.map((x, i) => x.map((y, j) => (j === i || j === x.length - i - 1 ? y : sum)));
+    const print = arr => arr.map(x => x.join(" ")).join("\n");
+
+    const [reversedMatrix, resultMatrix] = [arr.reverse(), arr.reverse()];
+
+    return getDiagSum(resultMatrix) === getDiagSum(reversedMatrix)
+        ? print(mapToSum(resultMatrix, getDiagSum(resultMatrix)))
+        : print(resultMatrix);
+}
+console.log(printArray(['1 1 1',
+    '1 1 1',
+    '1 1 0']
+));
