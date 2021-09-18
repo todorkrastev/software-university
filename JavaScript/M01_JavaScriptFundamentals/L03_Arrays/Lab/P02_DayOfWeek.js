dcb35ea4a06a4ef226ccef0225d3c2e28d569155
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-18 20:38:08 +0200

    Create P02_DayOfWeek.js

---
+++ JavaScript/M01_JavaScriptFundamentals/L03_Arrays/Lab/P02_DayOfWeek.js
@@ -0,0 +1,9 @@
+function dayOfWeek(day) {
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
+
+    if (day >= 1 && day <= 7) {
+        console.log(days[day - 1]);
+    } else {
+        console.log(`Invalid day!`);
+    }
+}
