0376138d79e1a58f2c77bb725855ce6b4b5c3c04
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-22 02:44:46 +0200

    Create P01_CityRecord.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P01_CityRecord.js
@@ -0,0 +1,8 @@
+function printCity(name, population, treasury) {
+    const cityRecord = {};
+    cityRecord.name = name;
+    cityRecord.population = population;
+    cityRecord.treasury = treasury;
+
+    return cityRecord;
+}
