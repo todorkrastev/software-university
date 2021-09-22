1e2893b8be4f24de12b1e5d04c69115edf3a23b7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-22 02:44:52 +0200

    Create P03_CityTaxes.js

---
+++ JavaScript/M02_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P03_CityTaxes.js
@@ -0,0 +1,27 @@
+function printCity(name, population, treasury) {
+    const townInfo = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * (percentage / 100));
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.ceil(this.treasury * (percentage / 100));
+        }
+    }
+    return townInfo;
+}
+
+const city =
+  printCity('Tortuga',
+  7000,
+  15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
