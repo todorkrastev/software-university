b8568383381eed1e71e4b0eb2a0cd560fef31dcf
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:06:09 +0200

    Update P03_CityTaxes.js

--- JavaScript/M03_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P03_CityTaxes.js
+++ JavaScript/M03_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P03_CityTaxes.js
@@ -15,13 +15,21 @@ function printCity(name, population, treasury) {
         }
     }
     return townInfo;
+
+    /*class City {
+        constructor(n, p, t) {
+            this.name = n
+            this.population = p
+            this.treasury = t
+            this.taxRate = 10
+        }
+
+        collectTaxes = () => Math.round((this.treasury += this.population * this.taxRate))
+        applyGrowth = p => Math.round((this.population += (this.population * p) / 100))
+        applyRecession = p => Math.round((this.treasury -= (this.treasury * p) / 100))
+    }
+
+    return new City(n, p, t)*/
 }
 
-const city =
-  printCity('Tortuga',
-  7000,
-  15000);
-city.collectTaxes();
-console.log(city.treasury);
-city.applyGrowth(5);
-console.log(city.population);
+
