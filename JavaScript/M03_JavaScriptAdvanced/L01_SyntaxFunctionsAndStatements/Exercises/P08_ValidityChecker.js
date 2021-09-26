ec729fcf6f3fc64c2aa7893a0e02b401d7e9e6e2
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:04:39 +0200

    Update P08_ValidityChecker.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P08_ValidityChecker.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P08_ValidityChecker.js
@@ -16,4 +16,16 @@ function solve(first, second, third, fourth) {
 
         return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
     }
+
+    /* const isValidDist = (x, y, x1, y1) =>
+     Math.sqrt(Math.pow(x1 - x, 2) + Math.pow(y1 - y, 2)) % 1 === 0
+ const pairs = [
+     [x, y, 0, 0],
+     [x1, y1, 0, 0],
+     [x, y, x1, y1],
+ ]
+ const printMsg = (a, b, c, d, validity) => `{${a}, ${b}} to {${c}, ${d}} is ${validity}`
+ pairs.forEach((x, i) =>
+     console.log(isValidDist(...pairs[i]) ? printMsg(...x, "valid") : printMsg(...x, "invalid"))
+ ) */
 }
