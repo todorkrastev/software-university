77810e9316df169b337300b4840ddba1f7b8a351
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:06:06 +0200

    Update P02_TownPopulation.js

--- JavaScript/M03_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P02_TownPopulation.js
+++ JavaScript/M03_JavaScriptAdvanced/L03_ObjectsAndComposition/Lab/P02_TownPopulation.js
@@ -16,4 +16,14 @@ function printTownPopulation(towns) {
     for (const town in townsInfo) {
         console.log(`${town} : ${townsInfo[town]}`);
     }
+
+    /* const parsedData = arr.reduce((a, b) => {
+         const [name, population] = b.split(" <-> ")
+         a[name] = (a[name] || 0) + Number(population)
+         return a
+     }, {})
+ 
+     return Object.entries(parsedData)
+         .map(x => `${x[0]} : ${x[1]}`)
+         .join("\n") */
 }
