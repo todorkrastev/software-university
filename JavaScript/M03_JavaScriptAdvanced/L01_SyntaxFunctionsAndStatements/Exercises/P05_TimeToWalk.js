808a8d5c9f432fd5c278efc29c9f3870329e8e30
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:04:28 +0200

    Update P05_TimeToWalk.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P05_TimeToWalk.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P05_TimeToWalk.js
@@ -13,4 +13,12 @@ function TimeToWalk(arg1, arg2, arg3) {
     let timeHr = Math.floor(time / 3600);
 
     console.log((timeHr < 10 ? "0" : "") + timeHr + ":" + (timeMin + rest < 10 ? "0" : "") + (timeMin + rest) + ":" + (timeSec < 10 ? "0" : "") + timeSec);
+
+    /*  const distance = steps * metersPerStep
+      const decimalTime = distance / 1000 / kmPerHour
+      const n = new Date(0, 0)
+      n.setSeconds(decimalTime * 60 * 60 + 1) //no idea why judge wants 1 second more, but I meh... added it.
+      n.setMinutes(n.getMinutes() + Math.floor(distance / 500))
+  
+      return n.toTimeString().slice(0, 8) */
 }
