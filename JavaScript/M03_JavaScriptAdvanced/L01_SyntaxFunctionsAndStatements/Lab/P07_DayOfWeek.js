c0bb18f32e20566089753e7c9c01f24a43bacf4a
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:05:02 +0200

    Update P07_DayOfWeek.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P07_DayOfWeek.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P07_DayOfWeek.js
@@ -27,4 +27,16 @@ function printNum(dayOfWeek) {
             break;
     }
     console.log(result);
+
+   /* const days = {
+        Monday: 1,
+        Tuesday: 2,
+        Wednesday: 3,
+        Thursday: 4,
+        Friday: 5,
+        Saturday: 6,
+        Sunday: 7,
+    }
+
+    return days[d] || "error" */
 }
