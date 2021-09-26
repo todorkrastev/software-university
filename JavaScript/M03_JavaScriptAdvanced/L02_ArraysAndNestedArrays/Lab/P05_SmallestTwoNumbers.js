e50f156969531daf47c3efedd93be2a2b821abe7
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:05:42 +0200

    Update P05_SmallestTwoNumbers.js

--- JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P05_SmallestTwoNumbers.js
+++ JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P05_SmallestTwoNumbers.js
@@ -4,4 +4,9 @@ function printSmallestNumbers(arrStr) {
         .sort((a, b) => a - b);
     const arrToPrint = [arrToNum[0], arrToNum[1]];
     return arrToPrint;
+
+    /* return arrStr
+     .sort((x, y) => x - y)
+     .slice(0, 2)
+     .join(" ")  */
 }
