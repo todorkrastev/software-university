7d4045fc0a2f985dfbb0caa77e4825f1dd47a865
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:05:20 +0200

    Update P09_MagicMatrices.js

--- JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Exercises/P09_MagicMatrices.js
+++ JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Exercises/P09_MagicMatrices.js
@@ -20,8 +20,18 @@ function isMagicMatrix(matrix) {
         }
     }
     return true;
+
+    /* return [
+         ...arr.map(x => x.reduce((a, v) => a + v, 0)),
+         ...arr.reduce((a, v, i) => {
+             a.push(arr.reduce((c, d) => c + d[i], 0))
+             return a
+         }, []),
+     ].every((x, _, arr) => x === arr[0])return [
+         ...arr.map(x => x.reduce((a, v) => a + v, 0)),
+         ...arr.reduce((a, v, i) => {
+             a.push(arr.reduce((c, d) => c + d[i], 0))
+             return a
+         }, []),
+     ].every((x, _, arr) => x === arr[0]) */
 }
-console.log(isMagicMatrix([[4],
-    [5],
-    [6]]
-   ));
