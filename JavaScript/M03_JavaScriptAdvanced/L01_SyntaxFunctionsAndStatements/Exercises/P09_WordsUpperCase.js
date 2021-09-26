ba456b85376e942c4b07fdf81ab5808664810e84
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:04:46 +0200

    Update P09_WordsUpperCase.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P09_WordsUpperCase.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Exercises/P09_WordsUpperCase.js
@@ -6,12 +6,14 @@ function solve(text) {
       .join(", ");
   
     console.log(result);
+    
+     /*  let result = text.toUpperCase()
+         .match(/\w+/g)
+         .join(', ');
+       
+       console.log(result); */
+
+    //   return s.match(/\w+/g).join(", ").toLocaleUpperCase()
   }
   
- /* function solve(text) {
-    let result = text.toUpperCase()
-      .match(/\w+/g)
-      .join(', ');
-    
-    console.log(result);
-  } */
+
