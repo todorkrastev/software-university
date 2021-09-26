647deeb949d3dfa8c770501396d71139d78c896e
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:05:59 +0200

    Update P11_EqualNeighnours.js

--- JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P11_EqualNeighnours.js
+++ JavaScript/M03_JavaScriptAdvanced/L02_ArraysAndNestedArrays/Lab/P11_EqualNeighnours.js
@@ -62,3 +62,26 @@ function findPairs(array2d) {
           }
       } */
 }
+
+//second option
+
+/* function findPairsII(arr) {
+    let count = 0
+    arr.forEach(x =>
+        x.reduce((a, v) => {
+            if (a === v) {
+                count += 1
+            }
+            return v
+        })
+    )
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i].forEach((_, j) => {
+            if (arr[i][j] === arr[i + 1][j]) {
+                count += 1
+            }
+        })
+    }
+    return count
+} */
