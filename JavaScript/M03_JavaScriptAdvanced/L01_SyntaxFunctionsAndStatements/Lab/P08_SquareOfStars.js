399a8e7efcac907032ad960ac2430cc4588fee9b
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:05:06 +0200

    Update P08_SquareOfStars.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P08_SquareOfStars.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P08_SquareOfStars.js
@@ -7,4 +7,9 @@ function printRectangle(input) {
         console.log(result);
         result = '';
     }
+
+ /*   const row = "* ".repeat(n).trim()
+    for (let i = 0; i < n; i++) {
+        console.log(row)
+    } */
 }
