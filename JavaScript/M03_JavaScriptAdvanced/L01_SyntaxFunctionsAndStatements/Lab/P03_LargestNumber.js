2002bbbdf4c98b91ac7af00614a305b3ddda7b14
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-26 03:04:55 +0200

    Update P03_LargestNumber.js

--- JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P03_LargestNumber.js
+++ JavaScript/M03_JavaScriptAdvanced/L01_SyntaxFunctionsAndStatements/Lab/P03_LargestNumber.js
@@ -1,4 +1,6 @@
 function findLargestNum(firstNum, secondNum, thirdNum) {
     let result = Math.max(firstNum, secondNum, thirdNum);
     console.log(`The largest number is ${result}.`);
+
+    //    return `The largest number is ${args.sort((a, b) => b - a)[0]}.`
 }
