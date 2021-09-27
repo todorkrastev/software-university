19877b2b0659734e9fbdff5f5bed64ee2d7fce29
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-27 23:00:09 +0200

    Update CollectListItems.js

--- JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Lab/P02_CollectListItems/CollectListItems.js
+++ JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Lab/P02_CollectListItems/CollectListItems.js
@@ -1,5 +1,7 @@
 function extractText() {
     const items = document.getElementById('items').children;
+    // second option
+    // const items = document.querySelectorAll('#items li');
 
     const result = [];
 
@@ -7,4 +9,14 @@ function extractText() {
         result.push(item.textContent);
     }
     document.getElementById('result').textContent = result.join('\n');
+
+    // third option
+    /* const html = {
+        list: () => document.getElementById("items"),
+        result: () => document.getElementById("result"),
+    }
+
+    html.result().innerHTML = Array.from(html.list().children)
+        .map(x => x.innerHTML)
+        .join("\n") */
 }
