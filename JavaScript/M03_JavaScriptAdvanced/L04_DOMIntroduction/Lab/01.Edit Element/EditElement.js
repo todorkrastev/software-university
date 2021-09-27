9d142ce0560cbd6559f8b4276ddcb236950642ed
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-27 22:40:27 +0200

    Rename Folder Name

--- JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Lab/01.Edit Element/EditElement.js
+++
@@ -1,8 +0,0 @@
-function editElement(element, match, replacer) {
-    const matcher = new RegExp(match, 'g');
-    element.textContent = element.textContent.replace(matcher, replacer);
-    // second option
-    // element.textContent = element.textContent.replaceAll(match, replacer);
-    // third option
-    // element.textContent = element.textContent.split(match).join(replacer);
-}
