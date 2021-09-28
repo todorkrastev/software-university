877a8d6d3add67798d35d3d93979e7c8e203d7dd
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-28 19:15:33 +0200

    Create subtract.js

---
+++ JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Exercises/P01_Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let num1 = document.getElementById('firstNumber').value;
+    let num2 = document.getElementById('secondNumber').value;
+
+    let subtract = Number(num1) - Number(num2);
+    document.getElementById('result').innerHTML = subtract.toString();
+}
