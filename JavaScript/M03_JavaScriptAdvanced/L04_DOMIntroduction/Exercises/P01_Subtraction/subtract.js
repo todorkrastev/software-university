851067c17edc4771e911e8dc091fe602daa58323
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-28 20:41:14 +0200

    Update subtract.js

--- JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Exercises/P01_Subtraction/subtract.js
+++ JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Exercises/P01_Subtraction/subtract.js
@@ -4,4 +4,15 @@ function subtract() {
 
     let subtract = Number(num1) - Number(num2);
     document.getElementById('result').innerHTML = subtract.toString();
+
+    // second option
+
+    // const html = {
+    //     firstV: document.getElementById("firstNumber"),
+    //     secondV: document.getElementById("secondNumber"),
+    //     result: document.getElementById("result"),
+    // }
+
+    // html.result.innerHTML =
+    //     (Number(html.firstV.value) || 0) - (Number(html.secondV.value) || 0)
 }
