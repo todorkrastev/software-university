b2951e4263de2fdeed37d6556c723778321baec2
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-29 19:13:05 +0200

    Update solution.js

--- JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Exercises/P05_TableSearchEngine/solution.js
+++ JavaScript/M03_JavaScriptAdvanced/L04_DOMIntroduction/Exercises/P05_TableSearchEngine/solution.js
@@ -16,4 +16,27 @@ function solve() {
       })
       input.value = '';
    }
+
+   // second option
+
+   // const data = {
+   //    collection: document.getElementsByTagName("tr"),
+   //    getValue: () => document.getElementById("searchField").value,
+   // }
+
+   // function onClick({ collection, value }) {
+   //    collection.map(x => (x.className = ""))
+   //    collection.map(x => {
+   //       if (x.innerHTML.includes(value)) x.className = "select"
+
+   //       return x
+   //    })
+   // }
+
+   // document.getElementById("searchBtn").addEventListener("click", () =>
+   //    onClick({
+   //       collection: Array.from(data.collection),
+   //       value: data.getValue(),
+   //    })
+   // )
 }
