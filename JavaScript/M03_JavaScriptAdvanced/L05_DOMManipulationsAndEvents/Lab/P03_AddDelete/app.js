0345f02515124c236c469588dbe67c24e33b7ee0
Todor Krastev <krastevtodordinev@yahoo.com>
2021-09-30 13:04:28 +0200

    Create app.js

---
+++ JavaScript/M03_JavaScriptAdvanced/L05_DOMManipulationsAndEvents/Lab/P03_AddDelete/app.js
@@ -0,0 +1,77 @@
+function addItem() {
+    const input = document.getElementById('newItemText');
+
+    if (input.value.length === 0) {
+        return;
+    }
+
+    const liElement = document.createElement('li');
+    liElement.textContent = input.value;
+
+    const button = document.createElement('a');
+    button.href = '#';
+    button.textContent = '[Delete]';
+    button.addEventListener('click', removeElement);
+
+    liElement.appendChild(button);
+
+    document.getElementById('items').appendChild(liElement);
+
+    input.value = '';
+
+    function removeElement(ev) {
+        //  let result = confirm('Are you sure you want to delete this record?');
+        //   if (result) {
+        const parent = ev.target.parentNode;
+        parent.remove();
+        //   }
+    }
+
+    // second option
+
+    // let input = document.getElementById('newItemText');
+    // if (input.value.length === 0) {
+    //     return;
+    // }
+    // let li = document.createElement('li');
+    // li.appendChild(document.createTextNode(input.value)); // -> li.textContent = input.value;
+    // let listItems = document.getElementById('items');
+    // let remove = document.createElement('a');
+    // let linkText = document.createTextNode("[Delete]");
+    // remove.appendChild(linkText);
+    // remove.href = '#';
+    // remove.addEventListener("click", deleteItem);
+
+    // li.appendChild(remove);
+    // listItems.appendChild(li);
+
+    // function deleteItem() {
+    //     li.remove();
+    // }
+
+    // input.value = '';
+
+    // third option 
+
+    // const data = {
+    //     valueToAdd: document.getElementById("newText").value,
+    //     list: document.getElementById("items"),
+    // }
+
+    // function eFactory(tag, content) {
+    //     const temp = document.createElement(tag)
+    //     temp.innerHTML = content
+
+    //     return temp
+    // }
+
+    // const item = eFactory("li", data.valueToAdd)
+    // const deleteLink = eFactory("a", "[Delete]")
+    // deleteLink.href = "#"
+    // deleteLink.addEventListener("click", e => e.target.parentNode.remove())
+
+    // item.appendChild(deleteLink)
+
+    // data.list.appendChild(item)
+
+}
