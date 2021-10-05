02e2beb8110d37bd925285d36128b90283112c2f
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-05 17:40:34 +0200

    Update P01_AreaAndVolumeCalculator.js

--- JavaScript/M03_JavaScriptAdvanced/L06_AdvancedFunctions/Lab/P01_AreaAndVolumeCalculator.js
+++ JavaScript/M03_JavaScriptAdvanced/L06_AdvancedFunctions/Lab/P01_AreaAndVolumeCalculator.js
@@ -14,17 +14,28 @@ function solve(area, vol, input) {
     })
 
     return result;
+
+
+
+    // modification of the first option
+
+
+    //     return JSON.parse(input)
+    //         .map(figure => ({
+    //             area: area.call(figure),
+    //             volume: vol.call(figure)
+    //         }));
+
+
+
+    // using Reduce
+
+
+    // return JSON.parse(inputs).reduce((a, v, i, obj) => {
+    //     a.push({
+    //         area: area.call(v),
+    //         volume: volume.call(v),
+    //     });
+    //     return a;
+    // }, []);
 }
-
-
-
-// modification
-
-
-// function solve(area, vol, input) {
-//     return JSON.parse(input)
-//         .map(figure => ({
-//             area: area.call(figure),
-//             volume: vol.call(figure)
-//         }));
-// }
