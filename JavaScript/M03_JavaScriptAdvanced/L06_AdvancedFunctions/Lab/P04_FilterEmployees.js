922bfb43a15d51074f3b14e02e5a162a2900a9e0
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-05 22:24:13 +0200

    Update P04_FilterEmployees.js

--- JavaScript/M03_JavaScriptAdvanced/L06_AdvancedFunctions/Lab/P04_FilterEmployees.js
+++ JavaScript/M03_JavaScriptAdvanced/L06_AdvancedFunctions/Lab/P04_FilterEmployees.js
@@ -1,9 +1,25 @@
 function solve(data, criteria) {
-    const parsed = JSON.parse(data);
-    const [crit, specificCrit] = criteria.split("-");
+    let [prefix, value] = criteria.split('-');
+    let counter = 0;
 
-    return parsed
-        .filter(x => x[crit] === specificCrit)
-        .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
-        .join("\n");
+    JSON.parse(data).forEach(person => selectByCriteria.call(person));
+
+    function selectByCriteria() {
+        if (this[prefix] === value || criteria === 'all') {
+            return console.log(`${counter++}. ${this.first_name} ${this.last_name} - ${this.email}`);
+        }
+    }
+
+
+    
+     // second option
+
+
+    // const parsed = JSON.parse(data);
+    // const [crit, specificCrit] = criteria.split("-");
+
+    // return parsed
+    //     .filter(x => x[crit] === specificCrit || x[crit] === 'all')
+    //     .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+    //     .join("\n");
 }
