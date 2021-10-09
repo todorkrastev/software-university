d33b501a1f8d007aac496b46cebb0e8335243322
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-09 20:56:25 +0200

    Create GetPerson.js

---
+++ JavaScript/M03_JavaScriptAdvanced/L08_Classes/Lab/P02_GetPersons/GetPerson.js
@@ -0,0 +1,21 @@
+function getPerson() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    let arr = [];
+    arr.push(new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'))
+    arr.push(new Person('SoftUni'))
+    arr.push(new Person('Stephan', 'Johnson', 25))
+    arr.push(new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com'))
+    return arr;
+}
