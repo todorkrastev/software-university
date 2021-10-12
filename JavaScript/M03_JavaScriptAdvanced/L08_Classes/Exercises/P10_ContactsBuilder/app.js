491bd83f19f452f83a87579c005334d6f72f32a3
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-12 23:13:50 +0100

    Create app.js

---
+++ JavaScript/M03_JavaScriptAdvanced/L08_Classes/Exercises/P10_ContactsBuilder/app.js
@@ -0,0 +1,54 @@
+class Contact {
+    constructor(f, l, p, e) {
+        this.firstName = f;
+        this.lastName = l;
+        this.phone = p;
+        this.email = e;
+        this._online = false;
+    }
+
+    get online() {
+        return this._online;
+    }
+
+    set online(v) {
+        this._online = v;
+
+        if (this.onlineDiv) {
+            this.onlineDiv.className = this._online ? 'title online' : 'title';
+        }
+    }
+
+    eFactory(tag, content = '') {
+        const e = document.createElement(tag);
+        e.innerHTML = content;
+
+        return e;
+    }
+
+    render(id) {
+        this.templ = this.eFactory('article');
+        this.onlineDiv = this.eFactory('div', `${this.firstName} ${this.lastName}`);
+        this.infoBtn = this.eFactory('button', '&#8505;');
+        this.infoDiv =
+            this.eFactory(
+                'div',
+                `<span>&phone; ${this.phone}</span><span>&#9993; ${this.email}</span>`
+            );
+
+
+        this.onlineDiv.className = this.online ? 'title online' : 'title';
+        this.infoDiv.className = 'info';
+        this.infoDiv.style.display = 'none';
+
+        this.onlineDiv.appendChild(this.infoBtn);
+        this.templ.appendChild(this.onlineDiv);
+        this.templ.appendChild(this.infoDiv);
+
+        document.getElementById(id).appendChild(this.templ);
+
+        this.infoBtn.addEventListener('click', () => {
+            this.infoDiv.style.display = this.infoDiv.style.display === 'none' ? 'block' : 'none';
+        });
+    }
+}
