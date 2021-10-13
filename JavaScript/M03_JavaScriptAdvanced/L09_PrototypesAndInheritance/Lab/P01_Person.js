cfd16d633201ebd68409428bdc4c04ca26204d65
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-13 20:01:05 +0100

    Create P01_Person.js

---
+++ JavaScript/M03_JavaScriptAdvanced/L09_PrototypesAndInheritance/Lab/P01_Person.js
@@ -0,0 +1,24 @@
+function createPerson(firstName, lastName) {
+    const result = {
+        firstName,
+        lastName,
+        fullName: ''
+    };
+
+    Object.defineProperty(result, 'fullName', {
+        get() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        set(value) {
+            const [first, last] = value.split(' ');
+            if (first != undefined && last != undefined) {
+                this.firstName = first;
+                this.lastName = last;
+            }
+        },
+        configurable: true,
+        enumerable: true
+    });
+
+    return result;
+}
