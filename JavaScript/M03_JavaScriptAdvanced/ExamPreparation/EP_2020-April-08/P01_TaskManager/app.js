50eb2f12edb4324bb3a1f565802c233d47fe79e1
Todor Krastev <krastevtodordinev@yahoo.com>
2021-10-21 22:31:51 +0200

    Update app.js

--- JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2020-April-08/P01_TaskManager/app.js
+++ JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2020-April-08/P01_TaskManager/app.js
@@ -81,3 +81,82 @@ function solve() {
         completedTask.appendChild(article);
     }
 }
+
+
+
+// second option
+
+
+// function solve () {
+// 	const inputFields = [
+// 		document.getElementById(`task`),
+// 		document.getElementById(`description`),
+// 		document.getElementById(`date`),
+// 	]
+
+// 	const sections = Array.from(document.getElementsByTagName('section'))
+// 		.slice(1)
+// 		.map(x => x.children[1])
+
+// 	function articleTemplFactory (b, c, b1, c1, n, d, da) {
+// 		const article = document.createElement('article')
+
+// 		article.innerHTML = `<h3>${n}</h3><p>${d}</p>
+// <p>${da}</p>`
+
+// 		article.innerHTML += b !== ''
+// 			? `<div class="flex">
+// <button class=${c}>${b}</button>
+// <button class=${c1}>${b1}</button>
+// </div>`
+// 			: ''
+
+// 		return article
+// 	}
+
+// 	const isValidInput = (arr) => arr.every(x => x !== '')
+// 	const clearInput = (arr) => arr.forEach(x => x.value = '')
+// 	const remove = (e) => e.parentNode.parentNode.outerHTML = ''
+
+// 	const appendNewItem = (s, b, c, b1, c1, n, d, da) =>
+// 		s.appendChild(articleTemplFactory(b, c, b1, c1, n, d, da))
+
+// 	const add = appendNewItem.bind(undefined, sections[0], 'Start', 'green', 'Delete', 'red',)
+// 	const start = appendNewItem.bind(undefined, sections[1], 'Delete', 'red', 'Finish', 'orange',)
+// 	const finish = appendNewItem.bind(undefined, sections[2], '', '', '', '')
+
+// 	document.addEventListener('click', (e) => {
+// 		if (e.target.tagName === 'BUTTON') {
+// 			e.preventDefault()
+
+// 			const action = e.target.className
+// 			const values = inputFields.map(x => x.value)
+
+// 			const workValues = [values[0], `Description: ${values[1]}`, `Due Date: ${values[2]}`]
+
+// 			const getData = () =>
+// 				Array.from(e.target.parentNode.parentNode.children)
+// 					.map(x => x.innerHTML)
+
+// 			const actions = {
+// 				'': () => {
+// 					if (isValidInput(values)) {
+// 						add(...workValues)
+// 						clearInput(inputFields)
+// 					}
+// 				},
+// 				'green': () => {
+// 					start(...getData())
+// 					remove(e.target)
+// 				},
+// 				'orange': () => {
+// 					finish(...getData())
+// 					remove(e.target)
+// 				},
+// 				'red': () => remove(e.target)
+// 			}
+
+// 			actions[action]()
+// 		}
+// 	})
+// }
