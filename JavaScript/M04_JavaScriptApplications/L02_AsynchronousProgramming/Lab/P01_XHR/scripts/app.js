17dc596a1d2cf1485ab17adba3bf9742284bd039
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-01 17:26:08 +0100

    Create app.js

---
+++ JavaScript/M04_JavaScriptApplications/L02_AsynchronousProgramming/Lab/P01_XHR/scripts/app.js
@@ -0,0 +1,14 @@
+function loadRepos() {
+   let button = document.querySelector('button[onclick="loadRepos()"]');
+   button.addEventListener('click', function loadRepos() {
+      let url = 'https://api.github.com/users/testnakov/repos';
+      const httpRequest = new XMLHttpRequest();
+      httpRequest.addEventListener('readystatechange', function () {
+         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            document.getElementById("res").textContent = httpRequest.responseText;
+         }
+      });
+      httpRequest.open("GET", url);
+      httpRequest.send();
+   });
+}
