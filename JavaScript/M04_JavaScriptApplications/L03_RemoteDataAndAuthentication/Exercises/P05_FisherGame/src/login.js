20f11d657bb85fb7b35f6d9621a6a3870a1f5904
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-08 21:43:52 +0100

    Update login.js

--- JavaScript/M04_JavaScriptApplications/L03_RemoteDataAndAuthentication/Exercises/P05_FisherGame/src/login.js
+++ JavaScript/M04_JavaScriptApplications/L03_RemoteDataAndAuthentication/Exercises/P05_FisherGame/src/login.js
@@ -1,1 +1,37 @@
-console.log('TODO:// Implement Login functionality');
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', login);
+})
+
+async function login(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+        const response = await fetch('http://localhost:3030/users/login', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok != true) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        const data = await response.json();
+        const userData = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken
+        };
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+        window.location = './index.html';
+    }
+    catch (error) {
+        alert(error.message);
+    }
+}
