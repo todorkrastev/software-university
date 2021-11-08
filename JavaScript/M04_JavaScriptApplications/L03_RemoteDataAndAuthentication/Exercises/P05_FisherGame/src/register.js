13a351450e31e9e63e8e9f1e9ee95052f5af2b79
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-08 21:43:56 +0100

    Update register.js

--- JavaScript/M04_JavaScriptApplications/L03_RemoteDataAndAuthentication/Exercises/P05_FisherGame/src/register.js
+++ JavaScript/M04_JavaScriptApplications/L03_RemoteDataAndAuthentication/Exercises/P05_FisherGame/src/register.js
@@ -1,1 +1,47 @@
-console.log('TODO:// Implement Register functionality');
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', register);
+})
+
+async function register(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const rePass = formData.get('rePass');
+
+    try {
+        if (email == '' || password == '') {
+            return alert('All field must be filled!');
+        } else if (password != rePass) {
+            return alert("Passwords don't match!");
+        }
+
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (!response.ok) {
+            const error = await response.json();
+            return alert(error.message);
+        }
+
+        const data = await response.json();
+        const userData = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken
+        };
+
+        sessionStorage.setItem('userData', JSON.stringify(userData));
+        window.location = './index.html';
+
+        alert('Registered successfully!');
+        event.target.reset();
+    }
+    catch (error) {
+        alert(error.message);
+    }
+}
