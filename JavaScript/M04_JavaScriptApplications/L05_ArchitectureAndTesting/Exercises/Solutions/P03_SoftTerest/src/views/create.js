6315c379afd7e56edf4010c8f0df8a6ab2800c25
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-19 04:27:34 +0100

    Create create.js

---
+++ JavaScript/M04_JavaScriptApplications/L05_ArchitectureAndTesting/Exercises/Solutions/P03_SoftTerest/src/views/create.js
@@ -0,0 +1,35 @@
+import { createIdea } from '../api/data.js';
+
+const section = document.getElementById('createPage');
+section.remove();
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
+
+export async function showCreatePage(ctxTarget) {
+    ctx = ctxTarget;
+    ctx.showSection(section);
+}
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const title = formData.get('title').trim();
+    const description = formData.get('description').trim();
+    const img = formData.get('imageURL').trim();
+
+    if (title.length < 6) {
+        return alert('Title must be at least 6 characters long');
+    }
+    if (description.length < 10) {
+        return alert('Description must be at least 10 characters long');
+    }
+    if (img.length < 5) {
+        return alert('Image URL must be at least 5 characters long');
+    }
+
+    createIdea({ title, description, img });
+    form.reset(); 
+    ctx.goTo('catalog');
+}
