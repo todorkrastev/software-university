bfd133b197873c80916a9083b194a943a7c7356e
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-19 04:27:47 +0100

    Create login.js

---
+++ JavaScript/M04_JavaScriptApplications/L05_ArchitectureAndTesting/Exercises/Solutions/P03_SoftTerest/src/views/login.js
@@ -0,0 +1,25 @@
+import { login } from '../api/api.js';
+
+const section = document.getElementById('loginPage');
+section.remove();
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
+
+export async function showLoginPage(ctxTarget) {
+    ctx = ctxTarget;
+    ctx.showSection(section);
+}
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const email = formData.get('email').trim();
+    const password = formData.get('password').trim();
+
+    await login(email, password);
+    form.reset();
+    ctx.goTo('home');
+    ctx.updateNav();
+}
