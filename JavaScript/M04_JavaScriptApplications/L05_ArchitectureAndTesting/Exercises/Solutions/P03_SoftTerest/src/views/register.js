cb7879488544a57dbbdaed1546d06c0f59bb3ecc
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-19 04:27:51 +0100

    Create register.js

---
+++ JavaScript/M04_JavaScriptApplications/L05_ArchitectureAndTesting/Exercises/Solutions/P03_SoftTerest/src/views/register.js
@@ -0,0 +1,34 @@
+import { register } from '../api/data.js';
+
+const section = document.getElementById('registerPage');
+section.remove();
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
+
+export async function showRegisterPage(ctxTarget) {
+    ctx = ctxTarget;
+    ctx.showSection(section);
+}
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const email = formData.get('email').trim();
+    const password = formData.get('password').trim();
+    const repass = formData.get('repeatPassword').trim();
+
+    if (!email || !password) {
+        return alert('All fields are requred!');
+    }
+
+    if (password != repass) {
+        return alert('Passwords don\'t match!');
+    }
+
+    await register(email, password);
+    form.reset();
+    ctx.goTo('home');
+    ctx.updateNav();
+}
