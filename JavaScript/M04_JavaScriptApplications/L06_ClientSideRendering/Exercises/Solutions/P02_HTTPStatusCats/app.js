4b77e67c1e9e0e6c32974c59bfa49c2a22afba44
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-21 20:21:57 +0100

    Create app.js

---
+++ JavaScript/M04_JavaScriptApplications/L06_ClientSideRendering/Exercises/Solutions/P02_HTTPStatusCats/app.js
@@ -0,0 +1,30 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { cats as catData } from './catSeeder.js';
+
+const catCard = (cat) => html`
+<li>
+    <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+    <div class="info">
+        <button @click=${() => toggleInfo(cat)} class="showBtn">${cat.info ? 'Hide' : 'Show'} status code</button>
+        ${cat.info ? html`<div class="status" id="${cat.id}">
+            <h4>Status Code: ${cat.statusCode}</h4>
+            <p>${cat.statusMessage}</p>
+        </div>` :
+            null}
+    </div>
+</li>
+`;
+
+const root = document.getElementById('allCats');
+catData.forEach(c => c.info = false);
+update();
+
+function update() {
+    render(html`<ul>${catData.map(catCard)}</ul>`, root);
+}
+
+
+function toggleInfo(cat) {
+    cat.info = !cat.info;
+    update();
+}
