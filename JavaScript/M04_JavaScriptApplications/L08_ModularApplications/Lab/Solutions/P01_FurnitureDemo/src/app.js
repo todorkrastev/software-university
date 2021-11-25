f96230546bc3f7c50fc796a7d0fb2301bc1d9ae5
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-25 01:11:02 +0100

    Create app.js

---
+++ JavaScript/M04_JavaScriptApplications/L08_ModularApplications/Lab/Solutions/P01_FurnitureDemo/src/app.js
@@ -0,0 +1,48 @@
+import { logout } from './api/api.js';
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+
+const root = document.querySelector('div.container');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', catalogPage);
+page('/details/:id', detailsPage);
+page('/create', createPage);
+page('/edit/:id', editPage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/my-furniture', catalogPage);
+
+updateUserNav();
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+async function onLogout() {
+    await logout();
+    updateUserNav();
+    page.redirect('/');
+}
