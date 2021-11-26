c0d6b62f1950971fc1b0a21ffd7f91a99f366718
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-26 02:16:45 +0100

    Create lib.js

---
+++ JavaScript/M04_JavaScriptApplications/L08_ModularApplications/Exercises/Solutions/P01_ForumDemo/src/lib.js
@@ -0,0 +1,12 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { until } from '../node_modules/lit-html/directives/until.js';
+import { classMap } from '../node_modules/lit-html/directives/class-map.js';
+import page from '../node_modules/page/page.mjs';
+
+export {
+    html,
+    render,
+    until,
+    classMap,
+    page
+};
