9601c7f1603b5c29863cd265859d29f05f1aeeb8
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-28 23:40:51 +0100

    Update solution.js

--- JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-06-27/P01_TravelAgency/solution.js
+++ JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-06-27/P01_TravelAgency/solution.js
@@ -67,3 +67,106 @@ function solution() {
     document.querySelector('#block').innerHTML = `<h3>Thank you for your reservation!</h3>`;
   }
 }
+
+
+
+// refactoring
+
+/*
+function solution() {
+  let fullNameField = document.getElementById('fname');
+  let emailField = document.getElementById('email');
+  let phoneNumberField = document.getElementById('phone');
+  let addressField = document.getElementById('address');
+  let postalCodeField = document.getElementById('code');
+  const submitBtn = document.getElementById('submitBTN');
+  const editBtn = document.getElementById('editBTN');
+  const continueBtn = document.getElementById('continueBTN');
+
+  submitBtn.addEventListener('click', submitHandler);
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const fullName = fullNameField.value;
+    const email = emailField.value;
+    const phone = phoneNumberField.value;
+    const address = addressField.value;
+    const postalCode = postalCodeField.value;
+
+    if (fullName == '' && email == '') {
+      return;
+    }
+
+    const ul = document.querySelector('#infoPreview');
+
+    ul.appendChild(e('li', {}, `Full Name: ${fullName}`));
+    ul.appendChild(e('li', {}, `Email: ${email}`));
+    ul.appendChild(e('li', {}, `Phone Number: ${phone}`));
+    ul.appendChild(e('li', {}, `Address: ${address}`));
+    ul.appendChild(e('li', {}, `Postal Code: ${postalCode}`));
+
+
+    fullNameField.value = '';
+    emailField.value = '';
+    phoneNumberField.value = '';
+    addressField.value = '';
+    postalCodeField.value = '';
+
+    submitBtn.disabled = true;
+    editBtn.disabled = false;
+    continueBtn.disabled = false;
+
+    editBtn.addEventListener('click', () => {
+      fullNameField.value = fullName;
+      emailField.value = email;
+      phoneNumberField.value = phone;
+      addressField.value = address;
+      postalCodeField.value = postalCode;
+
+      submitBtn.disabled = false;
+      editBtn.disabled = true;
+      continueBtn.disabled = true;
+
+      // ul.remove();
+      // document.querySelector('.preview').appendChild(e('ul', { id: 'infoPreview' }));
+      let inputs = Array.from(event.target.parentElement.parentElement.querySelectorAll('li'));
+      inputs.forEach(el => el.remove());
+
+    });
+    continueBtn.addEventListener('click', () => {
+      const parent = document.getElementById("block");
+      while (parent.firstChild) {
+        parent.firstChild.remove();
+      }
+      parent.appendChild(e('h3', {}, 'Thank you for your reservation!'));
+    });
+  }
+
+
+
+  function e(type, attributes, ...content) {
+    const result = document.createElement(type);
+
+    for (let [attr, value] of Object.entries(attributes || {})) {
+      if (attr.substring(0, 2) == 'on') {
+        result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
+      } else {
+        result[attr] = value;
+      }
+    }
+
+    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
+
+    content.forEach(e => {
+      if (typeof e == 'string' || typeof e == 'number') {
+        const node = document.createTextNode(e);
+        result.appendChild(node);
+      } else {
+        result.appendChild(e);
+      }
+    });
+
+    return result;
+  }
+}
+ */
