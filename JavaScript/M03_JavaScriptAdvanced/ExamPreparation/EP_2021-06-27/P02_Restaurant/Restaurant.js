b11885222c86e17872691c29e4f52e0595196bcf
beinsaduno <krastevtodordinev@yahoo.com>
2021-11-30 00:49:04 +0100

    Update Restaurant.js

--- JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-06-27/P02_Restaurant/Restaurant.js
+++ JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-06-27/P02_Restaurant/Restaurant.js
@@ -1,6 +1,6 @@
 class Restaurant {
-    constructor(budgetMoney) {
-        this.budgetMoney = budgetMoney;
+    constructor(budget) {
+        this.budgetMoney = budget;
         this.menu = {};
         this.stockProducts = {};
         this.history = [];
@@ -8,37 +8,75 @@ class Restaurant {
 
     loadProducts(products) {
         for (const product of products) {
-            let currName = product.split(' ')[0];
-            let currQuantity = product.split(' ')[1];
-            let currPrice = Number(product.split(' ')[2]);
+            let [productName, productQuantity, productPrice] = product.split(' ');
+            productQuantity = Number(productQuantity);
+            productPrice = Number(productPrice);
 
-            if (currQuantity * currPrice <= this.budgetMoney) {
-                if()
-                this.stockProducts[currName] = `${currName} ${currQuantity}`;
-
+            if (productPrice <= this.budgetMoney) {
+                if (this.stockProducts.hasOwnProperty(productName)) {
+                    this.stockProducts[productName] += productQuantity;
+                } else {
+                    this.stockProducts[productName] = productQuantity;
+                }
+                this.budgetMoney -= productPrice;
+                this.history.push(`Successfully loaded ${productQuantity} ${productName}`);
+            } else {
+                this.history.push(`There was not enough money to load ${productQuantity} ${productName}`);
             }
+        }
+        return this.history.join('\n');
+    }
+
+    addToMenu(meal, neededProducts, price) {
+        if (this.menu.hasOwnProperty(meal)) {
+            return `The ${meal} is already in the our menu, try something different.`;
+        }
+        this.menu[meal] = {
+            neededProducts,
+            price
+        };
+        if (Object.keys(this.menu).length == 1) {
+            return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        } else {
+            return `Great idea! Now with the ${meal} we have ${Object.keys(this.menu).length} meals in the menu, other ideas?`;
         }
     }
 
-    addToMenu(meal, products, price) {
-
-    }
-
     showTheMenu() {
+        if (Object.keys(this.menu).length == 0) {
+            return `Our menu is not ready yet, please come later...`;
+        }
 
+        const output = [];
+        for (const [meal, object] of Object.entries(this.menu)) {
+            output.push(`${meal} - $ ${object.price}`);
+        }
+
+        return output.join('\n');
     }
 
-    makeTheOrder(meal) {
+    makeTheOrder(mealName) {
+        if (this.menu.hasOwnProperty(mealName) == false) {
+            return `There is not ${mealName} yet in our menu, do you want to order something else?`;
+        }
 
+        const meal = this.menu[mealName];
+
+        for (const element of meal.neededProducts) {
+            const [singleProduct, quantity] = element.split(' ');
+
+            if (this.stockProducts.hasOwnProperty(singleProduct) == false || this.stockProducts[singleProduct] < quantity) {
+                return `For the time being, we cannot complete your order (${mealName}), we are very sorry...`;
+            }
+        }
+
+        for (const element of meal.neededProducts) {
+            const [singleProduct, quantity] = element.split(' ');
+
+            this.stockProducts[singleProduct] -= quantity;
+            this.budgetMoney += meal.price;
+        }
+
+        return `Your order (${mealName}) will be completed in the next 30 minutes and will cost you ${meal.price}.`;
     }
 }
-
-let kitchen = new Restaurant(1000);
-console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
-
-// Successfully loaded 10 Banana
-// Successfully loaded 20 Banana
-// Successfully loaded 50 Strawberries
-// Successfully loaded 10 Yogurt
-// There was not enough money to load 500 Yogurt
-// Successfully loaded 5 Honey
