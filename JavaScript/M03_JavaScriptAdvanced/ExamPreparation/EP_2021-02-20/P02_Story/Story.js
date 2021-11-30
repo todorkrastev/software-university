384145314b940cceb9b7e3f96008bc8ea1663421
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-01 00:16:55 +0100

    Update Story.js

--- JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-02-20/P02_Story/Story.js
+++ JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2021-02-20/P02_Story/Story.js
@@ -1,3 +1,107 @@
+class Story {
+    constructor(title, creator) {
+        this.title = title;
+        this.creator = creator;
+        this._comments = [];
+        this._likes = [];
+    }
+
+    get likes() {
+        if (this._likes.length === 0) {
+            return `${this.title} has 0 likes`;
+        } else if (this._likes.length === 1) {
+            return `${this._likes[0]} likes this story!`;
+        }
+        return `${this._likes[0]} and ${this._likes.length - 1} others like this story!`;
+
+    }
+
+    like(username) {
+        if (this._likes.includes(username)) {
+            throw new Error('You can\'t like the same story twice!');
+        } else if (this.creator === username) {
+            throw new Error('You can\'t like your own story!');
+        }
+
+        this._likes.push(username);
+
+        return `${username} liked ${this.title}!`;
+    }
+
+    dislike(username) {
+        if (this._likes.includes(username) === false) {
+            throw new Error(`You can't dislike this story!`);
+        }
+
+        let index = this._likes.indexOf(username);
+        this._likes.splice(index, 1);
+        return `${username} disliked ${this.title}`;
+    }
+
+    comment(username, content, id) {
+        if (id === undefined || this._comments.some(x => x.id === id) === false) {
+            let comment = {
+                id: this._comments.length + 1,
+                username,
+                content,
+                replies: [],
+            };
+            this._comments.push(comment);
+
+            return `${username} commented on ${this.title}`;
+        }
+
+        let comment = this._comments.find(x => x.id === id);
+        let reply = {
+            id: comment.replies.length + 1,
+            username,
+            content,
+        };
+
+        comment.replies.push(reply);
+
+        return `You replied successfully`;
+    }
+
+    toString(sortingType) {
+        let output = '';
+        output += `Title: ${this.title}\n`;
+        output += `Creator: ${this.creator}\n`;
+        output += `Likes: ${this._likes.length}\n`;
+        output += 'Comments:';
+
+        if (this._comments.length > 0) {
+            if (sortingType === 'asc') {
+                for (const comment of this._comments.sort((a, b) => a.id - b.id)) {
+                    output += `\n-- ${comment.id}. ${comment.username}: ${comment.content}`;
+                    for (const reply of comment.replies.sort((a, b) => a.id - b.id)) {
+                        output += `\n--- ${comment.id}.${reply.id}. ${reply.username}: ${reply.content}`;
+                    }
+                }
+            } else if (sortingType === 'desc') {
+                for (const comment of this._comments.sort((a, b) => b.id - a.id)) {
+                    output += `\n-- ${comment.id}. ${comment.username}: ${comment.content}`;
+                    for (const reply of comment.replies.sort((a, b) => b.id - a.id)) {
+                        output += `\n--- ${comment.id}.${reply.id}. ${reply.username}: ${reply.content}`;
+                    }
+                }
+            } else if (sortingType === 'username') {
+                for (const comment of this._comments.sort((a, b) => a.username.localeCompare(b.username))) {
+                    output += `\n-- ${comment.id}. ${comment.username}: ${comment.content}`;
+                    for (const reply of comment.replies.sort((a, b) => a.username.localeCompare(b.username))) {
+                        output += `\n--- ${comment.id}.${reply.id}. ${reply.username}: ${reply.content}`;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+}
+
+
+// second option
+
+
 // class Story {
 //     constructor(title, creator) {
 //         this.title = title;
@@ -112,6 +216,10 @@
 //     }
 // }
 
+
+// third option
+
+/*
 class Story {
     constructor(title, creator) {
         this.title = title;
@@ -214,3 +322,4 @@ Likes: ${this._likes.length}
 Comments:${fullCommentsString}`;
     }
 }
+ */
