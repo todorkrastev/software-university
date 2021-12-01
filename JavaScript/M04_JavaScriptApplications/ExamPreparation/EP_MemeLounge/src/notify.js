cfa902a1bf4b8fd1ffd320c8aeb2065bca0ff6b2
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-02 00:02:47 +0100

    Create notify.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_MemeLounge/src/notify.js
@@ -0,0 +1,9 @@
+const element = document.getElementById('errorBox');
+const output = element.querySelector('span');
+
+export function notify(msg) {
+    output.textContent = msg;
+    element.style.display = 'block';
+
+    setTimeout(() => element.style.display = 'none', 3000);
+}
