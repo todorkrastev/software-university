ca8553e24807a38f53d40359bb96062a3a7b824f
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-03 00:03:59 +0100

    Create app.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_OnlineBooksLibrary/src/app.js
@@ -0,0 +1,57 @@
+import { logout } from './api/data.js';
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { myBooksPage } from './views/my-books.js';
+import { registerPage } from './views/register.js';
+
+// DEBUG
+
+/*
+import * as api from './api/api.js';
+window.api = api;
+*/
+
+const root = document.getElementById('site-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/my-books', myBooksPage);
+
+updateUserNav();
+page.start();
+
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('guest').style.display = 'none';
+        document.querySelector('#user span').textContent = `Welcome, ${userData.email}`;
+    } else {
+        document.getElementById('user').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
