3e8df234fb34d0a1affa924fb75126b573b4c3e3
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-03 21:11:27 +0100

    Create login.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_CarsTube/src/views/login.js
@@ -0,0 +1,47 @@
+import { login } from '../api/api.js';
+import { html } from '../lib.js';
+
+const loginTemplate = (onSubmit) => html`
+<section id="login">
+    <div class="container">
+        <form @submit=${onSubmit} id="login-form" action="#" method="post">
+            <h1>Login</h1>
+            <p>Please enter your credentials.</p>
+            <hr>
+
+            <p>Username</p>
+            <input placeholder="Enter Username" name="username" type="text">
+
+            <p>Password</p>
+            <input type="password" placeholder="Enter Password" name="password">
+            <input type="submit" class="registerbtn" value="Login">
+        </form>
+        <div class="signin">
+            <p>Dont have an account?
+                <a href="/register">Sign up</a>.
+            </p>
+        </div>
+    </div>
+</section>
+`;
+
+export function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const username = formData.get('username').trim();
+        const password = formData.get('password').trim();
+
+        if (username == '' || password == '') {
+            return alert('Please fill both fields!');
+        }
+
+        await login(username, password);
+        event.target.reset();
+        ctx.updateUserNav();
+        ctx.page.redirect('/');
+    }
+}
