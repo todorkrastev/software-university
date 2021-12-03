40c560e47ceb9ea9337731f331a5371a3ac5aefb
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-03 07:43:26 +0100

    Create login.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_GamesPlay/src/views/login.js
@@ -0,0 +1,43 @@
+import { login } from '../api/api.js';
+import { html } from '../lib.js';
+
+const loginTemplate = (onSubmit) => html`
+<section id="login-page" class="auth">
+    <form @submit=${onSubmit} id="login">
+
+        <div class="container">
+            <div class="brand-logo"></div>
+            <h1>Login</h1>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Sokka@gmail.com">
+
+            <label for="login-pass">Password:</label>
+            <input type="password" id="login-password" name="password">
+            <input type="submit" class="btn submit" value="Login">
+            <p class="field">
+                <span>If you don't have profile click <a href="/register">here</a></span>
+            </p>
+        </div>
+    </form>
+</section>
+`;
+
+export function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if (email == '' || password == '') {
+            return alert('Please fill both fields!');
+        }
+
+        await login(email, password);
+        ctx.updateUserNav();
+        ctx.page.redirect('/');
+    }
+}
