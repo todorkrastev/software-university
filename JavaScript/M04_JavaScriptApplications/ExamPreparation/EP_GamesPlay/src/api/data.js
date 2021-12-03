c599d2760fe228dba3cc5bb74246039b4d82ed4b
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-03 07:42:55 +0100

    Create data.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_GamesPlay/src/api/data.js
@@ -0,0 +1,29 @@
+import * as api from './api.js';
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+export async function getAllGames() {
+    return api.get('/data/games?sortBy=_createdOn%20desc');
+}
+
+export async function getRecentGames() {
+    return api.get('/data/games?sortBy=_createdOn%20desc&distinct=category');
+}
+
+export async function createGame(game) {
+    return api.post('/data/games', game);
+}
+
+export async function getGameById(id) {
+    return api.get('/data/games/' + id);
+}
+
+export async function editGame(id, game) {
+    return api.put('/data/games/' + id, game);
+}
+
+export async function deleteGame(id) {
+    return api.del('/data/games/' + id);
+}
