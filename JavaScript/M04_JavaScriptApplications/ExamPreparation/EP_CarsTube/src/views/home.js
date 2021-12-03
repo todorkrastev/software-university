c53560b85f90f8f793aebe016a4197ac05ede887
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-03 21:11:23 +0100

    Create home.js

---
+++ JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_CarsTube/src/views/home.js
@@ -0,0 +1,21 @@
+import { html } from '../lib.js';
+
+
+const homeTemplate = () => html`
+<section id="main">
+    <div id="welcome-container">
+        <h1>Welcome To Car Tube</h1>
+        <img class="hero" src="/images/car-png.webp" alt="carIntro">
+        <h2>To see all the listings click the link below:</h2>
+        <div>
+            <a href="/catalogue" class="button">Listings</a>
+        </div>
+    </div>
+</section>
+`;
+
+
+
+export async function homePage(ctx) {
+    ctx.render(homeTemplate());
+}
