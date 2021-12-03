9f5cfa447dd328e2c8a383e6e6543a307d56db19
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-04 00:44:34 +0100

    Rename Folder Name

--- JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_MemeLounge/src/lib.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-import page from '../node_modules/page/page.mjs';
-
-export {
-    html,
-    render,
-    page
-};
--- JavaScript/M04_JavaScriptApplications/ExamPreparation/EP_MemeLounge/src/notify.js
+++
@@ -1,9 +0,0 @@
-const element = document.getElementById('errorBox');
-const output = element.querySelector('span');
-
-export function notify(msg) {
-    output.textContent = msg;
-    element.style.display = 'block';
-
-    setTimeout(() => element.style.display = 'none', 3000);
-}
