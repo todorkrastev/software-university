1e48b2387cd6f3e5556367bed2d2b2093805a356
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-09 15:12:27 +0100

    Update app.js

--- JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2020-04-08/P01_TaskManager/app.js
+++ JavaScript/M03_JavaScriptAdvanced/ExamPreparation/EP_2020-04-08/P01_TaskManager/app.js
@@ -86,6 +86,192 @@ function solve() {
 
 // second option
 
+/*
+function solve() {
+    let wrapper = document.querySelectorAll('.wrapper section');
+    let open = wrapper[1].querySelectorAll('div')[1];
+    let inProgress = wrapper[2].querySelectorAll('div')[1];
+    let complete = wrapper[3].querySelectorAll('div')[1];
+    let addBtn = document.querySelector('#add');
+    addBtn.addEventListener('click', onClick);
+
+    function onClick(event) {
+        event.preventDefault();
+
+        let task = document.querySelector('#task');
+        let description = document.querySelector('#description');
+        let date = document.querySelector('#date');
+
+
+        if (task != '' && description != '' && date != '') {
+            let startBtn = e('button', { className: 'green' }, 'Start');
+            let deleteBtn = e('button', { className: 'red' }, 'Delete');
+            let article = e('article', {},
+                e('h3', {}, `${task.value.trim()}`),
+                e('p', {}, `Description: ${description.value.trim()}`),
+                e('p', {}, `Due Date: ${date.value.trim()}`),
+                e('div', { className: 'flex' },
+                    startBtn,
+                    deleteBtn));
+
+            open.appendChild(article);
+
+            startBtn.addEventListener('click', onStartHandler);
+            deleteBtn.addEventListener('click', onDeleteHandler);
+        }
+    }
+
+    function onStartHandler() {
+        let finishBtn = e('button', { className: 'orange' }, 'Finish');
+        let article = e('article', {},
+            e('h3', {}, `${this.parentElement.parentElement.children[0].textContent}`),
+            e('p', {}, `Description: ${this.parentElement.parentElement.children[1].textContent.replace('Description: ', '')}`),
+            e('p', {}, `Due Date: ${this.parentElement.parentElement.children[2].textContent.replace('Due Date: ', '')}`),
+            e('div', { className: 'flex' },
+                this.parentElement.parentElement.children[3].querySelector('.red'),
+                finishBtn));
+
+        finishBtn.addEventListener('click', onFinishHandler);
+        inProgress.appendChild(article);
+        this.parentElement.parentElement.remove()
+    }
+
+    function onFinishHandler() {
+        let article = e('article', {},
+            e('h3', {}, `${this.parentElement.parentElement.children[0].textContent}`),
+            e('p', {}, `Description: ${this.parentElement.parentElement.children[1].textContent.replace('Description: ', '')}`),
+            e('p', {}, `Due Date: ${this.parentElement.parentElement.children[2].textContent.replace('Due Date: ', '')}`));
+
+        complete.appendChild(article);
+        this.parentElement.parentElement.remove()
+    }
+
+    function onDeleteHandler() {
+        this.parentElement.parentElement.remove();
+    }
+
+
+    function e(type, attributes, ...content) {
+        const result = document.createElement(type);
+
+        for (let [attr, value] of Object.entries(attributes || {})) {
+            if (attr.substring(0, 2) == 'on') {
+                result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
+            } else {
+                result[attr] = value;
+            }
+        }
+
+        content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
+
+        content.forEach(e => {
+            if (typeof e == 'string' || typeof e == 'number') {
+                const node = document.createTextNode(e);
+                result.appendChild(node);
+            } else {
+                result.appendChild(e);
+            }
+        });
+
+        return result;
+    }
+}
+ */
+
+
+
+// third option
+
+/*
+function solve() {
+    const taskField = document.getElementById('task');
+    const descriptionField = document.getElementById('description');
+    const dueDateField = document.getElementById('date');
+    const addBtn = document.getElementById('add');
+
+    addBtn.addEventListener('click', addHandler);
+
+    function addHandler(event){
+        event.preventDefault();
+        const task = taskField.value;
+        const description = descriptionField.value;
+        const dueDate = dueDateField.value;
+        if (task === '' || description === '' || dueDate === ''){
+            return;
+        }
+        const openElement = document.querySelectorAll('section')[1].querySelectorAll('div')[1];
+        const startBtn = e('button', { className: 'green'}, 'Start');
+        const deleteBtn = e('button', { className: 'red'}, 'Delete');
+        const elementToAttach = e('article', {},
+                                    e('h3', {}, task),
+                                    e('p', {}, `Description: ${description}`),
+                                    e('p', {}, `Due Date: ${dueDate}`),
+                                    e('div', { className: 'flex'}, startBtn, deleteBtn));
+
+        openElement.appendChild(elementToAttach);
+
+        deleteBtn.addEventListener('click', () => {
+            elementToAttach.remove();
+        });
+
+        startBtn.addEventListener('click', () => startHandler(elementToAttach, startBtn, deleteBtn));
+    }
+
+    function startHandler(elementToAttach, startBtn, deleteBtn){
+        const deleteButton  = startBtn;
+        const finishButton = deleteBtn;
+        deleteButton.className = 'red';
+        deleteButton.textContent = 'Delete';
+        finishButton.className = 'orange';
+        finishButton.textContent = 'Finish';
+
+        const progressElement = document.getElementById('in-progress');
+        progressElement.appendChild(elementToAttach);
+
+        deleteButton.addEventListener('click', () => {
+            elementToAttach.remove();
+        });
+
+        finishButton.addEventListener('click', () => {
+            const completeElement = document.querySelectorAll('section')[3].querySelectorAll('div')[1];
+            completeElement.appendChild(elementToAttach);
+            elementToAttach.querySelector('div').remove();
+
+        });
+
+    }
+
+    function e(type, attributes, ...content) {
+        const result = document.createElement(type);
+
+        for (let [attr, value] of Object.entries(attributes || {})) {
+            if (attr.substring(0, 2) == 'on') {
+                result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
+            } else {
+                result[attr] = value;
+            }
+        }
+
+        content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
+
+        content.forEach(e => {
+            if (typeof e == 'string' || typeof e == 'number') {
+                const node = document.createTextNode(e);
+                result.appendChild(node);
+            } else {
+                result.appendChild(e);
+            }
+        });
+
+        return result;
+    }
+}
+ */
+
+
+
+// fourth option
+
 
 // function solve () {
 // 	const inputFields = [
