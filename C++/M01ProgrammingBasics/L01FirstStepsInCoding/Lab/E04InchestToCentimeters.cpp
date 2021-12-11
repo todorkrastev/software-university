03cade6fb078ca52776d2656e352ee342bb80934
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-11 12:17:52 +0100

    Create E04InchestToCentimeters.cpp

---
+++ C++/M01ProgrammingBasics/L01FirstStepsInCoding/Lab/E04InchestToCentimeters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double input;
+    cin >> input;
+
+    double inchesToCentimeters = input * 2.54;
+
+    cout << inchesToCentimeters;
+    return 0;
+}
