e1c5704486bf6346bddb72e4daf67ba6125e63c5
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-14 23:05:29 +0000

    Create lib.js

---
+++ JavaScript/M04_JavaScriptApplications/L09_Workshop/Cookbook/src/lib.js
@@ -0,0 +1,12 @@
+import { html, render } from './lib/lit-html.js';
+import { until } from './lib/directives/until.js';
+import { classMap } from './lib/directives/class-map.js';
+import page from './lib/page.mjs';
+
+export {
+    html,
+    render,
+    until,
+    classMap,
+    page
+};
