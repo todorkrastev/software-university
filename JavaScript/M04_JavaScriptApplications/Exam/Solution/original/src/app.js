3b6925aa76f8c0e5b2d3a6e667181141a99e98dd
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-17 23:12:04 -0300

    Create app.js

---
+++ JavaScript/M04_JavaScriptApplications/Exam/Solution/original/src/app.js
@@ -0,0 +1,57 @@
+import { logout } from './api/data.js';
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+
+import { homePage } from './views/home.js';
+import { loginPage } from './views/login.js';
+import { registerPage } from './views/register.js';
+import { catalogPage } from './views/catalog.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+
+/*
+DEBUG
+import * as api from './api/api.js';
+window.api = api;
+ */
+
+
+const root = document.getElementById('main-content');
+document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', homePage);
+page('/login', loginPage);
+page('/register', registerPage);
+page('/catalog', catalogPage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+
+updateUserNav();
+page.start();
+
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root);
+    ctx.updateUserNav = updateUserNav;
+    next();
+}
+
+function updateUserNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.getElementById('profile').style.display = 'inline';
+        document.getElementById('guest').style.display = 'none';
+    } else {
+        document.getElementById('profile').style.display = 'none';
+        document.getElementById('guest').style.display = 'inline';
+    }
+}
+
+function onLogout() {
+    logout();
+    updateUserNav();
+    page.redirect('/');
+}
