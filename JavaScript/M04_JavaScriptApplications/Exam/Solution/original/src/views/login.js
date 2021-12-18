aad5db8634e51ad6af55c77b33dfa4d784454687
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-17 23:12:30 -0300

    Create login.js

---
+++ JavaScript/M04_JavaScriptApplications/Exam/Solution/original/src/views/login.js
@@ -0,0 +1,45 @@
+import { login } from '../api/api.js';
+import { html } from '../lib.js';
+
+const loginTemplate = (onSubmit) => html`
+<section id="loginPage">
+    <form @submit=${onSubmit}>
+        <fieldset>
+            <legend>Login</legend>
+
+            <label for="email" class="vhide">Email</label>
+            <input id="email" class="email" name="email" type="text" placeholder="Email">
+
+            <label for="password" class="vhide">Password</label>
+            <input id="password" class="password" name="password" type="password" placeholder="Password">
+
+            <button type="submit" class="login">Login</button>
+
+            <p class="field">
+                <span>If you don't have profile click <a href="/register">here</a></span>
+            </p>
+        </fieldset>
+    </form>
+</section>
+`;
+
+export function loginPage(ctx) {
+    ctx.render(loginTemplate(onSubmit));
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        if (email == '' || password == '') {
+            return alert('Please fill both fields!');
+        }
+
+        await login(email, password);
+        event.target.reset();
+        ctx.updateUserNav();
+        ctx.page.redirect('/');
+    }
+}
