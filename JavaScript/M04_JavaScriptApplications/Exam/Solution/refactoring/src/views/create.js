61c0ad2eb3475283b46657efbbdcad4509fb95ff
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-17 23:14:09 -0300

    Create create.js

---
+++ JavaScript/M04_JavaScriptApplications/Exam/Solution/refactoring/src/views/create.js
@@ -0,0 +1,63 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+import { createArticle } from '../data/requests.js';
+
+const template = (onSubmit) => html`
+<section class="createPage">
+<form id="create" @submit=${onSubmit}>
+    <fieldset>
+        <legend>Add Album</legend>
+
+        <div class="container">
+            <label for="name" class="vhide">Album name</label>
+            <input id="name" name="name" class="name" type="text" placeholder="Album name">
+
+            <label for="imgUrl" class="vhide">Image Url</label>
+            <input id="imgUrl" name="imgUrl" class="imgUrl" type="text" placeholder="Image Url">
+
+            <label for="price" class="vhide">Price</label>
+            <input id="price" name="price" class="price" type="text" placeholder="Price">
+
+            <label for="releaseDate" class="vhide">Release date</label>
+            <input id="releaseDate" name="releaseDate" class="releaseDate" type="text" placeholder="Release date">
+
+            <label for="artist" class="vhide">Artist</label>
+            <input id="artist" name="artist" class="artist" type="text" placeholder="Artist">
+
+            <label for="genre" class="vhide">Genre</label>
+            <input id="genre" name="genre" class="genre" type="text" placeholder="Genre">
+
+            <label for="description" class="vhide">Description</label>
+            <textarea name="description" class="description" placeholder="Description"></textarea>
+
+            <button class="add-album" type="submit">Add New Album</button>
+        </div>
+    </fieldset>
+</form>
+</section>`;
+
+
+export const createPage = (ctx) => {
+
+    ctx.render(template(onSubmit));
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const name = formData.get('name').trim();
+        const imgUrl = formData.get('imgUrl').trim();
+        const price = formData.get('price').trim();
+        const releaseDate = formData.get('releaseDate').trim();
+        const artist = formData.get('artist').trim();
+        const genre = formData.get('genre').trim();
+        const description = formData.get('description').trim();
+
+
+        if (!name || !imgUrl || !price || !releaseDate || !artist || !genre || !description) {
+            return alert('All fields are required!');
+        }
+
+       await createArticle({ name, imgUrl, price, releaseDate, artist, genre, description }, ctx.userData.token);
+       ctx.page.redirect('/');
+    }
+}
