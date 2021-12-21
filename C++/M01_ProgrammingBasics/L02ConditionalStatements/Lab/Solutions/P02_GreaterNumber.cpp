111771b139d604c8d1c17d7f0f62cd61f46b7981
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-22 00:43:34 +0100

    Create P02_GreaterNumber.cpp

---
+++ C++/M01_ProgrammingBasics/L02ConditionalStatements/Lab/Solutions/P02_GreaterNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int num1, num2;
+	cin >> num1 >> num2;
+
+	cout << std::max(num1, num2) << endl;
+
+	return 0;
+}
