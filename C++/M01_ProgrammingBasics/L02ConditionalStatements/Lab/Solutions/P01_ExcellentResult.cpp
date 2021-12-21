712f94e1d38b51403567d8c23a62f3ddcbacddf8
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-22 00:43:30 +0100

    Create P01_ExcellentResult.cpp

---
+++ C++/M01_ProgrammingBasics/L02ConditionalStatements/Lab/Solutions/P01_ExcellentResult.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double grade;
+	cin >> grade;
+
+	if (5.5 <= grade && grade <= 6.00) {
+		cout << "Excellent!" << endl;
+	}
+
+	return 0;
+}
