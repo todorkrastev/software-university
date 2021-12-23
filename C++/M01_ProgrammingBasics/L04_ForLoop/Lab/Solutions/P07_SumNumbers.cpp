73d686873276e851c6b6220a9297c18bd34badd4
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-23 22:27:32 +0100

    Create P07_SumNumbers.cpp

---
+++ C++/M01_ProgrammingBasics/L04_ForLoop/Lab/Solutions/P07_SumNumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		int input;
+		cin >> input;
+		sum += input;
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
