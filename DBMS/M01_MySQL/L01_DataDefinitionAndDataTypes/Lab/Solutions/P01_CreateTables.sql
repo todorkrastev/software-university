2f86ef5562725cb03b261e289adfe0b48778b2a7
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-30 21:07:00 +0100

    Update P01_CreateTables.sql

--- DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Lab/Solutions/P01_CreateTables.sql
+++ DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Lab/Solutions/P01_CreateTables.sql
@@ -14,5 +14,10 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `category_id` INT NOT NULL
+  `category_id` INT NOT NULL,
+  CONSTRAINT fk_category_id FOREIGN KEY (`category_id`)
+  REFERENCES `categories` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+  INDEX `fk_categories_idx` (`category_id` ASC) INVISIBLE
 );
