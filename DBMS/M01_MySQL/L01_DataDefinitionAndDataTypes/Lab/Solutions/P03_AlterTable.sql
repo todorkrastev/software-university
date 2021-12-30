4f14774195cd216d006118d537ce23924868e996
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-30 21:07:09 +0100

    Create P03_AlterTable.sql

---
+++ DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Lab/Solutions/P03_AlterTable.sql
@@ -0,0 +1,12 @@
+  ALTER TABLE `employees`
+  ADD `middle_name` VARCHAR(50) NOT NULL DEFAULT '';
+
+/*
+ALTER TABLE `products` 
+ADD INDEX `fk_categories_ind` (`category_id` ASC) INVISIBLE;
+
+  
+  ALTER TABLE `employees`
+  ADD COLUMN `salary` DECIMAL(10, 2);
+  */
+  
