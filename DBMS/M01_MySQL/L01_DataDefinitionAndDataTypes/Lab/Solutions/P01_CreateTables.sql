a6c46e111a0b17985e56d88df4ce2a74e00fc240
beinsaduno <krastevtodordinev@yahoo.com>
2021-12-30 19:54:17 +0100

    Create P01_CreateTables.sql

---
+++ DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Lab/Solutions/P01_CreateTables.sql
@@ -0,0 +1,18 @@
+USE `gamebar`;
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL
+);
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL
+);
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `category_id` INT NOT NULL
+);
