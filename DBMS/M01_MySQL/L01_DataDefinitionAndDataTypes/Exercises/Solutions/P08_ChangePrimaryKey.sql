e17a1c115896c101b6b07122fbb71faa3cee4769
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-11 00:13:11 +0100

    Update P08_ChangePrimaryKey.sql

--- DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Exercises/Solutions/P08_ChangePrimaryKey.sql
+++ DBMS/M01_MySQL/L01_DataDefinitionAndDataTypes/Exercises/Solutions/P08_ChangePrimaryKey.sql
@@ -1,16 +1,4 @@
-CREATE TABLE `users`(
-`id` INT NOT NULL AUTO_INCREMENT,
-`username` VARCHAR(30) NOT NULL,
-`password` VARCHAR(26) NOT NULL,
-`profile_picture` LONGBLOB,
-`last_login_time` DATETIME,
-`is_deleted` CHAR(5),
-PRIMARY KEY (`id`)
-);
-
-INSERT INTO `users` (`id`, `username`, `password`, `profile_picture`, `last_login_time`, `is_deleted`) VALUES
-(1, 'beinsaduno', '8', NULL, NULL, 'true'),
-(2, 'beinsaduno', '8', NULL, NULL, 'true'),
-(3, 'beinsaduno', '8', NULL, NULL, 'true'),
-(4, 'beinsaduno', '8', NULL, NULL, 'true'),
-(5, 'beinsaduno', '8', NULL, NULL, 'true');
+ALTER TABLE `users`
+DROP PRIMARY KEY,
+ADD CONSTRAINT pk_users
+PRIMARY KEY `users`(`id`, `username`);
