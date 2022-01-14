6813412f8dc76a2273a48d0cc1fd01dc23f6e6ad
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-14 03:46:34 +0100

    Update P15_ShowAllGamesWithDuration.sql

--- DBMS/M01_MySQL/L03_BuiltinFunctions/Exercises/Solutions/P15_ShowAllGamesWithDuration.sql
+++ DBMS/M01_MySQL/L03_BuiltinFunctions/Exercises/Solutions/P15_ShowAllGamesWithDuration.sql
@@ -3,14 +3,14 @@ USE `diablo`;
 SELECT 
     `name`,
     (CASE
-        WHEN HOUR(`start`) BETWEEN 0 AND 12 THEN 'Morning'
+        WHEN HOUR(`start`) BETWEEN 0 AND 11 THEN 'Morning'
         WHEN HOUR(`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
         ELSE 'Evening'
     END) AS 'Part of the Day',
     (CASE
         WHEN `duration` <= 3 THEN 'Extra Short'
-        WHEN `duration` BETWEEN 3 AND 6 THEN 'Short'
-        WHEN `duration` BETWEEN 6 AND 10 THEN 'Long'
+        WHEN `duration` BETWEEN 4 AND 6 THEN 'Short'
+        WHEN `duration` BETWEEN 7 AND 10 THEN 'Long'
         ELSE 'Extra Long'
     END) AS 'Duration'
 FROM
