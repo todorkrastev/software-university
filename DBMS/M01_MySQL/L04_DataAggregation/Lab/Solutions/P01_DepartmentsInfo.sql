781381708bbfaba95e7b2d4d8dea2770a079f2d8
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-17 19:07:07 +0100

    Create P01_DepartmentsInfo.sql

---
+++ DBMS/M01_MySQL/L04_DataAggregation/Lab/Solutions/P01_DepartmentsInfo.sql
@@ -0,0 +1,8 @@
+USE `restaurant`;
+
+SELECT 
+    e.`department_id`, COUNT(`id`) AS `Number of employess`
+FROM
+    `employees` AS `e`
+GROUP BY `department_id`
+ORDER BY `department_id` ASC , `Number of employess` ASC;
