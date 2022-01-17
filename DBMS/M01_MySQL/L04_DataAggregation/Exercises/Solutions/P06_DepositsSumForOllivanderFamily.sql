50798512188bfd79f3b2a3f8374cc2a977167ec0
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-17 23:53:51 +0100

    Create P06_DepositsSumForOllivanderFamily.sql

---
+++ DBMS/M01_MySQL/L04_DataAggregation/Exercises/Solutions/P06_DepositsSumForOllivanderFamily.sql
@@ -0,0 +1,10 @@
+USE `gringotts`;
+
+SELECT 
+    wd.`deposit_group`, SUM(wd.`deposit_amount`) AS `total_sum`
+FROM
+    `wizzard_deposits` AS wd
+WHERE
+    `magic_wand_creator` = 'Ollivander family'
+GROUP BY wd.`deposit_group`
+ORDER BY wd.`deposit_group`;
