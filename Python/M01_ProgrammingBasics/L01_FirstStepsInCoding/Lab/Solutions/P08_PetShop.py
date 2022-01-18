06603b3a25ea8411b51ba71b59fabbfdcad9d4d1
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-19 00:00:07 +0100

    Create P08_PetShop.py

---
+++ Python/M01_ProgrammingBasics/L01_FirstStepsInCoding/Lab/Solutions/P08_PetShop.py
@@ -0,0 +1,10 @@
+num_dogs = int(input())
+num_animals = int(input())
+
+dog_food = 2.5
+animal_food = 4.0
+
+total_amount = num_dogs * dog_food + num_animals * animal_food
+
+print(f'{total_amount} lv.')
+
