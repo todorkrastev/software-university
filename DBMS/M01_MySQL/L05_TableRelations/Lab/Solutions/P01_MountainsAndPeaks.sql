64a43c46d693e42fa5ef40534901dcb930b97450
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-20 00:01:31 +0100

    Update P01_MountainsAndPeaks.sql

--- DBMS/M01_MySQL/L05_TableRelations/Lab/Solutions/P01_MountainsAndPeaks.sql
+++ DBMS/M01_MySQL/L05_TableRelations/Lab/Solutions/P01_MountainsAndPeaks.sql
@@ -6,10 +6,9 @@ CREATE TABLE `mountains` (
 );
 
 CREATE TABLE `peaks` (
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(45) NOT NULL,
-`mountain_id` INT,
-CONSTRAINT fk_peaks_montains
-FOREIGN KEY (`mountain_id`)
-REFERENCES `mountains`(`id`)
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `mountain_id` INT,
+    CONSTRAINT fk_peaks_montains FOREIGN KEY (`mountain_id`)
+        REFERENCES `mountains` (`id`)
 );
