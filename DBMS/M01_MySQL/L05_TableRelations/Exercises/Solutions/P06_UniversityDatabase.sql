2d6244457ea60374b6ddb06d4ec7efb84df7bfa9
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-21 17:26:30 +0100

    Create P06_UniversityDatabase.sql

---
+++ DBMS/M01_MySQL/L05_TableRelations/Exercises/Solutions/P06_UniversityDatabase.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA `university`;
+USE `university`;
+
+CREATE TABLE `subjects` (
+    `subject_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50)
+);
+
+CREATE TABLE `majors` (
+    `major_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `students` (
+    `student_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `student_number` VARCHAR(12),
+    `student_name` VARCHAR(50),
+    `major_id` INT,
+    CONSTRAINT pk_students_majors FOREIGN KEY (`major_id`)
+        REFERENCES `majors` (`major_id`)
+);
+
+CREATE TABLE `payments` (
+    `payment_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `payment_date` DATE,
+    `payment_amount` DECIMAL(8 , 2 ),
+    `student_id` INT,
+    CONSTRAINT fk_payments_students FOREIGN KEY (`student_id`)
+        REFERENCES `students` (`student_id`)
+);
+
+CREATE TABLE `agenda` (
+    `student_id` INT,
+    `subject_id` INT,
+    CONSTRAINT pk_agenda_subjects_students PRIMARY KEY (`student_id` , `subject_id`),
+    CONSTRAINT fk_agenda_subjects FOREIGN KEY (`subject_id`)
+        REFERENCES `subjects` (`subject_id`),
+    CONSTRAINT fk_agenda_students FOREIGN KEY (`student_id`)
+        REFERENCES `students` (`student_id`)
+);
