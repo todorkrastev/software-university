d2b25de0ef6ba0855bac4c2e97f1478ba3bb2ea5
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-26 00:46:46 +0100

    Create P05_EmployeesWithoutProject.sql

---
+++ DBMS/M01_MySQL/L06_SubqueriesAndJOIN/Exercises/Solutions/P05_EmployeesWithoutProject.sql
@@ -0,0 +1,23 @@
+USE `soft_uni`;
+
+SELECT e.`employee_id`, e.`first_name`
+FROM `employees` AS `e`
+LEFT JOIN `employees_projects` AS `ep`
+ON e.`employee_id` = ep.`employee_id`
+WHERE ep.`project_id` IS NULL
+ORDER BY e.`employee_id` DESC
+LIMIT 3;
+
+-- second option
+
+SELECT 
+    e.`employee_id`, e.`first_name`
+FROM
+    `employees` AS e
+WHERE
+    e.`employee_id` NOT IN (SELECT 
+            `employee_id`
+        FROM
+            `employees_projects`)
+ORDER BY e.`employee_id` DESC
+LIMIT 3;
