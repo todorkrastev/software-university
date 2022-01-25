c9aa05165947eae406a2121c117f42ec76a5ce16
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-25 23:33:11 +0100

    Create P01_EmployeeAddress.sql

---
+++ DBMS/M01_MySQL/L06_SubqueriesAndJOIN/Exercises/Solutions/P01_EmployeeAddress.sql
@@ -0,0 +1,13 @@
+USE `soft_uni`;
+
+SELECT 
+    e.`employee_id`,
+    e.`job_title`,
+    a.`address_id`,
+    a.`address_text`
+FROM
+    `employees` AS e
+        JOIN
+    `addresses` AS a ON e.`address_id` = a.`address_id`
+ORDER BY `address_id`
+LIMIT 5;
