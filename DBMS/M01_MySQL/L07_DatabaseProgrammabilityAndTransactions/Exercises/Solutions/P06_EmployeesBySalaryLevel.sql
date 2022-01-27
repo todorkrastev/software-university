b38d3432199dde9e47292a58b0593a5b11f7e6bb
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-28 00:27:34 +0100

    Update P06_EmployeesBySalaryLevel.sql

--- DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P06_EmployeesBySalaryLevel.sql
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P06_EmployeesBySalaryLevel.sql
@@ -15,10 +15,10 @@ DELIMITER ;
 DELIMITER $$$
 CREATE PROCEDURE `usp_get_employees_by_salary_level`(`level_of_salary` VARCHAR(10))
 BEGIN
-	SELECT `first_name`, `last_name`
-    FROM `employees`
-    WHERE `ufn_get_salary_level`(`salary`) = `level_of_salary`
-    ORDER BY `first_name` DESC, `last_name` DESC;
+	SELECT e.`first_name`, e.`last_name`
+    FROM `employees` AS `e`
+    WHERE `ufn_get_salary_level`(e.`salary`) = `level_of_salary`
+    ORDER BY e.`first_name` DESC, e.`last_name` DESC;
 END; $$$
 DELIMITER ;
 
