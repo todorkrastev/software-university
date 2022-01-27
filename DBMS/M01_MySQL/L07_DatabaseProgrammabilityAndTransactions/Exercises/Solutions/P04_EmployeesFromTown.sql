77eb646962e9f7fc401eaa734bd71b90a27c9c3d
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-27 23:18:26 +0100

    Create P04_EmployeesFromTown.sql

---
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P04_EmployeesFromTown.sql
@@ -0,0 +1,18 @@
+DELIMITER $$$
+CREATE PROCEDURE `usp_get_employees_from_town`(`town_name` VARCHAR(50))
+BEGIN
+SELECT 
+    e.`first_name`, e.`last_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` USING (`address_id`)
+        JOIN
+    `towns` AS `t` USING (`town_id`)
+WHERE
+    t.`name` = `town_name`
+ORDER BY `first_name` , `last_name` , `employee_id`;
+END $$$
+DELIMITER ;
+
+CALL `usp_get_employees_from_town`('Sofia');
