4ac3da44ca3ebf45b63a0682a05c015a74456829
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-27 22:10:52 +0100

    Create P01_EmployeesWithSalaryAbove35000.sql

---
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P01_EmployeesWithSalaryAbove35000.sql
@@ -0,0 +1,12 @@
+DELIMITER $$$
+CREATE PROCEDURE `usp_get_employees_salary_above_35000`()
+BEGIN
+SELECT `first_name`, `last_name` 
+FROM `employees`
+WHERE `salary` > 35000
+ORDER BY `first_name`,
+`last_name`,
+`employee_id`;
+END $$$
+
+CALL `usp_get_employees_salary_above_35000`();
