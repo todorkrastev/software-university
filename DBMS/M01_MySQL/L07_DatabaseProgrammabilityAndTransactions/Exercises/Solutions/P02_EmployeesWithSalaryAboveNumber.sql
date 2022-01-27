6b1a91d7d91fd000c9493c3b2a6c872e84031e47
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-27 22:24:56 +0100

    Create P02_EmployeesWithSalaryAboveNumber.sql

---
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P02_EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,14 @@
+DELIMITER $$$
+CREATE PROCEDURE `usp_get_employees_salary_above`(`decimal_num` DECIMAL(19, 4))
+BEGIN
+SELECT 
+    `first_name`, `last_name`
+FROM
+    `employees`
+WHERE
+    `salary` >= `decimal_num`
+ORDER BY `first_name` , `last_name` , `employee_id`;
+END $$$
+DELIMITER ;
+
+CALL `usp_get_employees_salary_above`(45000);
