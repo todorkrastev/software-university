efb1779e62afa9069b4a856d6fb944185dbd5d04
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-30 19:20:24 +0100

    Create P02_WeekendOrWorkingDay.py

---
+++ Python/M01_ProgrammingBasics/L03_ConditionalStatementsAdvanced/Lab/Solutions/P02_WeekendOrWorkingDay.py
@@ -0,0 +1,18 @@
+day = input()
+
+if day == "Monday":
+    print('Working day')
+elif day == "Tuesday":
+    print('Working day')
+elif day == "Wednesday":
+    print('Working day')
+elif day == "Thursday":
+    print('Working day')
+elif day == "Friday":
+    print('Working day')
+elif day == "Saturday":
+    print('Weekend')
+elif day == 'Sunday':
+    print('Weekend')
+else:
+    print('Error')
