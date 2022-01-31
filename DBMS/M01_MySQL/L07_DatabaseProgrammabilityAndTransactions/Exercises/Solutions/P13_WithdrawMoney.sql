f269b0a6768e42b9b2cd8cd57fe83e3c026fbeb3
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-31 20:34:59 +0100

    Update P13_WithdrawMoney.sql

--- DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P13_WithdrawMoney.sql
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P13_WithdrawMoney.sql
@@ -4,8 +4,10 @@ DELIMITER $$$
 CREATE PROCEDURE `usp_withdraw_money`(`account_id` INT, `money_amount` DECIMAL(19,4))
 BEGIN
 	START TRANSACTION;
-    IF (`money_amount` <= 0)
-        OR ((SELECT `balance` FROM `accounts` WHERE `id` = `account_id`) > `money_amount`)
+    IF (SELECT COUNT(*) FROM `accounts` WHERE `id` = `account_id`) = 0
+    OR
+    (`money_amount` <= 0)
+        OR ((SELECT `balance` FROM `accounts` WHERE `id` = `account_id`) < `money_amount`)
         THEN ROLLBACK;
 	ELSE
 			UPDATE `accounts`
