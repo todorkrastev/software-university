e4d69947f5d503aa1490874b51f456e80e58cb1c
beinsaduno <krastevtodordinev@yahoo.com>
2022-01-31 15:14:56 +0100

    Create P08_FindFullName.sql

---
+++ DBMS/M01_MySQL/L07_DatabaseProgrammabilityAndTransactions/Exercises/Solutions/P08_FindFullName.sql
@@ -0,0 +1,14 @@
+USE `soft_uni`;
+
+DELIMITER $$$
+CREATE PROCEDURE `usp_get_holders_full_name`()
+BEGIN
+SELECT 
+    CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`
+FROM
+    `account_holders`
+ORDER BY `full_name` , `id`;
+END; $$$
+DELIMITER ;
+
+CALL `usp_get_holders_full_name`();
