c5ef9a8b0f80af19a3c5d291ec95f385e3dd627b
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-02 23:46:51 +0100

    Create P06_DriversAndCars.sql

---
+++ DBMS/M01_MySQL/ExamPreparation/EP_2021-06-20/Solutions/P06_DriversAndCars.sql
@@ -0,0 +1,17 @@
+USE `stc`;
+
+SELECT 
+    d.`first_name`,
+    d.`last_name`,
+    c.`make`,
+    c.`model`,
+    c.`mileage`
+FROM
+    `drivers` AS d
+        JOIN
+    `cars_drivers` AS cd ON d.`id` = cd.`driver_id`
+        JOIN
+    `cars` AS c ON cd.`car_id` = c.`id`
+WHERE
+    c.`mileage` IS NOT NULL
+ORDER BY c.`mileage` DESC , d.`first_name`;
