523319a8506abc953c515c3f09e855bca07a0de0
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-03 12:47:14 +0100

    Update P04_Delete.sql

--- DBMS/M01_MySQL/ExamPreparation/EP_2021-06-20/Solutions/P04_Delete.sql
+++ DBMS/M01_MySQL/ExamPreparation/EP_2021-06-20/Solutions/P04_Delete.sql
@@ -1,6 +1,18 @@
 USE `stc`;
 
-DELETE cl, co FROM `clients` AS cl
-LEFT JOIN `courses` AS co
-ON co.`client_id` = cl.`id`
-WHERE CHAR_LENGTH(cl.`full_name`) > 3 AND co.`client_id` IS NULL;
+DELETE cl , co FROM `clients` AS cl
+        LEFT JOIN
+    `courses` AS co ON co.`client_id` = cl.`id` 
+WHERE
+    CHAR_LENGTH(cl.`full_name`) > 3
+    AND co.`client_id` IS NULL;
+
+-- or
+
+DELETE FROM `clients` 
+WHERE
+    `id` NOT IN (SELECT 
+        `client_id`
+    FROM
+        `courses`)
+    AND CHAR_LENGTH(`full_name`) > 3;
