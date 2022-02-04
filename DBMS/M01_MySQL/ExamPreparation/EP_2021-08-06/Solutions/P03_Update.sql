c5744da5ca6a04450e2be9f0700531d94460207b
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-04 01:40:10 +0100

    Create P03_Update.sql

---
+++ DBMS/M01_MySQL/ExamPreparation/EP_2021-08-06/Solutions/P03_Update.sql
@@ -0,0 +1,12 @@
+USE `sgd`;
+
+UPDATE `employees` 
+SET 
+    `salary` = `salary` + 1000
+WHERE
+    `id` IN (SELECT 
+            `leader_id`
+        FROM
+            `teams`)
+        AND `age` < 40
+        AND `salary` < 5000;
