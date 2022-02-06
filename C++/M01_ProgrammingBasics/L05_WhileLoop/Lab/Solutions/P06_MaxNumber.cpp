ef8d7b5b5e003412b08e975f6c0b0b20b0d7277c
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-06 19:20:26 +0100

    Update P06_MaxNumber.cpp

--- C++/M01_ProgrammingBasics/L05_WhileLoop/Lab/Solutions/P06_MaxNumber.cpp
+++ C++/M01_ProgrammingBasics/L05_WhileLoop/Lab/Solutions/P06_MaxNumber.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main() {
 
-	int n;
-	cin >> n;
+	string command;
+	cin >> command;
+	int n = stoi(command);
 	int maxNum = INT16_MIN;
-	int input;
-	for (int i = 0; i < n; i++) {
-		cin >> input;
-		maxNum = max(maxNum, input);
+
+	while (command != "Stop") {
+		int currNum = stoi(command);
+		maxNum = max(maxNum, currNum);
+		cin >> command;
 	}
 
 	cout << maxNum << endl;
