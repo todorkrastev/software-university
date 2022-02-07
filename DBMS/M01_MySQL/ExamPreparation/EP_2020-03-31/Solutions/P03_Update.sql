446f60677699f088c2b32471fe6635ef584b254b
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-08 00:32:15 +0100

    Create P03_Update.sql

---
+++ DBMS/M01_MySQL/ExamPreparation/EP_2020-03-31/Solutions/P03_Update.sql
@@ -0,0 +1,10 @@
+USE `ii`;
+
+UPDATE `addresses` 
+SET 
+    `country` = (CASE
+        WHEN `country` LIKE 'B%' THEN 'Blocked'
+        WHEN `country` LIKE 'T%' THEN 'Test'
+        WHEN `country` LIKE 'P%' THEN 'In Progress'
+        ELSE `country`
+    END);
