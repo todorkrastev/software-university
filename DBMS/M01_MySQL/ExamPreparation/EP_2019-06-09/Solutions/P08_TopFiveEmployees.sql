d14dc72873e22865912f75cc7599424d346171ce
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-09 18:30:51 +0100

    Update P08_TopFiveEmployees.sql

--- DBMS/M01_MySQL/ExamPreparation/EP_2019-06-09/Solutions/P08_TopFiveEmployees.sql
+++ DBMS/M01_MySQL/ExamPreparation/EP_2019-06-09/Solutions/P08_TopFiveEmployees.sql
@@ -1,13 +1,15 @@
 USE `ruk`;
 
-SELECT e.`id`,
-    CONCAT(e.`first_name`, ' ', e.`last_name`) AS `name`,
-    e.`started_on`,
+SELECT 
+    CONCAT_WS(' ',
+            ANY_VALUE(e.`first_name`),
+            ANY_VALUE(e.`last_name`)) AS `name`,
+    ANY_VALUE(e.`started_on`),
     COUNT(ec.`client_id`) AS `count_of_clients`
 FROM
     `employees` AS e
         LEFT JOIN
     `employees_clients` AS ec ON e.`id` = ec.`employee_id`
 GROUP BY ec.`employee_id`
-ORDER BY COUNT(ec.`client_id`) DESC, e.`id`
+ORDER BY count_of_clients DESC , ec.`employee_id`
 LIMIT 5;
