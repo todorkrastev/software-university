c7bcd2da625d801a3173beffeb23a934b1feb7f6
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-12 20:40:43 +0100

    Update P08_CountLikesAndComments.sql

--- DBMS/M01_MySQL/ExamPreparation/EP_2020-03-31/Solutions/P08_CountLikesAndComments.sql
+++ DBMS/M01_MySQL/ExamPreparation/EP_2020-03-31/Solutions/P08_CountLikesAndComments.sql
@@ -29,3 +29,18 @@ SELECT
 FROM
     `photos`
 ORDER BY `likes_count` DESC , `comments_count` DESC , `id`;
+
+-- Second option
+
+SELECT 
+    p.`id` AS `photo_id`,
+    COUNT(DISTINCT l.`id`) AS `likes_count`,
+    COUNT(DISTINCT c.`id`) AS `comments_count`
+FROM
+    `photos` AS p
+        LEFT JOIN
+    `likes` AS l ON p.`id` = l.`photo_id`
+        LEFT JOIN
+    `comments` AS c ON c.`photo_id` = p.`id`
+GROUP BY photo_id
+ORDER BY likes_count DESC , comments_count DESC , photo_id;
