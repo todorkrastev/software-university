0ad125beda68c9973f85aeddeb180861662b34b0
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-12 01:48:08 +0100

    Update Main.java

--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Solutions/src/main/java/Main.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Solutions/src/main/java/Main.java
@@ -1,37 +1,4 @@
-import implementations.ArrayList;
-import implementations.Stack;
-import interfaces.List;
-
 public class Main {
     public static void main(String[] args) {
-
-        Stack<String> el = new Stack<>();
-
-        el.push("c");
-        el.push("a");
-        el.push("b");
-
-        el.pop();
-        el.pop();
-        el.pop();
-
-        /*
-        List<String> names = new ArrayList<>();
-
-        names.add("one");
-        names.add("2");
-        names.add("3");
-        names.add("4");
-        names.add("five");
-
-        names.add(2, "two and a half");
-
-        System.out.println(names.get(0));
-        System.out.println(names.get(1));
-        System.out.println(names.get(2));
-        System.out.println(names.get(3));
-        System.out.println(names.get(4));
-        System.out.println(names.get(5));
-         */
     }
 }
