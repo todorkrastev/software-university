2a6b036a1c3a0bbbf8b8d50d542d414f27134d16
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-12 01:46:26 +0100

    Add L02_LinearDataStructures

---
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Solutions/src/main/java/implementations/Queue.java
@@ -0,0 +1,99 @@
+package implementations;
+
+import interfaces.AbstractQueue;
+
+import java.util.Iterator;
+
+public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private final E value;
+        private Node<E> next;
+
+        Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void offer(E element) {
+        Node<E> toInsert = new Node<>(element);
+        if (this.isEmpty()) {
+            this.head = toInsert;
+            this.size++;
+
+            return;
+        }
+        Node<E> current = this.head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = toInsert;
+        this.size++;
+    }
+
+    @Override
+    public E poll() {
+        ensureNotEmpty();
+
+        Node<E> first = this.head;
+        this.head = first.next;
+
+        this.size--;
+
+        return first.value;
+    }
+
+    @Override
+    public E peek() {
+        ensureNotEmpty();
+
+        return this.head.value;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+
+                current = current.next;
+
+                return value;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+    }
+}
