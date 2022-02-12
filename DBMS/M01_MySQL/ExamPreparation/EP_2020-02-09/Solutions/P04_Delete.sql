654c896d0c2e66122f895b7d483014f5b295237c
beinsaduno <krastevtodordinev@yahoo.com>
2022-02-12 22:58:00 +0100

    Update P04_Delete.sql

--- DBMS/M01_MySQL/ExamPreparation/EP_2020-02-09/Solutions/P04_Delete.sql
+++ DBMS/M01_MySQL/ExamPreparation/EP_2020-02-09/Solutions/P04_Delete.sql
@@ -3,3 +3,11 @@ USE `fsd`;
 DELETE FROM `players` 
 WHERE
     `age` >= 45;
+    
+-- Second option
+
+DELETE pc , p FROM `players` AS p
+        LEFT JOIN
+    `players_coaches` AS pc ON p.`id` = pc.`player_id` 
+WHERE
+    `team_id` IS NOT NULL;
