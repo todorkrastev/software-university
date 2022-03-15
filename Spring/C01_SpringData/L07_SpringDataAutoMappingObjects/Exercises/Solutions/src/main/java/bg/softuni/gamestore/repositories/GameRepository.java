df192b52af7d590c20ce6856c3a695024ddd5d63
beinsaduno <krastevtodordinev@yahoo.com>
2022-03-16 00:23:22 +0100

    Delete L07_SpringDataAutoMappingObjects

--- Spring/C01_SpringData/L07_SpringDataAutoMappingObjects/Exercises/Solutions/src/main/java/bg/softuni/gamestore/repositories/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.gamestore.repositories;
-
-import bg.softuni.gamestore.models.entities.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
--- Spring/C01_SpringData/L07_SpringDataAutoMappingObjects/Exercises/Solutions/src/main/java/bg/softuni/gamestore/services/GameService.java
+++
@@ -1,7 +0,0 @@
-package bg.softuni.gamestore.services;
-
-import bg.softuni.gamestore.models.dto.GameAddDto;
-
-public interface GameService {
-    void addGame(GameAddDto gameAddDto);
-}
