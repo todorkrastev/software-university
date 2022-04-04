7282d97a048ea2d227ee8633309eb9a9af3f0a42
beinsaduno <krastevtodordinev@yahoo.com>
2022-04-04 20:37:39 +0200

    Add L02_Functions

---
+++ C++/M02_Fundamentals/L02_Functions/Demo/04.Parameters-and-Returning-Values.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int getMax(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+int main() {
+    cout << getMax(5, 7) << endl;
+
+    return 0;
+}
