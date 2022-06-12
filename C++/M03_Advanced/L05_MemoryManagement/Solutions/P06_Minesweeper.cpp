7cf6fb65f83d9abbfcb2dead7e6d8fba5590de4a
beinsaduno <krastevtodordinev@yahoo.com>
2022-06-12 14:56:19 +0200

    Update P06_Minesweeper.cpp

--- C++/M03_Advanced/L05_MemoryManagement/Solutions/P06_Minesweeper.cpp
+++ C++/M03_Advanced/L05_MemoryManagement/Solutions/P06_Minesweeper.cpp
@@ -23,13 +23,13 @@ char numberOfMines(size_t currRow, size_t currCol, const char* field, size_t row
 
 	char mines = '0' +
 		hasMine(currRow - 1, currCol - 1, field, rows, cols) +
-		hasMine(currRow - 1, currCol,	  field, rows, cols) +
+		hasMine(currRow - 1, currCol, field, rows, cols) +
 		hasMine(currRow - 1, currCol + 1, field, rows, cols) +
-		hasMine(currRow,	 currCol - 1, field, rows, cols) +
-		hasMine(currRow,	 currCol,	  field, rows, cols) +
-		hasMine(currRow,	 currCol + 1, field, rows, cols) +
+		hasMine(currRow, currCol - 1, field, rows, cols) +
+		hasMine(currRow, currCol, field, rows, cols) +
+		hasMine(currRow, currCol + 1, field, rows, cols) +
 		hasMine(currRow + 1, currCol - 1, field, rows, cols) +
-		hasMine(currRow + 1, currCol,	  field, rows, cols) +
+		hasMine(currRow + 1, currCol, field, rows, cols) +
 		hasMine(currRow + 1, currCol + 1, field, rows, cols);
 
 	return mines;
@@ -55,6 +55,97 @@ int main() {
 	}
 
 	printMinefield(rows, cols, digitfield);
+
+
+	delete[] digitfield;
+	delete[] minefield;
+
+	return 0;
+}
+
+
+// -----------------------------------------------------------------------------------------------
+
+// Second Option
+
+// -----------------------------------------------------------------------------------------------
+
+
+#include <iostream>
+
+void printMinefield(size_t rows, size_t cols, char** field) {
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		for (size_t currCol = 0; currCol < cols; currCol++) {
+			std::cout << field[currRow][currCol];
+		}
+		std::cout << std::endl;
+	}
+}
+
+int hasMine(size_t currRow, size_t currCol, char** field, size_t rows, size_t cols) {
+
+	if (currRow >= rows || currCol >= cols) {
+		return 0;
+	}
+
+	return field[currRow][currCol] == '!' ? 1 : 0;
+}
+
+char numberOfMines(size_t currRow, size_t currCol, char** field, size_t rows, size_t cols) {
+
+	char mines = '0' +
+		hasMine(currRow - 1, currCol - 1, field, rows, cols) +
+		hasMine(currRow - 1, currCol, field, rows, cols) +
+		hasMine(currRow - 1, currCol + 1, field, rows, cols) +
+		hasMine(currRow, currCol - 1, field, rows, cols) +
+		hasMine(currRow, currCol, field, rows, cols) +
+		hasMine(currRow, currCol + 1, field, rows, cols) +
+		hasMine(currRow + 1, currCol - 1, field, rows, cols) +
+		hasMine(currRow + 1, currCol, field, rows, cols) +
+		hasMine(currRow + 1, currCol + 1, field, rows, cols);
+
+	return mines;
+}
+
+int main() {
+
+	size_t rows, cols;
+	std::cin >> rows >> cols;
+
+	char** minefield = new char* [rows];
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		minefield[currRow] = new char[cols];
+	}
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		for (size_t currCol = 0; currCol < cols; currCol++) {
+			std::cin >> minefield[currRow][currCol];
+		}
+	}
+
+	char** digitfield = new char* [rows];
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		digitfield[currRow] = new char[cols];
+	}
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		for (size_t currCol = 0; currCol < cols; currCol++) {
+			digitfield[currRow][currCol] = numberOfMines(currRow, currCol, minefield, rows, cols);
+		}
+	}
+
+	printMinefield(rows, cols, digitfield);
+
+	for (size_t currRow = 0; currRow < rows; currRow++) {
+		delete[] minefield[currRow];
+		delete[] digitfield[currRow];
+	}
+
+	delete[] digitfield;
+	delete[] minefield;
 
 	return 0;
 }
