48277f1938d6d2101c9c23b828d1c4651a16172f
beinsaduno <krastevtodordinev@yahoo.com>
2022-08-05 22:42:19 -0100

    Add EP_2021-10-24

---
+++ Spring/C02_SpringFundamentals/ExamPreparation/EP_2021-10-24/Solution/BattleShips/src/main/java/bg/manhattan/battleships/BattleShipsApplication.java
@@ -0,0 +1,13 @@
+package bg.manhattan.battleships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleShipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleShipsApplication.class, args);
+    }
+
+}
---
+++ Spring/C02_SpringFundamentals/ExamPreparation/EP_2021-10-24/Solution/BattleShips/src/test/java/bg/manhattan/battleships/BattleShipsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.manhattan.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleShipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
