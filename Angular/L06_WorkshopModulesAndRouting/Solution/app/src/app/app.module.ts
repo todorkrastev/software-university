9685dccb6d6bec111b0965ee4665d470a2ea874a
beinsaduno <krastevtodordinev@yahoo.com>
2022-11-25 22:45:49 -1200

    Update L06_WorkshopModulesAndRouting

--- Angular/L06_WorkshopModulesAndRouting/Solution/app/src/app/app.module.ts
+++ Angular/L06_WorkshopModulesAndRouting/Solution/app/src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { ThemeListComponent } from './theme-list/theme-list.component';
-import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MainComponent } from './main/main.component';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { MyModule } from './my-module';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ThemeListComponent,
-    RecentPostsComponent,
-    MainComponent
+    AppComponent
   ],
   imports: [
+    AuthModule,
+    ThemeModule,
+    AppRoutingModule,
     BrowserModule,
     CoreModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    MyModule.withProviders()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/L06_WorkshopModulesAndRouting/Solution/app/src/app/core/core.module.ts
+++ Angular/L06_WorkshopModulesAndRouting/Solution/app/src/app/core/core.module.ts
@@ -2,19 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
+    HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    // MyModule
+  ],
+  providers: [
+    // {
+    //   provide: 'Test',
+    //   useValue: 'Best'
+    // }
   ],
   exports: [
+    HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ]
 })
 export class CoreModule { }
