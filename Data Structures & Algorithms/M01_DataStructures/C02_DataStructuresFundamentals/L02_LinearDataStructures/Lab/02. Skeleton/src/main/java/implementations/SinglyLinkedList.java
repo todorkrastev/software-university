74ff675bada60b0bb4013070d2c4c6090d86af6a
todorkrastev <todorkrastev@web.de>
2025-02-12 22:30:41 +0100

    Initial Commit

---
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/SinglyLinkedList.java
@@ -0,0 +1,53 @@
+package implementations;
+
+import interfaces.LinkedList;
+
+import java.util.Iterator;
+
+public class SinglyLinkedList<E> implements LinkedList<E> {
+
+    @Override
+    public void addFirst(E element) {
+
+    }
+
+    @Override
+    public void addLast(E element) {
+
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public E getFirst() {
+        return null;
+    }
+
+    @Override
+    public E getLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
