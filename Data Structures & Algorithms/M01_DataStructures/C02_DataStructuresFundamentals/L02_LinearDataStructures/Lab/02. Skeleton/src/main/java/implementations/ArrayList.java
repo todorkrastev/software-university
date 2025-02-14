b0c83684cfc46b2bb47a35d481f4313d1398f711
todorkrastev <todorkrastev@web.de>
2025-02-14 22:59:06 +0100

    Add ArrayList implementation

--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/ArrayList.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/ArrayList.java
@@ -6,54 +6,155 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
+    private static final int INITIAL_SIZE = 4;
+    private Object[] elements;
+    private int size;
+    private int capacity;
+
+    public ArrayList() {
+        this.elements = new Object[INITIAL_SIZE];
+        this.size = 0;
+        this.capacity = INITIAL_SIZE;
+    }
 
     @Override
     public boolean add(E element) {
-        return false;
+        if (this.size == this.capacity) {
+            grow();
+        }
+
+        this.elements[this.size++] = element;
+
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        if (!validIndex(index)) {
+            return false;
+        }
+
+        shiftRight(index);
+        this.elements[index] = element;
+        this.size++;
+
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        ensureIndex(index);
+
+        return (E) this.elements[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        ensureIndex(index);
+
+        Object existing = this.elements[index];
+        this.elements[index] = element;
+
+        return (E) existing;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        ensureIndex(index);
+
+        Object existing = this.elements[index];
+
+        getShiftLeft(index);
+        this.size--;
+
+        shrinkIfNeeded();
+
+        return (E) existing;
+    }
+
+    private void shrinkIfNeeded() {
+        if (this.size > this.capacity / 3) {
+            return;
+        }
+
+        this.capacity /= 2;
+        this.elements = Arrays.copyOf(this.elements, this.capacity);
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) != -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return this.index < size;
+            }
+
+            @Override
+            public E next() {
+                return get(index++);
+            }
+        };
+    }
+
+    private void grow() {
+        this.capacity *= 2;
+        Object[] tmp = new Object[this.capacity];
+
+        for (int i = 0; i < this.elements.length; i++) {
+            tmp[i] = this.elements[i];
+        }
+
+        this.elements = tmp;
+    }
+
+    private void shiftRight(int index) {
+        for (int i = this.size - 1; i >= index; i--) {
+            this.elements[i + 1] = this.elements[i];
+        }
+    }
+
+    private void getShiftLeft(int index) {
+        for (int i = index; i < this.size - 1; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+    }
+
+    private void ensureIndex(int index) {
+        if (!validIndex(index)) {
+            throw new IndexOutOfBoundsException(
+                    "Cannot get/set element at index " + index + " because the index is out of bounds.");
+        }
+    }
+
+    private boolean validIndex(int index) {
+        return index >= 0 && index < this.size;
     }
 }
