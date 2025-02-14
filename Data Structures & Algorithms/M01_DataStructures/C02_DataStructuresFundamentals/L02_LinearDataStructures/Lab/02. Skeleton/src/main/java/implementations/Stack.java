b35af35cab6e4d1a1e3cd20352cdf93952abca30
todorkrastev <todorkrastev@web.de>
2025-02-14 21:06:33 +0100

    Update 02. Skeleton

---
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/Stack.java
@@ -0,0 +1,38 @@
+package implementations;
+
+import interfaces.AbstractStack;
+
+import java.util.Iterator;
+
+public class Stack<E> implements AbstractStack<E> {
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
