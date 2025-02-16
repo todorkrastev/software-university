dcf1f3499f7b4dc7ba3e8b715424231a185d8b1f
todorkrastev <todorkrastev@web.de>
2025-02-16 23:50:50 +0100

    Add Stack implementation

--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/Stack.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/Stack.java
@@ -5,6 +5,22 @@ import interfaces.AbstractStack;
 import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
 
     @Override
     public void push(E element) {
