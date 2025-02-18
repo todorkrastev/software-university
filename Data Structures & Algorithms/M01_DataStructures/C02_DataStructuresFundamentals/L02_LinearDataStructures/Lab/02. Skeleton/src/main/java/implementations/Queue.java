9807e248a31bbc1a32a1fe402db655336683118b
todorkrastev <todorkrastev@web.de>
2025-02-18 16:02:44 +0100

    Add Queue implementation

--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/Queue.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/L02_LinearDataStructures/Lab/02. Skeleton/src/main/java/implementations/Queue.java
@@ -5,34 +5,93 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
 
     @Override
     public void offer(E element) {
+        Node<E> toInsert = new Node<>(element);
+        if (this.head == null) {
+            this.head = toInsert;
+        } else {
+            Node<E> current = this.head;
 
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = toInsert;
+        }
+
+        this.size++;
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNotEmpty();
+
+        Node<E> first = this.head;
+        this.head = first.next;
+        this.size--;
+
+        return first.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+
+        return this.head.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+
+                current = current.next;
+
+                return value;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if (this.head == null) {
+            throw new IllegalStateException("Queue is empty");
+        }
     }
 }
