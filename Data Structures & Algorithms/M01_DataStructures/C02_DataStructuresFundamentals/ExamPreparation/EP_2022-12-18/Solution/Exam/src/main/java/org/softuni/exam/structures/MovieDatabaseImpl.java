fef928a642663098c25d5f9486ca4694b981f117
todorkrastev <todorkrastev@web.de>
2025-03-15 23:54:16 +0100

    Add `EP_2022-12-18`

--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/ExamPreparation/EP_2022-12-18/Solution/Exam/src/main/java/org/softuni/exam/structures/MovieDatabaseImpl.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/ExamPreparation/EP_2022-12-18/Solution/Exam/src/main/java/org/softuni/exam/structures/MovieDatabaseImpl.java
@@ -3,50 +3,85 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.Actor;
 import org.softuni.exam.entities.Movie;
 
+import java.util.*;
+
 public class MovieDatabaseImpl implements MovieDatabase {
+    private Map<String, Actor> actors;
+    private Map<String, Movie> movies;
+    private Map<String, List<Movie>> actorMovies;
+
+    public MovieDatabaseImpl() {
+        this.actors = new LinkedHashMap<>();
+        this.movies = new LinkedHashMap<>();
+        this.actorMovies = new HashMap<>();
+    }
 
     @Override
     public void addActor(Actor actor) {
-
+        this.actors.put(actor.getId(), actor);
     }
 
     @Override
     public void addMovie(Actor actor, Movie movie) throws IllegalArgumentException {
-
+        if (!contains(actor)) {
+            throw new IllegalArgumentException();
+        }
+        this.movies.put(movie.getId(), movie);
+        this.actorMovies.computeIfAbsent(actor.getId(), k -> new ArrayList<>()).add(movie);
     }
 
     @Override
     public boolean contains(Actor actor) {
-        return false;
+        return this.actors.containsKey(actor.getId());
     }
 
     @Override
     public boolean contains(Movie movie) {
-        return false;
+        return this.movies.containsKey(movie.getId());
     }
 
     @Override
     public Iterable<Movie> getAllMovies() {
-        return null;
+        return this.movies.values();
     }
 
     @Override
     public Iterable<Actor> getNewbieActors() {
-        return null;
+        List<Actor> newbieActors = new ArrayList<>();
+        for (Actor actor : this.actors.values()) {
+            if (!this.actorMovies.containsKey(actor.getId())) {
+                newbieActors.add(actor);
+            }
+        }
+        return newbieActors;
     }
 
     @Override
     public Iterable<Movie> getMoviesOrderedByBudgetThenByRating() {
-        return null;
+        List<Movie> sortedMovies = new ArrayList<>(this.movies.values());
+        sortedMovies.sort(Comparator.comparing(Movie::getBudget).reversed()
+                .thenComparing(Movie::getRating, Comparator.reverseOrder()));
+        return sortedMovies;
     }
 
     @Override
     public Iterable<Actor> getActorsOrderedByMaxMovieBudgetThenByMoviesCount() {
-        return null;
+        List<Actor> sortedActors = new ArrayList<>(this.actors.values());
+        sortedActors.sort(Comparator.comparing((Actor actor) -> this.actorMovies.getOrDefault(actor.getId(), Collections.emptyList()).stream()
+                        .mapToDouble(Movie::getBudget).max().orElse(0)).reversed()
+                .thenComparing((Actor actor) -> this.actorMovies.getOrDefault(actor.getId(), Collections.emptyList()).size(), Comparator.reverseOrder()));
+        return sortedActors;
     }
 
     @Override
     public Iterable<Movie> getMoviesInRangeOfBudget(double lower, double upper) {
-        return null;
+        List<Movie> filteredMovies = new ArrayList<>();
+        for (Movie movie : this.movies.values()) {
+            if (movie.getBudget() >= lower && movie.getBudget() <= upper) {
+                filteredMovies.add(movie);
+            }
+        }
+        filteredMovies.sort(Comparator.comparing(Movie::getRating).reversed());
+        return filteredMovies;
     }
 }
--- Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/ExamPreparation/EP_2022-12-18/Solution/Exam/src/main/java/org/softuni/exam/structures/ViTubeRepositoryImpl.java
+++ Data Structures & Algorithms/M01_DataStructures/C02_DataStructuresFundamentals/ExamPreparation/EP_2022-12-18/Solution/Exam/src/main/java/org/softuni/exam/structures/ViTubeRepositoryImpl.java
@@ -3,60 +3,103 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.User;
 import org.softuni.exam.entities.Video;
 
+import java.util.*;
+
 public class ViTubeRepositoryImpl implements ViTubeRepository {
+    private Map<String, User> users;
+    private Map<String, Video> videos;
+    private Map<String, Set<String>> userWatchedVideos;
+    private Map<String, Set<String>> userLikedVideos;
+    private Map<String, Set<String>> userDislikedVideos;
+
+    public ViTubeRepositoryImpl() {
+        this.users = new HashMap<>();
+        this.videos = new HashMap<>();
+        this.userWatchedVideos = new HashMap<>();
+        this.userLikedVideos = new HashMap<>();
+        this.userDislikedVideos = new HashMap<>();
+    }
 
     @Override
     public void registerUser(User user) {
-
+        this.users.put(user.getId(), user);
     }
 
     @Override
     public void postVideo(Video video) {
-
+        this.videos.put(video.getId(), video);
     }
 
     @Override
     public boolean contains(User user) {
-        return false;
+        return this.users.containsKey(user.getId());
     }
 
     @Override
     public boolean contains(Video video) {
-        return false;
+        return this.videos.containsKey(video.getId());
     }
 
     @Override
     public Iterable<Video> getVideos() {
-        return null;
+        return this.videos.values();
     }
 
     @Override
     public void watchVideo(User user, Video video) throws IllegalArgumentException {
-
+        if (!contains(user) || !contains(video)) {
+            throw new IllegalArgumentException();
+        }
+        video.setViews(video.getViews() + 1);
+        this.userWatchedVideos.computeIfAbsent(user.getId(), k -> new HashSet<>()).add(video.getId());
     }
 
     @Override
     public void likeVideo(User user, Video video) throws IllegalArgumentException {
-
+        if (!contains(user) || !contains(video)) {
+            throw new IllegalArgumentException();
+        }
+        video.setLikes(video.getLikes() + 1);
+        this.userLikedVideos.computeIfAbsent(user.getId(), k -> new HashSet<>()).add(video.getId());
     }
 
     @Override
     public void dislikeVideo(User user, Video video) throws IllegalArgumentException {
-
+        if (!contains(user) || !contains(video)) {
+            throw new IllegalArgumentException();
+        }
+        video.setDislikes(video.getDislikes() + 1);
+        this.userDislikedVideos.computeIfAbsent(user.getId(), k -> new HashSet<>()).add(video.getId());
     }
 
     @Override
     public Iterable<User> getPassiveUsers() {
-        return null;
+        List<User> passiveUsers = new ArrayList<>();
+        for (User user : this.users.values()) {
+            if (!this.userWatchedVideos.containsKey(user.getId()) &&
+                    !this.userLikedVideos.containsKey(user.getId()) &&
+                    !this.userDislikedVideos.containsKey(user.getId())) {
+                passiveUsers.add(user);
+            }
+        }
+        return passiveUsers;
     }
 
     @Override
     public Iterable<Video> getVideosOrderedByViewsThenByLikesThenByDislikes() {
-        return null;
+        List<Video> sortedVideos = new ArrayList<>(this.videos.values());
+        sortedVideos.sort(Comparator.comparing(Video::getViews).reversed()
+                .thenComparing(Video::getLikes, Comparator.reverseOrder())
+                .thenComparing(Video::getDislikes));
+        return sortedVideos;
     }
 
     @Override
     public Iterable<User> getUsersByActivityThenByName() {
-        return null;
+        List<User> sortedUsers = new ArrayList<>(this.users.values());
+        sortedUsers.sort(Comparator.comparing((User user) -> this.userWatchedVideos.getOrDefault(user.getId(), Collections.emptySet()).size()).reversed()
+                .thenComparing((User user) -> this.userLikedVideos.getOrDefault(user.getId(), Collections.emptySet()).size() + this.userDislikedVideos.getOrDefault(user.getId(), Collections.emptySet()).size(), Comparator.reverseOrder())
+                .thenComparing(User::getUsername));
+        return sortedUsers;
     }
 }
