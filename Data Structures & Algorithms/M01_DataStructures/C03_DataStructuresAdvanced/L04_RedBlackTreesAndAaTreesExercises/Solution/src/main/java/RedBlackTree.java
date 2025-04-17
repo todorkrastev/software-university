588092f9b6ea2b89a435083909814f29367c56ac
todorkrastev <todorkrastev@web.de>
2025-04-17 14:45:21 +0200

    Implement rank method in RedBlackTree

--- Data Structures & Algorithms/M01_DataStructures/C03_DataStructuresAdvanced/L04_RedBlackTreesAndAaTreesExercises/Solution/src/main/java/RedBlackTree.java
+++ Data Structures & Algorithms/M01_DataStructures/C03_DataStructuresAdvanced/L04_RedBlackTreesAndAaTreesExercises/Solution/src/main/java/RedBlackTree.java
@@ -408,12 +408,25 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
     }
 
     public int rank(Key key) {
-        return 0;
+        if (key == null) {
+            throw new IllegalArgumentException("argument to rank() is null");
+        }
+        return rank(key, this.root);
     }
 
     // number of keys less than key in the subtree rooted at x
     private int rank(Key key, Node x) {
-        return 0;
+        if (x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
     }
 
     public Iterable<Key> keys() {
